206fc22d559af1bef53e0f470fa23e0e29e3cf17
CompulsiveCoder <compulsivecoder@gmail.com>
2017-05-12 03:03:09 +0000

    Added install platformio script

---
+++ scripts/install/install-platformio.sh
@@ -0,0 +1,4 @@
+# NOTE: You may need to run this file as administrator. Example:
+# sudo sh install-platformio.sh
+
+python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
