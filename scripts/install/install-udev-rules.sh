3663a38b8cd22a62dba238bfc6ea8627a5527ad5
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-21 01:41:33 +0000

    Added init-udev-rules.sh

---
+++ scripts/install/install-udev-rules.sh
@@ -0,0 +1,7 @@
+mkdir tmp
+cd tmp
+wget https://github.com/platformio/platformio-core/blob/develop/scripts/99-platformio-udev.rules
+sudo cp 99-platformio-udev.rules /etc/udev/rules.d/99-platformio-udev.rules
+sudo service udev restart
+
+
