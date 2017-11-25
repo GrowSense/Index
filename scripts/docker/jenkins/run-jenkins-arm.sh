2963d563fe3aeb783e253fab13dd6896248aba6c
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-25 16:26:05 +1100

    Updated run-jenkins-arm.sh script to persist data

--- scripts/docker/jenkins/run-jenkins-arm.sh
+++ scripts/docker/jenkins/run-jenkins-arm.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8080:8080 -p 50000:50000 reverie/armhf-jenkins
+sudo docker run -dit --restart always -u $USER -p 8080:8080 -p 50000:50000 --device /dev/ttyUSB0 --device /dev/ttyUSB1 -v /home/$USER/jenkins:/var/jenkins_home reverie/armhf-jenkins
