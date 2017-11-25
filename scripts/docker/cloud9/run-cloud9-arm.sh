40e237c195c20a0f75040d06f034212835fcdb03
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-25 05:15:11 +0000

    Updated docker scripts

--- scripts/docker/cloud9/run-cloud9-arm.sh
+++ scripts/docker/cloud9/run-cloud9-arm.sh
@@ -4,11 +4,5 @@
 HOST_PORT="8181"
 WORKSPACE_PATH="/home/$USER/workspace/GreenSense/Index"
 
-# No authentication
-#docker run --name rpi-cloud9-ide -it -d -p $HOST_PORT:8181 -v $WORKSPACE_PATH:/workspace hwegge2/rpi-cloud9-ide node server.js -w/workspace --listen 0.0.0.0 -a :
+docker run --name rpi-cloud9-ide -it -d -p $HOST_PORT:8181 -v $WORKSPACE_PATH:/workspace hwegge2/rpi-cloud9-ide node server.js -w/workspace --listen 0.0.0.0 -a :
 
-USERNAME="user"
-PASWORD="pass1234"
-
-# Authenticated by username and password
-docker run --name rpi-cloud9-ide -it -d -p $HOST_PORT:8181 -v $WORKSPACE_PATH:/workspace hwegge2/rpi-cloud9-ide node server.js -w/workspace --listen 0.0.0.0 -a $USERNAME:$PASSWORD
