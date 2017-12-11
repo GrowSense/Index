a7d18261e88489ada8ce4fcd6d88981d359cefaf
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-11 21:39:00 +0000

    Updated noip duc installer

--- scripts/install/install-noip-duc.sh
+++
@@ -1,8 +0,0 @@
-mkdir /home/$USER/noip
-cd /home/$USER/noip
-
-wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
-tar vzxf noip-duc-linux.tar.gz
-cd noip-2.1.9-1
-sudo make
-sudo make install
