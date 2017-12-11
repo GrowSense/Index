2897837e546da15743e24e269226740fcca462bf
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-11 21:37:17 +1100

    Updated start-nginx-public-arm script

--- scripts/docker/nginx-public/start-nginx-arm-public.sh
+++ scripts/docker/nginx-public/start-nginx-arm-public.sh
@@ -1,3 +1,3 @@
 PUBLIC_DIR=$PWD/../../../public/
 mkdir -p $PUBLIC_DIR
-docker run -d --name greensense-test-results -p 8081:80 -v $PUBLIC_DIR:/usr/share/nginx/html:ro -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -i zsoltm/nginx-armhf
+docker run -i --rm --name greensense-test-results -p 8081:80 -v $PUBLIC_DIR:/usr/share/nginx/html:ro -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -i zsoltm/nginx-armhf
