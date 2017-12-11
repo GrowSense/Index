4a8c8f127d17cd8a8557a7be827720b4e7eb9aa1
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-11 19:08:20 +1100

    Added irrigator tests

--- test-all-via-docker-from-github-and-log.sh
+++ test-all-via-docker-from-github-and-log.sh
@@ -6,9 +6,23 @@ USER=j
 TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
 
 # SoilMoistureSensorCalibratedSerial
-SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH="/home/$USER/workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial"
-SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH/logs"
-SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH/$TIMESTAMP.log"
-mkdir -p $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH
-echo "Logging to: $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH"
-curl -s https://raw.githubusercontent.com/GreenSense/SoilMoistureSensorCalibratedSerial/master/test-via-docker-from-github.sh | bash > $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH
+echo "Testing SoilMoistureSensorCalibratedSerial"
+#SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH="/home/$USER/workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial"
+#SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH/logs"
+#SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH/$TIMESTAMP.log"
+#mkdir -p $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH
+#echo "Logging to: $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH"
+#curl -s https://raw.githubusercontent.com/GreenSense/SoilMoistureSensorCalibratedSerial/master/test-via-docker-from-github.sh | bash > $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH
+
+# SoilMoistureSensorCalibratedPump
+echo "Testing SoilMoistureSensorCalibratedPump"
+SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_PATH="/home/$USER/workspace/GreenSense/Index/sketches/irrigator/SoilMoistureSensorCalibratedPump"
+SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOGS_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_PATH/logs"
+SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOG_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOGS_PATH/$TIMESTAMP.log"
+mkdir -p $SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOGS_PATH
+echo "Logging to: $SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOG_PATH"
+curl -s https://raw.githubusercontent.com/GreenSense/SoilMoistureSensorCalibratedPump/master/test-via-docker-from-github-as-second-pair.sh | bash > $SO$
+
+
+
+
