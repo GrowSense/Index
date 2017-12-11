f5e9aba2d1d83acff40efa02e72fbb7058d7c914
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-11 19:26:02 +1100

    Timestamp updates for each project test

--- test-all-via-docker-from-github-and-log.sh
+++ test-all-via-docker-from-github-and-log.sh
@@ -6,6 +6,7 @@ USER=j
 TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
 
 # SoilMoistureSensorCalibratedSerial
+TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
 echo "Testing SoilMoistureSensorCalibratedSerial"
 #SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH="/home/$USER/workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial"
 #SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOGS_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_PATH/logs"
@@ -15,6 +16,7 @@ echo "Testing SoilMoistureSensorCalibratedSerial"
 #curl -s https://raw.githubusercontent.com/GreenSense/SoilMoistureSensorCalibratedSerial/master/test-via-docker-from-github.sh | bash > $SOIL_MOISTURE_SENSOR_CALIBRATED_SERIAL_LOG_PATH
 
 # SoilMoistureSensorCalibratedPump
+TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
 echo "Testing SoilMoistureSensorCalibratedPump"
 SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_PATH="/home/$USER/workspace/GreenSense/Index/sketches/irrigator/SoilMoistureSensorCalibratedPump"
 SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_LOGS_PATH="$SOIL_MOISTURE_SENSOR_CALIBRATED_PUMP_PATH/logs"
