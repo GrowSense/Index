edf2b54c06c07ac9d537e270460a0de649f0919d
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 05:14:07 +0000

    Partially reenabled test script

--- test-project-via-docker-from-github-and-log.sh
+++ test-project-via-docker-from-github-and-log.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 echo ""
 echo "Starting test of GreenSense project"
 echo ""
@@ -9,28 +7,28 @@ echo ""
 echo "Project: $1"
 
 # The username is hard coded to work with cron. This can be commented out to auto-detect the user.
-#USER=j
+USER=j
 
 WORKSPACE_PATH=/home/$USER/workspace
 ##WORKSPACE_PATH=/media/$USER/store/workspace
 
-#GREENSENSE_INDEX_PATH="$WORKSPACE_PATH/GreenSense/Index"
+GREENSENSE_INDEX_PATH="$WORKSPACE_PATH/GreenSense/Index"
 
-#TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
-#echo "Testing $1"
+TIMESTAMP=$(date +"%Y_%m_%d_%I_%M_%p")
 
-#PROJECT_NAME=$(basename $1)
-#echo "Project name: $PROJECT_NAME"
-#PROJECT_PATH="$GREENSENSE_INDEX_PATH/$1"
-#PROJECT_LOGS_PATH="$PROJECT_PATH/logs"
-#PROJECT_LOG_PATH="$PROJECT_LOGS_PATH/$TIMESTAMP.log"
-#PROJECT_GIT_URL="https://raw.githubusercontent.com/GreenSense/$PROJECT_NAME"
-#mkdir -p $PROJECT_LOGS_PATH
-#echo "Logging to: $PROJECT_LOG_PATH"
-#echo "Git URL: $PROJECT_GIT_URL"
+PROJECT_NAME=$(basename $1)
+echo "Project name: $PROJECT_NAME"
+PROJECT_PATH="$GREENSENSE_INDEX_PATH/$1"
+PROJECT_LOGS_PATH="$PROJECT_PATH/logs"
+PROJECT_LOG_PATH="$PROJECT_LOGS_PATH/$TIMESTAMP.log"
+PROJECT_GIT_URL="https://raw.githubusercontent.com/GreenSense/$PROJECT_NAME"
+mkdir -p $PROJECT_LOGS_PATH
+echo "Logging to: $PROJECT_LOG_PATH"
+echo "Git URL: $PROJECT_GIT_URL"
 
 # Get the script and run it
 #curl -s $PROJECT_GIT_URL/master/test-via-docker-from-github.sh | bash > $PROJECT_LOG_PATH
+curl -s $PROJECT_GIT_URL/master/test-via-docker-from-github-mock-success.sh | bash > $PROJECT_LOG_PATH
 
 # Check the output
 #ANALYSE_SCRIPT_URL="https://raw.githubusercontent.com/GreenSense/Index/master/test-project-via-docker-from-github-and-log.sh"
