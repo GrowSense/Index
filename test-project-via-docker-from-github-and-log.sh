9237947de057fbc167422712acd05eafe520b54e
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 05:22:53 +0000

    Fixed test scripts

--- test-project-via-docker-from-github-and-log.sh
+++ test-project-via-docker-from-github-and-log.sh
@@ -10,7 +10,7 @@ echo "Project: $1"
 USER=j
 
 WORKSPACE_PATH=/home/$USER/workspace
-##WORKSPACE_PATH=/media/$USER/store/workspace
+#WORKSPACE_PATH=/media/$USER/store/workspace
 
 GREENSENSE_INDEX_PATH="$WORKSPACE_PATH/GreenSense/Index"
 
@@ -26,12 +26,20 @@ mkdir -p $PROJECT_LOGS_PATH
 echo "Logging to: $PROJECT_LOG_PATH"
 echo "Git URL: $PROJECT_GIT_URL"
 
+echo ""
+echo "Getting project test script from git..."
+echo ""
+
 # Get the script and run it
 #curl -s $PROJECT_GIT_URL/master/test-via-docker-from-github.sh | bash > $PROJECT_LOG_PATH
 curl -s $PROJECT_GIT_URL/master/test-via-docker-from-github-mock-success.sh | bash > $PROJECT_LOG_PATH
 
 # Check the output
-ANALYSE_SCRIPT_URL="https://raw.githubusercontent.com/GreenSense/Index/analyse-test-log.sh"
+ANALYSE_SCRIPT_URL="https://raw.githubusercontent.com/GreenSense/Index/master/analyse-test-log.sh"
+
+echo ""
+echo "Getting analysis script from git..."
+echo ""
 
 curl $ANALYSE_SCRIPT_URL | bash -s $PROJECT_LOG_PATH
 
