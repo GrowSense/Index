35694cf0dd70c125a3bbbdd718a70c099f09041d
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 03:07:10 +0000

    Fixed test all script so it doesn't need the git repository to exist locally to run

--- test-all-via-docker-from-github-and-log.sh
+++ test-all-via-docker-from-github-and-log.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sh test-project-via-docker-from-github-and-log.sh "sketches/monitor/SoilMoistureSensorCalibratedSerial"
-sh test-project-via-docker-from-github-and-log.sh "sketches/irrigator/SoilMoistureSensorCalibratedPump"
+TEST_SCRIPT_URL="https://raw.githubusercontent.com/GreenSense/Index/master/test-project-via-docker-from-github-and-log.sh"
+
+curl $TEST_SCRIPT_URL | bash -s "sketches/monitor/SoilMoistureSensorCalibratedSerial"
+curl $TEST_SCRIPT_URL | bash -s "sketches/irrigator/SoilMoistureSensorCalibratedPump"
