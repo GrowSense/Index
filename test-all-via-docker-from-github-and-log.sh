831de932dc545473123af98b4c1335c8d804c23e
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 05:28:43 +0000

    Fixed test scripts

--- test-all-via-docker-from-github-and-log.sh
+++ test-all-via-docker-from-github-and-log.sh
@@ -6,8 +6,8 @@ echo ""
 
 TEST_SCRIPT_URL="https://raw.githubusercontent.com/GreenSense/Index/master/test-project-via-docker-from-github-and-log.sh"
 
-curl $TEST_SCRIPT_URL | bash -s "sketches/monitor/SoilMoistureSensorCalibratedSerial"
-#curl $TEST_SCRIPT_URL | bash -s "sketches/irrigator/SoilMoistureSensorCalibratedPump"
+curl -H 'Cache-Control: no-cache' $TEST_SCRIPT_URL | bash -s "sketches/monitor/SoilMoistureSensorCalibratedSerial"
+#curl -H 'Cache-Control: no-cache' $TEST_SCRIPT_URL | bash -s "sketches/irrigator/SoilMoistureSensorCalibratedPump"
 
 echo ""
 echo "Finished tests!"
