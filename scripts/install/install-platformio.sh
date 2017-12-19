9428a800586bfee4a3e1bb4c4ef92f212133cc33
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-19 14:36:45 +1100

    Updated install platform.io script

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,4 +1,6 @@
 # NOTE: You may need to run this file as administrator. Example:
 # sudo sh install-platformio.sh
 
+sudo apt-get install python python-pip
+
 python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
