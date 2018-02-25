8dcaa2211fac177edeb63a03dd7f90aa6b5105b4
CompulsiveCoder <compulsivecoder@gmail.com>
2018-02-25 06:59:24 +0000

    Updated blink sketch

--- sketches/util/Blink/src/Blink/Blink.ino
+++ sketches/util/Blink/src/Blink/Blink.ino
@@ -1,11 +1,13 @@
+int ledPin = 13;
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledPin, HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(ledPin, LOW);
   delay(1000);
 }
 
