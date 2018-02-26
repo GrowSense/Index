0473081e8704c9bedbf70710d9c5eb7ce92042af
CompulsiveCoder <compulsivecoder@gmail.com>
2018-02-26 01:37:54 +0000

    Added updated scripts

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+sudo apt update && \
+
+sudo sh scripts/install/install-platformio.sh && \
+sudo sh scripts/install/install-mono.sh
--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,6 +1,6 @@
 # NOTE: You may need to run this file as administrator. Example:
 # sudo sh install-platformio.sh
 
-sudo apt-get install python python-pip
+sudo apt-get install -y python python-pip
 
-python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
