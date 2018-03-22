dfaa0a484123ba560ed4afe26302cb5341a03ec0
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-22 10:47:57 +0000

    Changed jenkins docker container to daemon

--- scripts/docker/jenkins/run-jenkins-arm.sh
+++ scripts/docker/jenkins/run-jenkins-arm.sh
@@ -1,1 +1,1 @@
-docker run -it --restart always -p 8080:8080 -p 50000:50000 -v /usr/local/jenkins:/usr/local/jenkins compulsivecoder/jenkins-arm-iot-mono
+docker run -d --restart always -p 8080:8080 -p 50000:50000 -v /usr/local/jenkins:/usr/local/jenkins compulsivecoder/jenkins-arm-iot-mono
