c8b5ab6613b4aa1c0ff974e57a9bdc8a960178fc
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-24 08:29:46 +0000

    Added scripts and fixed services

--- create-garden-irrigator.sh
+++ create-garden-irrigator.sh
@@ -1,5 +1,5 @@
 echo ""
-echo "Creating garden monitor configuration"
+echo "Creating garden irrigator configuration"
 echo ""
 
 DIR=$PWD
@@ -8,7 +8,7 @@ DEVICE_NAME=$1
 DEVICE_PORT=$2
 
 if [ ! $DEVICE_NAME ]; then
-  DEVICE_NAME="monitor1"
+  DEVICE_NAME="irrigator1"
 fi
 
 if [ ! $DEVICE_PORT ]; then
@@ -20,7 +20,7 @@ echo "Device port: $DEVICE_PORT"
 
 # Set up MQTT bridge service
 cd scripts/apps/BridgeArduinoSerialToMqttSplitCsv/ && \
-cp svc/greensense-mqtt-bridge-monitor1.service.example svc/greensense-mqtt-bridge-$DEVICE_NAME.service && \
+cp svc/greensense-mqtt-bridge-irrigator1.service.example svc/greensense-mqtt-bridge-$DEVICE_NAME.service && \
 sed -i "s/monitor1/$DEVICE_NAME/g" svc/greensense-mqtt-bridge-$DEVICE_NAME.service && \
 sed -i "s/ttyUSB0/$DEVICE_PORT/g" svc/greensense-mqtt-bridge-$DEVICE_NAME.service && \
 sh install-services.sh && \
@@ -28,13 +28,13 @@ cd $DIR && \
 
 # Set up update service
 cd scripts/apps/GitDeployer/ && \
-cp svc/greensense-updater-monitor1.service.example svc/greensense-updater-$DEVICE_NAME.service && \
+cp svc/greensense-updater-irrigator1.service.example svc/greensense-updater-$DEVICE_NAME.service && \
 sed -i "s/ttyUSB0/$DEVICE_PORT/g" svc/greensense-updater-$DEVICE_NAME.service && \
 sh install-services.sh && \
 cd $DIR && \
 
 # Upload sketch
-cd sketches/monitor/SoilMoistureSensorCalibratedSerial
+cd sketches/irrigator/SoilMoistureSensorCalibratedPump
 sh init.sh && \
 sh build.sh && \
 sh upload.sh "/dev/$DEVICE_PORT"
