2309ec2b3eea7a6970e167aa783f7447146a2c8a
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-25 04:24:15 +0000

    Added upload blink sketch helper script

--- sketches/util/Blink/upload.sh
+++ sketches/util/Blink/upload.sh
@@ -1,6 +1,14 @@
-#pio run --target upload
+PORT_NAME=$1
 
-pio run --target upload --environment uno
-#pio run --target upload --environment nanoatmega328
+if [ ! $PORT_NAME ]; then
+  PORT_NAME="/dev/ttyUSB0"
+fi
+
+echo "Port: $PORT_NAME"
+
+#pio run --target upload --upload-port $PORT_NAME
+
+#pio run --target upload --environment uno --upload-port $PORT_NAME
+pio run --target upload --environment nanoatmega328 --upload-port $PORT_NAME
 
 #pio run --target upload --environment uno --upload-port /dev/ttyUSB3
