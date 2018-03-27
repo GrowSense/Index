4486837ecca990ceb242392b31437a8eda94883d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-27 01:17:19 +0000

    Updated create garden script

--- create-garden.sh
+++ create-garden.sh
@@ -1,11 +1,10 @@
 DIR=$DIR
 
-# Set up mosquitto MQTT service
-cd scripts/docker/mosquitto/
-cp data/mosquitto.userfile.example data/mosquitto.userfile
-sh install-mosquitto-docker-service.sh
-cd $DIR
+echo "Setting up mosquitto MQTT broker docker container"
 
-# Note: Edit the following file to set the mosquitto username and password:
-# scripts/docker/mosquitto/data/mosquitto.userfile 
+cd scripts/docker/mosquitto/ && \
+cp data/mosquitto.userfile.example data/mosquitto.userfile && \
+sh install-mosquitto-docker-service.sh && \
+cd $DIR && \
 
+echo "Setup complete"
