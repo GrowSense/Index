97808cfd3cec869a51c8acd6f04b74519df01fe5
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-27 01:19:06 +0000

    Fixed prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get -y install systemd  \
+sudo apt-get update && sudo apt-get -y install systemd && \
 
 sudo sh scripts/install/install-platformio.sh && \
 sudo sh scripts/install/install-mono.sh
