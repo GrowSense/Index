833853dcbcdc673f78e409bee03e073c44f6167d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-27 03:14:57 +0000

    Added jq to prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get -y install systemd && \
+sudo apt-get update && sudo apt-get -y install systemd jq && \
 
 sudo sh scripts/install/install-platformio.sh && \
 sudo sh scripts/install/install-mono.sh
