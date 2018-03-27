aecfc8beffa14450fda30e88d71700f53e7b5a9d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-27 05:46:14 +0000

    Fixed init script

--- init.sh
+++ init.sh
@@ -12,8 +12,8 @@ cd scripts/apps/GitDeployer/ && \
 sh init.sh && \
 cd $DIR && \
 
-cd mobile/linearmqtt/
-sh init.sh
+cd mobile/linearmqtt/ && \
+sh init.sh && \
 cd $DIR && \
 
 echo "Initialization completed."
