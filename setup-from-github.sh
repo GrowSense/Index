7a52cdc9d0119b9bc6417498ec7714a8d3127ed0
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-27 04:16:08 +0000

    Updated setup script

--- setup-from-github.sh
+++ setup-from-github.sh
@@ -1,6 +1,25 @@
+echo ""
+echo "Setting up GreenSense index from GitHub"
+echo ""
+
 git clone --recursive https://github.com/GreenSense/Index.git GreenSense/Index && \
 
-cd GreenSense/Index && \
+CURRENT_DIR=$PWD && \
+
+echo "Current directory:"
+echo "  $CURRENT_DIR"
+
+INDEX_DIR="$CURRENT_DIR/GreenSense/Index" && \
+
+echo "GreenSense index directory:"
+echo "  $INDEX_DIR"
+
+cd $INDEX_DIR && \
 
 sh prepare.sh && \
-sh init.sh
+sh init.sh && \
+
+cd $INDEX_DIR && \
+
+echo "" && \
+echo "The GreenSense index is initialized and ready to use."
