ec6184f82e61cc7d4478f75ebefcf44ae2802471
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-29 06:59:59 +0000

    Updated install platformio script so it skips if already found

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,9 +1,14 @@
 # NOTE: You may need to run this file as administrator. Example:
 # sudo sh install-platformio.sh
 
-sudo apt-get install -y python python-pip && \
 
-sudo pip install --upgrade pip && \
-sudo pip install setuptools && \
+if ! type "pio" > /dev/null; then
+  sudo apt-get install -y python python-pip && \
 
-sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+  sudo pip install --upgrade pip && \
+  sudo pip install setuptools && \
+
+  sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+else
+  echo "Platform.io is already installed. Skipping."
+fi
