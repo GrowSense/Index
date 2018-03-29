a0a529204995ca0f8587fd2cf6ac368a7ac984a0
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-29 05:49:45 +0000

    Updated setup from github script

--- setup-from-github.sh
+++ setup-from-github.sh
@@ -2,9 +2,9 @@ echo ""
 echo "Setting up GreenSense index from GitHub"
 echo ""
 
-if ! type "git" > /dev/null; then
-  sudo apt-get update -qq && sudo apt-get -y git
-fi
+#if ! type "git" > /dev/null; then
+  sudo apt-get update && sudo apt-get -y git
+#fi
 
 git clone --recursive https://github.com/GreenSense/Index.git GreenSense/Index && \
 
