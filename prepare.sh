13aaae8e9d107386df4b1e9215da2d49b72e6a7f
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-29 09:24:43 +0000

    Added unzip to prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get -y install systemd jq zip && \
+sudo apt-get update && sudo apt-get -y install systemd jq zip unzip && \
 
 sudo sh scripts/install/install-docker.sh && \
 sudo sh scripts/install/install-platformio.sh && \
