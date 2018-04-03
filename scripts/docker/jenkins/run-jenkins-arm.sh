c52aa3a5b9ef37ad81664cfbef5d2d6024dd41ae
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-03 01:59:36 +0000

    Added sock volume to jenkins docker containers

--- scripts/docker/jenkins/run-jenkins-arm.sh
+++ scripts/docker/jenkins/run-jenkins-arm.sh
@@ -1,4 +1,4 @@
 docker stop jenkins || echo "Skipping stop"
 docker rm jenkins || echo "Skipping remove" 
 
-docker run -d --name=jenkins --restart always -p 8080:8080 -p 50000:50000 -v /usr/local/jenkins:/usr/local/jenkins compulsivecoder/jenkins-arm-iot-mono
+docker run -d --name=jenkins --restart always -p 8080:8080 -p 50000:50000 -v /usr/local/jenkins:/usr/local/jenkins -v /var/run/docker.sock:/var/run/docker.sock compulsivecoder/jenkins-arm-iot-mono
