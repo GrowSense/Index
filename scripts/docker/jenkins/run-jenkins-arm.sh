4ba05421fd6e8cd7e7daea63b541e655a2ec9a52
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-04 00:20:48 +0000

    Updated run jenkins script

--- scripts/docker/jenkins/run-jenkins-arm.sh
+++ scripts/docker/jenkins/run-jenkins-arm.sh
@@ -1,4 +1,6 @@
 docker stop jenkins || echo "Skipping stop"
 docker rm jenkins || echo "Skipping remove" 
 
+docker pull compulsivecoder/jenkins-arm-iot-mono && \
+
 docker run -d --name=jenkins --restart always -p 8080:8080 -p 50000:50000 -v /usr/local/jenkins:/usr/local/jenkins compulsivecoder/jenkins-arm-iot-mono
