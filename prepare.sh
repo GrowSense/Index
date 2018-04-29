8f3cece2be7ff41945c2cac8013742e54fb5734c
John <compulsivecoder@gmail.com>
2018-04-29 22:17:56 +0000

    Fixed setup

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,12 @@
+DIR=$PWD
+
 sudo apt-get update && sudo apt-get -y install git jq zip unzip && \
 
-sudo sh scripts/install/install-docker.sh && \
-sudo sh scripts/install/install-platformio.sh && \
-sudo sh scripts/install/install-mono.sh
+cd scripts/install/ && \
+
+sudo sh install-systemd.sh && \
+sudo sh install-docker.sh && \
+sudo sh install-platformio.sh && \
+sudo sh install-mono.sh && \
+
+cd $DIR
--- scripts/install/install-systemd.sh
+++ scripts/install/install-systemd.sh
@@ -1,11 +1,10 @@
 MOCK_FLAG_FILE="../../is-mock-setup.txt"
 
 if ! type "systemd" > /dev/null; then
-    
-  if [ ! -f $MOCK_FLAG_FILE ]; then
+  if [ ! -f "$MOCK_FLAG_FILE" ]; then
     sudo apt-get install -y systemd
   else
-    exho "Mock setup. Skipping systemd install."
+    echo "Mock setup. Skipping systemd install."
   fi
 else
   echo "systemd is already installed. Skipping install."
