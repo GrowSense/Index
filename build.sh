725b3d59e9adbe391a787cab6d499871875ebcc1
John <compulsivecoder@gmail.com>
2018-04-29 22:12:58 +0000

    Updated build and install scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
-echo "Builds are not yet implemented"
+echo "Building entire index"
+
+DIR=$PWD
+
+cd sketches/monitor/SoilMoistureSensorCalibratedSerial
+sh build-all.sh
+cd $DIR
+
+cd sketches/monitor/SoilMoistureSensorCalibratedSerialESP
+sh build-all.sh
+cd $DIR
+
+cd sketches/irrigator/SoilMoistureSensorCalibratedPump
+sh build-all.sh
+cd $DIR
+
+cd sketches/irrigator/SoilMoistureSensorCalibratedPumpESP
+sh build-all.sh
+cd $DIR
