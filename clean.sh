9651332f98a7c37d9c3657632f1771134bbafb5c
John <compulsivecoder@gmail.com>
2018-04-30 08:10:44 +0000

    Fixing testing

--- clean.sh
+++ clean.sh
@@ -3,10 +3,6 @@ DIR=$PWD
 echo ""
 echo "Cleaning project"
 echo ""
-echo "Resetting linear MQTT UI"
-
-cd mobile/linearmqtt
-sh reset.sh
-cd $DIR
+sh remove-garden-devices.sh
 
 echo "Done"
--- remove-garden-device.sh
+++ remove-garden-device.sh
@@ -4,19 +4,21 @@ echo ""
 
 DEVICE_NAME=$1
 
+SYSTEMCTL_SCRIPT="../../../../systemctl.sh"
+
 if [ ! $DEVICE_NAME ]; then
   echo "Error: Specify a device name as an argument."
 else
   echo "Device name: $DEVICE_NAME"
 
   echo "Disabling MQTT bridge service" && \
-  sudo systemctl disable greensense-mqtt-bridge-$DEVICE_NAME.service && \
+  sudo sh $SYSTEMCTL_SCRIPT disable greensense-mqtt-bridge-$DEVICE_NAME.service && \
 
   echo "Removing MQTT bridge service" && \
   rm -f scripts/apps/BridgeArduinoSerialToMqttSplitCsv/svc/greensense-mqtt-bridge-$DEVICE_NAME.service && \
 
   echo "Disabling updater service" && \
-  sudo systemctl disable greensense-updater-$DEVICE_NAME.service && \
+  sudo sh $SYSTEMCTL_SCRIPT disable greensense-updater-$DEVICE_NAME.service && \
 
   echo "Removing updater service" && \
   rm -f scripts/apps/GitDeployer/svc/greensense-updater-$DEVICE_NAME.service && \
