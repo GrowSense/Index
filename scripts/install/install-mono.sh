5d8129a55f00c361e0d6695bddb2b598fe20820c
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-30 06:54:46 +0000

    Fixed .travis.yml

--- scripts/install/install-mono.sh
+++ scripts/install/install-mono.sh
@@ -1,9 +1,12 @@
 
 if ! type "msbuild" > /dev/null; then
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+  VERSION_NAME=$(lsb_release -cs)
 
-  apt-get update && apt-get install -y mono-complete ca-certificates-mono msbuild
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && \
+  echo "deb https://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list && \
+  apt-get update && \
+  apt-get install -y mono-complete ca-certificates-mono msbuild
 else
   echo "Mono/msbuild is already installed. Skipping install."
 fi
+
