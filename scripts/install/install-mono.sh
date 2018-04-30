8e4398a693cc25c79e6f86ddd499cde7edae05ed
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-30 00:29:07 +0000

    Fixed setup scripts and added docker tests

--- scripts/install/install-mono.sh
+++ scripts/install/install-mono.sh
@@ -1,9 +1,9 @@
 
 if ! type "msbuild" > /dev/null; then
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 
-  sudo apt-get update && apt-get install -y mono-complete ca-certificates-mono msbuild
+  apt-get update && apt-get install -y mono-complete ca-certificates-mono msbuild
 else
   echo "Mono/msbuild is already installed. Skipping install."
 fi
--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -3,12 +3,12 @@
 
 
 if ! type "pio" > /dev/null; then
-  sudo apt-get install -y python python-pip && \
+  apt-get install -y python python-pip && \
 
-  sudo pip install --upgrade pip && \
-  sudo pip install setuptools && \
+  #pip install --upgrade pip && \
+  #pip install setuptools && \
 
-  sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+  python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
   echo "Platform.io is already installed. Skipping."
 fi
