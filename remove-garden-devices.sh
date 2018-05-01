6997cf2d4ad1e68e81ea65aeb5c77ef2bfadb52c
John <compulsivecoder@gmail.com>
2018-05-01 23:28:03 +0000

    Fixing scripts and tests

--- remove-garden-devices.sh
+++ remove-garden-devices.sh
@@ -12,12 +12,14 @@ echo ""
 
 for filename in scripts/apps/BridgeArduinoSerialToMqttSplitCsv/svc/*.service; do
   [ -f "$filename" ] || break
-  echo "Removing device: $filename" && \
+  shortname=$(basename $filename)
+  echo "Removing service: $filename" && \
   echo "" && \
-  sudo sh $SYSTEMCTL_SCRIPT stop "$filename" && \
-  sudo sh $SYSTEMCTL_SCRIPT disable "$filename" && \
+  #sudo sh $SYSTEMCTL_SCRIPT stop "$filename" && \
+  #sudo sh $SYSTEMCTL_SCRIPT disable "$filename" && \
   echo "" && \
-  rm $filename || exit 1
+  rm -v $filename || exit 1
+  #rm -v /lib/systemd/system/$shortname || exit 1
 done
 
 echo ""
@@ -26,12 +28,25 @@ echo ""
 
 for filename in scripts/apps/GitDeployer/svc/*.service; do
   [ -f "$filename" ] || break
-  echo "Removing device: $filename" && \
+  shortname=$(basename $filename)
+  echo "Removing service: $filename" && \
+  echo "" && \
+  #sudo sh $SYSTEMCTL_SCRIPT stop "$filename" && \
+  #sudo sh $SYSTEMCTL_SCRIPT disable "$filename" && \
+  echo "" && \
+  rm -v $filename || exit 1
+  #rm -v /lib/systemd/system/$shortname || exit 1
+done
+
+for filename in /lib/systemd/system/greensense-*.service; do
+  [ -f "$filename" ] || break
+  shortname=$(basename $filename)
+  echo "Removing service: $filename" && \
   echo "" && \
   sudo sh $SYSTEMCTL_SCRIPT stop "$filename" && \
   sudo sh $SYSTEMCTL_SCRIPT disable "$filename" && \
   echo "" && \
-  rm $filename || exit 1
+  sudo rm -v $filename || exit 1
 done
 
 cd mobile/linearmqtt && \
