2a7944943e1a94a1032670dec06a7614abacee0e
John <compulsivecoder@gmail.com>
2018-05-03 04:27:15 +0000

    Fixed install platformio

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,11 +1,8 @@
-# NOTE: You may need to run this file as administrator. Example:
-# sudo sh install-platformio.sh
-
-
 if ! type "pio" > /dev/null; then
   sudo apt-get install -y python python-pip && \
 
   sudo pip install --upgrade pip && \
+  sudo pip install setuptools testresources && \
 
   sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
