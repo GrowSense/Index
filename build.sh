dacf88ba6a3f7a30ae94acf896ff604d0b3bcc23
John <compulsivecoder@gmail.com>
2018-05-03 01:58:30 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -2,20 +2,28 @@ echo "Building entire index"
 
 INDEX_DIR=$PWD
 
+echo "Building monitor" && \
 cd sketches/monitor/SoilMoistureSensorCalibratedSerial && \
 sh build-all.sh && \
 cd $INDEX_DIR && \
+echo "" && \
 
+echo "Building monitor ESP" && \
 cd sketches/monitor/SoilMoistureSensorCalibratedSerialESP && \
 sh build-all.sh && \
 cd $INDEX_DIR && \
+echo "" && \
 
+echo "Building irrigator" && \
 cd sketches/irrigator/SoilMoistureSensorCalibratedPump && \
 sh build-all.sh && \
 cd $INDEX_DIR && \
+echo "" && \
 
+echo "Building irrigator ESP" && \
 cd sketches/irrigator/SoilMoistureSensorCalibratedPumpESP && \
 sh build-all.sh && \
 cd $INDEX_DIR && \
+echo "" && \
 
 echo "Build index complete"
