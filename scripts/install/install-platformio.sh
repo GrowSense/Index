8480e7fab7a2eb2ff782c41402b9614deb08093b
John <compulsivecoder@gmail.com>
2018-05-03 04:21:36 +0000

    Fixing install platformio

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -5,8 +5,7 @@
 if ! type "pio" > /dev/null; then
   sudo apt-get install -y python python-pip && \
 
-  #pip install --upgrade pip && \
-  #pip install setuptools && \
+  sudo pip install --upgrade pip && \
 
   sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
