829ff9ada8b433b40cc2656f4199bf5cd496cb62
John <compulsivecoder@gmail.com>
2018-05-03 06:51:48 +0000

    Trying to fix install platformio

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -2,7 +2,7 @@ if ! type "pio" > /dev/null; then
   sudo apt-get install -y python python-pip && \
 
   pip install --upgrade pip && \
-  pip install setuptools testresources && \
+  pip install --user setuptools testresources pyOpenSSL ndg-httpsclient pyasn1 && \
 
   sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
