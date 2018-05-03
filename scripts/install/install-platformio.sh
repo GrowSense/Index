61082289bf3cff5bd53dd80a4568dc428b2516d4
John <compulsivecoder@gmail.com>
2018-05-03 07:00:14 +0000

    Trying to fix install platformio

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,10 +1,13 @@
 if ! type "pio" > /dev/null; then
+  mkdir -p ~/.platformio
+  sudo chown -R $USER ~/.platformio
+
   sudo apt-get install -y python python-pip && \
 
   pip install --upgrade pip && \
   pip install --user setuptools testresources pyOpenSSL ndg-httpsclient pyasn1 && \
 
-  sudo python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
+  python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
   echo "Platform.io is already installed. Skipping."
 fi
