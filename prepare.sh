f9abe1193152a7434dff15cfddeab6e790bbd07d
John <compulsivecoder@gmail.com>
2018-06-01 05:23:41 +0000

    Trying to fix prepare script

--- prepare.sh
+++ prepare.sh
@@ -6,10 +6,10 @@ sudo apt-get update && sudo apt-get -y install wget git zip unzip curl apt-trans
 
 cd scripts/install/ && \
 
+sh install-platformio.sh && \
 sudo sh install-jq.sh && \
 sudo sh install-systemd.sh && \
 sudo sh install-docker.sh && \
-sudo sh install-platformio.sh && \
 sudo sh install-mono.sh && \
 
 cd $DIR
