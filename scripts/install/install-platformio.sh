4143f8009307838ecc81233db61713ba453b10b6
John <compulsivecoder@gmail.com>
2018-06-01 05:18:47 +0000

    Added console output to install platformio script

--- scripts/install/install-platformio.sh
+++ scripts/install/install-platformio.sh
@@ -1,16 +1,27 @@
 # python
 if ! type "python" > /dev/null; then
+  echo "Installing python"
+
   sudo apt-get install -y python
 fi
 
 if ! type "pip" > /dev/null; then
+  echo "Installing python-pip"
+
   sudo apt-get install -y python-pip
 fi
 
+echo "Upgrading pip"
+
 pip install --upgrade pip
+
+echo "Installing pip setuptools"
+
 pip install --user setuptools
 
 if ! type "pio" > /dev/null; then
+  echo "Installing platformio"
+
   python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/master/scripts/get-platformio.py)"
 else
   echo "Platform.io is already installed. Skipping."
