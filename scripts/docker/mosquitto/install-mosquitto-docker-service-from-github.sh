59ed2f0b36e815a6b7421e8d2e656bab0bab66d6
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-13 08:45:02 +1000

    Updated install mosquitto from github script

--- scripts/docker/mosquitto/install-mosquitto-docker-service-from-github.sh
+++ scripts/docker/mosquitto/install-mosquitto-docker-service-from-github.sh
@@ -1,11 +1,43 @@
-# Install docker if not found
-if ! type "docker" > /dev/null; then
-  DOCKER_INSTALL_SCRIPT="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/install/install-docker.sh"
-  sudo wget -O - $DOCKER_INSTALL_SCRIPT | sh -s
+USERNAME=$1
+PASSWORD=$2
+
+ARGUMENTS_ERROR_MESSAGE="Please specify the MQTT username and password as arguments."
+
+if [ ! "$USERNAME" ]; then
+  echo $ARGUMENTS_ERROR_MESSAGE
+  exit 1 
 fi
 
-# Install jenkins service file
-SERVICE_FILE_URL="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/docker/mosquitto/greensense-mosquitto-docker.service"
+if [ ! "$PASSWORD" ]; then
+  echo $ARGUMENTS_ERROR_MESSAGE
+  exit 1 
+fi
+
+# Install docker if not found
+if ! type "docker" > /dev/null; then
+  echo "Installing docker"
+  
+  DOCKER_INSTALL_SCRIPT="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/install/install-docker.sh"
+  sudo wget -O - $DOCKER_INSTALL_SCRIPT | sh -s || exit 1
+fi
+
+echo "Creating mosquitto directory"
+
+MOSQUITTO_DIR="/usr/local/mosquitto"
+DATA_DIR="$MOSQUITTO_DIR/data"
+CREDENTIALS_FILE="$DATA_DIR/mosquitto.userfile"
+
+sudo mkdir -p $MOSQUITTO_DIR && \
+sudo mkdir -p $DATA_DIR && \
+sudo chmod 777 $DATA_DIR && \
+
+echo "Creating credentials file" && \
+
+echo "$USERNAME:$PASSWORD" > $CREDENTIALS_FILE && \
+
+echo "Install mosquitto service file"
+
+SERVICE_FILE_URL="https://raw.githubusercontent.com/GreenSense/Index/dev/scripts/docker/mosquitto/greensense-mosquitto-docker.service"
 
 sudo wget $SERVICE_FILE_URL -O /lib/systemd/system/greensense-mosquitto-docker.service
 
