88bdc41a2263b556b6ca033bb13cf917b7ce0669
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-15 11:40:56 +1000

    Updated push version script

--- push-version.sh
+++ push-version.sh
@@ -3,7 +3,7 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "dev" ];  then
-
+  git pull origin $BRANCH --quiet
   git commit buildnumber.txt -m "Updated version [ci skip]" && \
   git push origin $BRANCH --quiet
 else
