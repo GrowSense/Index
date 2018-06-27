828c496009ea77de550c73a65089509542f45919
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-27 11:56:05 +1000

    Added pull device info script

--- install-git-hooks.sh
+++ install-git-hooks.sh
@@ -1,5 +1,5 @@
 PRE_COMMIT_FILE=".git/hooks/pre-commit"
-COMMAND="test.sh && sh clean.sh"
+COMMAND="sh clean.sh"
 
 if [ -f $PRE_COMMIT_FILE ]; then
     cp "$PRE_COMMIT_FILE" "$PRE_COMMIT_FILE.bak"
