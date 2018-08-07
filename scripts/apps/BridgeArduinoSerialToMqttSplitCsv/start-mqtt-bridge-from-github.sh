bada1068c1df89dde9158911b859e9b4b5db19d2
CompulsiveCoder <compulsivecoder@gmail.com>
2018-08-07 02:18:18 +0000

    Updated submodules

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge-from-github.sh
+++
@@ -1,5 +0,0 @@
-
-INIT_SCRIPT_FILE_URL="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/apps/BridgeArduinoSerialToMqttSplitCsv/init.sh"
-wget -O - $INIT_SCRIPT_FILE_URL | sh -s || exit 1
-
-mono BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe $1 $2 $3 $4 $5
--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge.sh
+++
@@ -1,3 +0,0 @@
-sh init.sh
-
-mono BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe $1 $2 $3 $4 $5
