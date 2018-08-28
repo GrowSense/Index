97a5dedb53343b207c9b758d1f1257f19a0668f6
CompulsiveCoder <compulsivecoder@gmail.com>
2018-08-28 18:02:46 +1000

    Updated init scripts

--- init-submodules.sh
+++ init-submodules.sh
@@ -2,17 +2,25 @@ git submodule update --init
 
 DIR=$PWD
 
+echo "SoilMoistureSensorCalibratedSerial" && \
+
 cd sketches/monitor/SoilMoistureSensorCalibratedSerial/ && \
 sh init.sh && \
 cd $DIR && \
+
+echo "SoilMoistureSensorCalibratedSerialESP" && \
 
 cd sketches/monitor/SoilMoistureSensorCalibratedSerialESP/ && \
 sh init.sh && \
 cd $DIR && \
 
+echo "SoilMoistureSensorCalibratedPump" && \
+
 cd sketches/irrigator/SoilMoistureSensorCalibratedPump/ && \
 sh init.sh && \
 cd $DIR && \
+
+echo "SoilMoistureSensorCalibratedPumpESP" && \
 
 cd sketches/irrigator/SoilMoistureSensorCalibratedPumpESP/ && \
 sh init.sh && \
--- init.sh
+++ init.sh
@@ -4,18 +4,25 @@ sh update-submodules.sh
 
 DIR=$PWD
 
+echo "Index unit tests" && \
+
 cd tests/nunit/ && \
 sh init.sh && \
 cd $DIR && \
+
+echo "MQTT bridge" && \
 
 cd scripts/apps/BridgeArduinoSerialToMqttSplitCsv/ && \
 sh init.sh && \
 cd $DIR && \
 
+echo "Updater" && \
+
 cd scripts/apps/GitDeployer/ && \
 sh init.sh && \
 cd $DIR && \
 
+echo "Linear MQTT dashboard" && \
 cd mobile/linearmqtt/ && \
 sh init.sh && \
 cd $DIR && \
