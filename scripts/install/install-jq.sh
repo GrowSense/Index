f63439021047d12bf1db5e8f392443b0c5248b69
CompulsiveCoder <compulsivecoder@gmail.com>
2018-10-24 12:29:59 +1030

    Fixed install jq script

--- scripts/install/install-jq.sh
+++ scripts/install/install-jq.sh
@@ -9,4 +9,13 @@ if ! jq --version | grep '1.5'; then
   add-apt-repository -y ppa:eugenesan/ppa
   apt-get update
   apt-get -y install jq
+  apt-get -y upgrade
+fi
+
+if ! jq --version | grep '1.5'; then
+  echo "jq 1.5 still not found. Upgrading from source"
+  wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz
+  tar xzf jq-1.5.tar.gz
+  cd jq-1.5/
+  sudo ./configure && sudo make && sudo make install
 fi
