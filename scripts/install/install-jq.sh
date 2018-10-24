688e1510547bd84d344fe092beab328c5f2b03ac
CompulsiveCoder <compulsivecoder@gmail.com>
2018-10-24 12:18:24 +1030

    Fixed install jq script

--- scripts/install/install-jq.sh
+++ scripts/install/install-jq.sh
@@ -1,7 +1,7 @@
 if ! type "jq" > /dev/null; then
   apt-get -y install jq
 else
-  echo "jq is already installed. Skipping."
+  echo "jq is already installed. Skipping install."
 fi
 
 if ! jq --version | grep '1.5'; then
