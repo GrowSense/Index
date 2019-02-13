e126265ef826f96118cdd8ed87bae51d77916028
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-13 12:21:08 +0000

    Fixed mqtt bridge version

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/init.sh
+++ scripts/apps/BridgeArduinoSerialToMqttSplitCsv/init.sh
@@ -22,7 +22,7 @@ fi
   wget -O install-package.sh $INSTALL_SCRIPT_FILE_URL
 #fi
 
-sh install-package.sh BridgeArduinoSerialToMqttSplitCsv 1.0.1.2 || exit 1
+sh install-package.sh BridgeArduinoSerialToMqttSplitCsv 1.0.1.3 || exit 1
 
 echo "Installation complete. Launching bridge."
 
