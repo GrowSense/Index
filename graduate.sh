4e2e9e63bb880e77b084a540598054afdbc743dc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-20 03:50:45 +0000

    Updated/fixed the graduate script

--- graduate.sh
+++ graduate.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [ "$BRANCH" = "dev" ]
-then
-  echo "Graduating to master branch"
+if [ "$BRANCH" = "dev" ];  then
+  echo "Graduating dev branch to master branch"
+
+  # Fetch other branches
+  git fetch origin --quiet && \
+
+  # Pull the master branch into the dev branch
+  git pull origin master --quiet && \
 
   # Checkout the master branch
   git checkout master && \
 
   # Ensure it's up to date
-  git pull origin master && \
+  git pull origin master --quiet && \
 
   # Merge the dev branch
-  git merge dev && \
+  git merge -q dev && \
 
   # Push the updates
-  git push origin master && \
+  git push origin master --quiet && \
 
   # Go back to the dev branch
   git checkout dev && \
@@ -24,5 +30,5 @@ then
 
   (echo "Error" && exit 1)
 else
-  echo "You must be in the 'dev' branch to graduate to the 'master' branch."
+  echo "You must be in the 'dev' branch to graduate to the 'master' branch, but currently in the '$BRANCH' branch. Skipping."
 fi
