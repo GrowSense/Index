905dba53e65f5d6cbc19e3555fdbbe1523f488e1
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-20 02:12:39 +0000

    Cleaned up clean script

--- clean.sh
+++ clean.sh
@@ -4,19 +4,26 @@ echo ""
 echo "Cleaning project"
 echo ""
 
+# Remove all garden devices and services
 sh remove-garden-devices.sh
 
-TMP_DIR="$PWD.tmp";
-
-echo $TMP_DIR
-
-if [ -d "$TMP_DIR" ]; then
-  sudo rm "$TMP_DIR/" -r
-fi
-
+# Clean linear MQTT dashboard settings
 cd mobile/linearmqtt/
 sh reset.sh
-
 cd $DIR
+
+# Clean mock directory
+MOCK_DIR="mock";
+echo "Mock dir: $MOCK_DIR"
+if [ -d "$MOCK_DIR" ]; then
+  rm "$MOCK_DIR/" -r
+fi
+
+# Clean test .tmp directory
+TMP_DIR="$PWD.tmp";
+echo "Temp dir: $TMP_DIR"
+if [ -d "$TMP_DIR" ]; then
+  rm "$TMP_DIR/" -r
+fi
 
 echo "Finished cleaning project"
