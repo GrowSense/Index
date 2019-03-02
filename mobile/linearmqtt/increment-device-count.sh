58ed516d0523935275e7051b915bba167abaf5eb
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-02 23:21:21 +0000

    Fixed issues with increment-device-count script

--- mobile/linearmqtt/increment-device-count.sh
+++ mobile/linearmqtt/increment-device-count.sh
@@ -2,7 +2,14 @@
 
 echo "Incrementing device count"
 
-DEVICE_COUNT=$(cat devicecount.txt)
+DEVICE_COUNT=0
+
+if [ -f "devicecount.txt" ]; then
+  echo "Getting device count from: devicecount.txt"
+  DEVICE_COUNT=$(cat devicecount.txt)
+else
+  echo "No 'devicecount.txt' file found. Starting at 0."
+fi
 
 echo "Current count: $DEVICE_COUNT"
 
