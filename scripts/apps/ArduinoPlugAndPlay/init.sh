ce258a146c05708d2f071ec10734a39291a0c8d7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-16 02:53:56 +0000

    Fixed script

--- scripts/apps/ArduinoPlugAndPlay/init.sh
+++ scripts/apps/ArduinoPlugAndPlay/init.sh
@@ -18,10 +18,10 @@ if [ -f $CONFIG_FILE ]; then
 fi
 
 # TODO: Clean up. This check is disabled to allow the install package script to be overwritten
-#if [ ! -f "install-package.sh" ]; then
+if [ ! -f "install-package.sh" ]; then
   INSTALL_SCRIPT_FILE_URL="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/apps/ArduinoPlugAndPlay/install-package.sh"
   wget --no-cache -O install-package.sh $INSTALL_SCRIPT_FILE_URL
-#fi
+fi
 
 sh install-package.sh ArduinoPlugAndPlay 1.0.0.1 || exit 1
 
--- scripts/apps/ArduinoPlugAndPlay/install-package.sh
+++ scripts/apps/ArduinoPlugAndPlay/install-package.sh
@@ -19,21 +19,10 @@ PACKAGE_FILE_EXT="$PACKAGE_NAME.$PACKAGE_VERSION.nupkg"
 
 echo "Package file: $PACKAGE_FILE"
 
-NUGET_INSTALL_ERROR=0
-
 if [ ! -d "$PACKAGE_FILE" ]; then
+	wget -q "https://github.com/GreenSense/libs/raw/master/$PACKAGE_FILE.nupkg" -O $PACKAGE_FILE_EXT
 
-	# Nuget is disabled because it's slower than direct download and is not currently required
-	#mono nuget.exe install $PACKAGE_NAME -version $PACKAGE_VERSION || NUGET_INSTALL_ERROR=1
-
-	# Force direct download
-	NUGET_INSTALL_ERROR=1
-
-	if [ $NUGET_INSTALL_ERROR=1 ]; then
-		wget -q "https://github.com/GreenSense/libs/raw/master/$PACKAGE_FILE.nupkg" -O $PACKAGE_FILE_EXT
-
-		unzip -o "$PACKAGE_FILE_EXT" -d "$PACKAGE_NAME/"
-	fi
+	unzip -o "$PACKAGE_FILE_EXT" -d "$PACKAGE_NAME/"
 else
 	echo "$PACKAGE_FILE library already exists. Skipping download."
 fi
