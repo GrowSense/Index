51b6bb559bd00fcd20576e80119f79fe32ce002f
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-17 05:58:38 +0000

    Fixed sudo issues

--- systemctl.sh
+++ systemctl.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+
 MOCK_FLAG_FILE="is-mock-systemctl.txt"
 
 if [ ! -f $MOCK_FLAG_FILE ]; then
-    sudo systemctl $1 $2 $3
+  SUDO=""
+  if [ "$(id -u)" -eq 0 ]; then
+      SUDO='sudo'
+  fi
+  echo $SUDO
+
+  $SUDO systemctl $1 $2 $3
 else
-    echo "[mock] sudo systemctl $1 $2 $3"
+  echo "[mock] sudo systemctl $1 $2 $3"
 fi
