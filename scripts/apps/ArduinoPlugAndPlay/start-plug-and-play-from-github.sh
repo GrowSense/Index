dfdcc49fe8a322e3d9e55fcd23dac705445be900
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-17 03:05:25 +0000

    Updated script

--- scripts/apps/ArduinoPlugAndPlay/start-plug-and-play-from-github.sh
+++ scripts/apps/ArduinoPlugAndPlay/start-plug-and-play-from-github.sh
@@ -4,4 +4,9 @@ wget --no-cache -O init.sh $INIT_SCRIPT_FILE_URL
 
 sh init.sh || exit 1
 
-mono ArduinoPlugAndPlay/lib/net40/ArduinoPlugAndPlay.exe $1 $2 $3 $4 $5 $6 $7 $8 $9
+START_SCRIPT_FILE_URL="https://raw.githubusercontent.com/GreenSense/Index/master/scripts/apps/ArduinoPlugAndPlay/start-plug-and-play.sh"
+wget --no-cache -O init.sh $START_SCRIPT_FILE_URL
+
+sh init.sh || exit 1
+
+sh start-plug-and-play.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 || exit 1
