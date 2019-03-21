86ccc461b9ee4a973a6d10a62e36747863cd406e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-21 06:20:46 +0000

    Fixed script

--- expose-ui-config-via-http.sh
+++ expose-ui-config-via-http.sh
@@ -1,7 +1,13 @@
 DIR=$PWD
 
-cd scripts/docker/nginx
+# Only launch it if docker is enabled
+if [ ! -f "is-mock-docker.txt" ]; then
 
-sh start-linear-mqtt-config-http.sh
+  cd scripts/docker/nginx
 
-cd $DIR
+  sh start-linear-mqtt-config-http.sh
+
+  cd $DIR
+else
+  echo "[mock] sh start-linear-mqtt-config-http.sh"
+fi
