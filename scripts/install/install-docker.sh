88b4e415a65001f0cfc7935a36019aff86c6544b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-26 00:32:01 +0000

    Cleaned up install docker script

--- scripts/install/install-docker.sh
+++ scripts/install/install-docker.sh
@@ -3,14 +3,12 @@ MOCK_FLAG_FILE="../../is-mock-docker.txt"
 if ! type "docker" > /dev/null; then
   if [ ! -f "$MOCK_FLAG_FILE" ]; then
     curl https://get.docker.com/ | sh -s
+    
+    usermod -aG docker $USER
   else
     echo "Is mock docker. Skipping docker install."
   fi
 else
   echo "Docker is already installed. Skipping."
-fi
-
-if [ ! -f "$MOCK_FLAG_FILE" ]; then
-  usermod -aG docker $USER
 fi
 
