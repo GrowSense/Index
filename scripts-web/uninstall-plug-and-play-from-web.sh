d9acf6cc1d9df4842e762d298a64102457773fd6
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-26 01:26:50 +0000

    Added mqtt bridge and updater/git deploy removal to uninstall script

--- scripts-web/uninstall-plug-and-play-from-web.sh
+++ scripts-web/uninstall-plug-and-play-from-web.sh
@@ -36,4 +36,12 @@ cd $PNP_INSTALL_DIR
 echo "Uninstalling the plug and play application (by downloading uninstall-from-web.sh file)..."
 wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/master/scripts-web/uninstall-from-web.sh | bash -s - $BRANCH || (echo "Failed to uninstall ArduinoPlugAndPlay. Script: uninstall-from-web.sh" && exit 1)
 
+cd ..
+
+echo "Removing MQTT bridge install directory..."
+rm mqtt-bridge -R || (echo "Failed to remove MQTT bridge install directory." && exit 1)
+
+echo "Removing updater (git deployer) install directory..."
+rm git-deployer -R || (echo "Failed to remove updater (git deployer) install directory." && exit 1)
+
 echo "Finished uninstalling GreenSense plug and play!"
