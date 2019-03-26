880a3a46c318587ea0d1d1cd4d480ed6d6502ff9
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-26 22:35:25 +0000

    Updated scripts

--- scripts-web/uninstall-plug-and-play-from-web.sh
+++ scripts-web/uninstall-plug-and-play-from-web.sh
@@ -5,11 +5,14 @@ EXAMPLE_COMMAND="Example:\n..sh"
 PNP_INSTALL_DIR="/usr/local/ArduinoPlugAndPlay"
 
 echo "Checking for ArduinoPlugAndPlay install dir..."
-if [ ! -d $PNP_INSTALL_DIR ]; then
-  echo "ArduinoPlugAndPlay doesn't appear to be installed at:"
+if [ -d $PNP_INSTALL_DIR ]; then
+  cd $PNP_INSTALL_DIR
+
+  echo "Uninstalling the plug and play application (by downloading uninstall-from-web.sh file)..."
+  wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/master/scripts-web/uninstall-from-web.sh | bash -s - $BRANCH || (echo "Failed to uninstall ArduinoPlugAndPlay. Script: uninstall-from-web.sh" && exit 1)
+else
+  echo "ArduinoPlugAndPlay doesn't seem to be installed. Skipping."
   echo "  $PNP_INSTALL_DIR"
-  echo "Aborting uninstall."
-  exit 1
 fi
 
 INDEX_DIR="/usr/local/GreenSense/Index"
@@ -20,28 +23,26 @@ if [ ! -d $INDEX_DIR ]; then
   echo "  $INDEX_DIR"
   echo "Aborting uninstall."
   exit 1
+else
+  echo "Moving to GreenSense index dir..."
+  cd $INDEX_DIR
+
+  echo "Removing all devices and services..."
+  sh remove-garden-devices.sh || (echo "Failed to remove garden devices." && exit 1)
+
+  echo "Removing all devices and services..."
+  sh remove-garden-devices.sh || (echo "Failed to remove garden devices." && exit 1)
+
+  cd "/usr/local/"
+
+  echo "Removing MQTT bridge install directory..."
+  rm mqtt-bridge -R || (echo "Failed to remove MQTT bridge install directory." && exit 1)
+
+  echo "Removing updater (git deployer) install directory..."
+  rm git-deployer -R || (echo "Failed to remove updater (git deployer) install directory." && exit 1)
+
+  rm "/usr/local/GreenSense"  -R || (echo "Failed to remove GreenSense index directory." && exit 1)
+  
+  echo "Finished uninstalling GreenSense plug and play!"
 fi
 
-echo "Moving to GreenSense index dir..."
-cd $INDEX_DIR
-
-echo "Removing all devices and services..."
-sh remove-garden-devices.sh || (echo "Failed to remove garden devices." && exit 1)
-
-echo "Removing all devices and services..."
-sh remove-garden-devices.sh || (echo "Failed to remove garden devices." && exit 1)
-
-cd $PNP_INSTALL_DIR
-
-echo "Uninstalling the plug and play application (by downloading uninstall-from-web.sh file)..."
-wget -v --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/master/scripts-web/uninstall-from-web.sh | bash -s - $BRANCH || (echo "Failed to uninstall ArduinoPlugAndPlay. Script: uninstall-from-web.sh" && exit 1)
-
-cd ..
-
-echo "Removing MQTT bridge install directory..."
-rm mqtt-bridge -R || (echo "Failed to remove MQTT bridge install directory." && exit 1)
-
-echo "Removing updater (git deployer) install directory..."
-rm git-deployer -R || (echo "Failed to remove updater (git deployer) install directory." && exit 1)
-
-echo "Finished uninstalling GreenSense plug and play!"
