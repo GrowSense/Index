ef1d8628bdcfeadcc41533c4331f2e568b55fbb5
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-27 00:49:34 +0000

    Updated scripts and mqtt bridge version

--- scripts/apps/GitDeployer/start-git-deployer.sh
+++ scripts/apps/GitDeployer/start-git-deployer.sh
@@ -2,4 +2,4 @@ sh init.sh
 
 echo "Launching deployer."
 
-mono GitDeployer/lib/net40/GitDeployer.exe $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+mono GitDeployer/lib/net40/GitDeployer.exe $1 $2 $3 $4 $5 $6 $7 $8 $9
