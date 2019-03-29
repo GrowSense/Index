a007d48a2f76c8fe24d647ccaa737f7e6bcbd1de
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-29 00:25:49 +0000

    Tweaked supervise script

--- supervise.sh
+++ supervise.sh
@@ -1,8 +1,8 @@
 echo "-----"
 echo "Supervising GreenSense system..."
 
-sh supervise-devices.sh || (echo "Supervise garden devices failed." && exit 1)
-
 sh upgrade.sh || (echo "Upgrade failed." && exit 1)
+
+sh supervise-devices.sh || (echo "Supervise garden devices failed." && exit 1)
 
 echo "-----"
