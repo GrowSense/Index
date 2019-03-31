6222906c376a147bddfb17a15c2e14ecdd46a404
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-31 07:11:31 +0000

    Fixing ui

--- create-ui-controller-1602-service.sh
+++ create-ui-controller-1602-service.sh
@@ -13,11 +13,10 @@ if [ ! $DEVICE_PORT ]; then
     exit 1
 fi
 
-echo "Device type: $DEVICE_TYPE"
 echo "Device name: $DEVICE_NAME"
 echo "Device port: $DEVICE_PORT"
 
-SERVICE_EXAMPLE_FILE="greensense-ui-1602.service.example"
+SERVICE_EXAMPLE_FILE="greensense-ui-1602.service.template"
 SERVICE_FILE="greensense-ui-1602-$DEVICE_NAME.service"
 SERVICE_PATH="scripts/apps/Serial1602ShieldSystemUIController/svc"
 SERVICE_FILE_PATH="$SERVICE_PATH/$SERVICE_FILE"
@@ -30,11 +29,25 @@ echo "$SERVICE_FILE_PATH"
 
 echo "Copying service file..."
 
+#DEVICES_DIR="$PWD/devices"
+
+#INSTALL_DIR="/usr/local/Serial1602ShieldSystemUIController/"
+
+# If install into the system
+#if [ "$PWD" == "/usr/local/GreenSense/Index" ]; then
+#  DEVICES_DIR="/usr/local/GreenSense/Index/devices"
+#fi
+
+#echo "Devices dir:"
+#echo "  $DEVICES_DIR"
+#echo "UI controller install dir:"
+#echo "  $INSTALL_DIR"
+
 cp $SERVICE_EXAMPLE_FILE_PATH $SERVICE_FILE_PATH && \
 
 echo "Editing service file..."
-sed -i "s/Interface1/$DEVICE_NAME/g" $SERVICE_FILE_PATH && \
-sed -i "s/ttyUSB[0-9]/$DEVICE_PORT/g" $SERVICE_FILE_PATH && \
+sed -i "s/{DEVICE_NAME}/$DEVICE_NAME/g" $SERVICE_FILE_PATH && \
+sed -i "s/{PORT}/$DEVICE_PORT/g" $SERVICE_FILE_PATH && \
 
 echo "Installing service..."
 sh install-service.sh $SERVICE_FILE_PATH && \
--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -4,6 +4,6 @@ echo "  Dir: $PWD"
 sh install-package.sh NUnit 2.6.4
 sh install-package.sh NUnit.Runners 2.6.4
 sh install-package.sh Newtonsoft.Json 11.0.2
-sh install-package.sh ArduinoPlugAndPlay 1.0.0.89
+sh install-package.sh ArduinoPlugAndPlay 1.0.0.94
 
 echo "Finished getting library files."
