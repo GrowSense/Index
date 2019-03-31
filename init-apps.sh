ecb8bcf3236f179c5ca3dd9bc9cc5e395d66d7cd
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-31 07:31:57 +0000

    Updated init

--- init-apps.sh
+++ init-apps.sh
@@ -7,12 +7,19 @@ cd scripts/apps/BridgeArduinoSerialToMqttSplitCsv/ && \
 sh init.sh || (echo "Failed to initialize MQTT bridge." && exit 1)
 cd $DIR
 
-echo && \
-echo "Initializing updater (GitDeployer) utility" && \
+echo ""
+echo "Initializing UI controller"
 
-cd scripts/apps/GitDeployer/ && \
-sh init.sh || (echo "Failed to initialize updater (GitDeployer)." && exit 1)
+cd scripts/apps/Serial1602ShieldSystemUIController/ && \
+sh init.sh || (echo "Failed to initialize UI controller." && exit 1)
 cd $DIR
+
+#echo && \
+#echo "Initializing updater (GitDeployer) utility" && \
+
+#cd scripts/apps/GitDeployer/ && \
+#sh init.sh || (echo "Failed to initialize updater (GitDeployer)." && exit 1)
+#cd $DIR
 
 echo "" && \
 echo "Initializing linear MQTT dashboard UI related scripts" && \
