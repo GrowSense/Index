9a9ca76fd763f9d06f7c4d3f5946083476d06443
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-02 06:56:40 +0000

    Added check deployment

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,4 @@ echo "Installing GreenSense plug and play to remote computer..."
 
 sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "wget --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH_NAME/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH_NAME ? $WIFI_NAME $WIFI_PASSWORD $MQTT_HOST $MQTT_USERNAME $MQTT_PASSWORD $MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS" 
 
-echo "Viewing installation status on remote computer..."
-
-sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status arduino-plug-and-play.service"
+echo "Finished deployment."
