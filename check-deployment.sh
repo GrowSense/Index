98b54575ea78ae538fc7992fb99cbbc62b963d1b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-05 22:08:24 +0000

    Fixed check deployment and jenkinsfile

--- check-deployment.sh
+++ check-deployment.sh
@@ -1,5 +1,23 @@
 echo "Checking status of deployment..."
 
+
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ $BRANCH == "master" ]; then
+  echo "Master deployment on live garden..."
+  INSTALL_HOST=$MASTER_INSTALL_HOST
+  INSTALL_SSH_USERNAME=$MASTER_INSTALL_SSH_USERNAME
+  INSTALL_SSH_PASSWORD=$MASTER_INSTALL_SSH_PASSWORD
+  INSTALL_SSH_PORT=$MASTER_INSTALL_SSH_PORT
+fi
+if [ $BRANCH == "dev" ]; then
+  echo "Dev deployment on staging garden..."
+  INSTALL_HOST=$DEV_INSTALL_HOST
+  INSTALL_SSH_USERNAME=$DEV_INSTALL_SSH_USERNAME
+  INSTALL_SSH_PASSWORD=$DEV_INSTALL_SSH_PASSWORD
+  INSTALL_SSH_PORT=$DEV_INSTALL_SSH_PORT
+fi
+
 echo "Viewing arduino plug and play service status..."
 
 sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status arduino-plug-and-play.service"
