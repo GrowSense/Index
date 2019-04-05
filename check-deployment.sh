0364498ec9d5ff233fccac711e187a03bd701ae0
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-05 06:07:39 +0000

    Tweak

--- check-deployment.sh
+++ check-deployment.sh
@@ -1,7 +1,5 @@
 echo "Checking status of deployment..."
 
-sleep 15s
-
 echo "Viewing arduino plug and play service status..."
 
 sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status arduino-plug-and-play.service"
