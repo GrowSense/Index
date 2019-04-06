e2aff62fcef750dc6c812c20812ff9fa4aec7514
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-06 22:03:33 +0000

    Changed deploy master to a complete uninstall and complete reinstall

--- check-deployment.sh
+++ check-deployment.sh
@@ -23,7 +23,7 @@ echo "Viewing arduino plug and play service status..."
 
 PNP_RESULT=$(sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status arduino-plug-and-play.service" || (echo "Error attempting to view arduino plug and play status." && exit 1))
 
-echo $PNP_RESULT
+echo ${PNP_RESULT}
 
 [[ ! $(echo $PNP_RESULT) =~ "Loaded: loaded" ]] && (echo "Arduino Plug and Play service isn't loaded" && exit 1)
 [[ ! $(echo $PNP_RESULT) =~ "Active: active" ]] && (echo "Arduino Plug and Play service isn't active" && exit 1)
@@ -33,7 +33,7 @@ echo "Viewing GreenSense supervisor service status..."
 
 SUPERVISOR_RESULT=$(sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status greensense-supervisor.service" || (echo "Error attempting to view garden supervisor status." && exit 1))
 
-echo $SUPERVISOR_RESULT
+echo ${SUPERVISOR_RESULT}
 
 [[ ! $(echo $SUPERVISOR_RESULT) =~ "Loaded: loaded" ]] && (echo "GreenSense supervisor service isn't loaded" && exit 1)
 [[ ! $(echo $SUPERVISOR_RESULT) =~ "Active: active" ]] && (echo "GreenSense supervisor service isn't active" && exit 1)
@@ -43,7 +43,7 @@ echo "Viewing GreenSense UI controller service status..."
 
 UI_CONTOLLER_RESULT=$(sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "systemctl status greensense-ui-1602-ui1.service" || (echo "Error attempting to view UI controller status." && exit 1))
 
-echo $UI_CONTROLLER_RESULT
+echo ${UI_CONTROLLER_RESULT}
 
 [[ ! $(echo $UI_CONTOLLER_RESULT) =~ "Loaded: loaded" ]] && echo "The UI controller service isn't loaded" && exit 1
 [[ ! $(echo $UI_CONTOLLER_RESULT) =~ "Active: active" ]] && echo "The UI controller service isn't active" && exit 1
