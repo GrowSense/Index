fb61b018e1263beb339a93070c4bab8d706b30ce
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-06 22:44:35 +0000

    Fixed check garden scripts

--- check-garden-device.sh
+++ check-garden-device.sh
@@ -5,41 +5,85 @@ if [ ! $DEVICE_NAME ]; then
   exit 1
 fi
 
-OUTPUT=$(sh view-mqtt-bridge-status.sh $DEVICE_NAME)
+DEVICE_GROUP=$(cat "devices/$DEVICE_NAME/group.txt")
 
-RESULT="NotSet"
-SUB_RESULT=""
+if [ "$DEVICE_GROUP" = "ui" ]; then
 
-case "$OUTPUT" in 
-  *inactive*)
-    RESULT="Inactive"
-    ;;
-  *active*)
-    RESULT="Active"
-    ;;
-  *failed*)
-    RESULT="Failed"
-    ;;
-esac
+  OUTPUT=$(sh view-ui-controller-1602-status.sh $DEVICE_NAME)
 
-case "$OUTPUT" in 
-  *MqttConnectionException*)
-    SUB_RESULT="Failed to connect to broker"
-    ;;
-  *"An error occurred while retrieving package metadata"*)
-    SUB_RESULT="Nuget connection error"
-    ;;
-  *"No such file or directory"*)
-    SUB_RESULT="USB device not found"
-    ;;
-esac
+  RESULT="NotSet"
+  SUB_RESULT=""
 
-#echo $OUTPUT
+  case "$OUTPUT" in 
+    *inactive*)
+      RESULT="Inactive"
+      ;;
+    *active*)
+      RESULT="Active"
+      ;;
+    *failed*)
+      RESULT="Failed"
+      ;;
+  esac
 
-echo "  MQTT Bridge Service: $RESULT"
+  # TODO: Reimplement if possible
+  #case "$OUTPUT" in 
+  #  *MqttConnectionException*)
+  #    SUB_RESULT="Failed to connect to broker"
+  #    ;;
+  #  *"An error occurred while retrieving package metadata"*)
+  #    SUB_RESULT="Nuget connection error"
+  #    ;;
+  #  *"No such file or directory"*)
+  #    SUB_RESULT="USB device not found"
+  #    ;;
+  #esac
 
-if [ "$SUB_RESULT" ]; then
-  echo "    $SUB_RESULT"
+  #echo $OUTPUT
+
+  echo "  UI Controller Service: $RESULT"
+
+  if [ "$SUB_RESULT" ]; then
+    echo "    $SUB_RESULT"
+  fi
+else
+
+  OUTPUT=$(sh view-mqtt-bridge-status.sh $DEVICE_NAME)
+
+  RESULT="NotSet"
+  SUB_RESULT=""
+
+  case "$OUTPUT" in 
+    *inactive*)
+      RESULT="Inactive"
+      ;;
+    *active*)
+      RESULT="Active"
+      ;;
+    *failed*)
+      RESULT="Failed"
+      ;;
+  esac
+
+  case "$OUTPUT" in 
+    *MqttConnectionException*)
+      SUB_RESULT="Failed to connect to broker"
+      ;;
+    *"An error occurred while retrieving package metadata"*)
+      SUB_RESULT="Nuget connection error"
+      ;;
+    *"No such file or directory"*)
+      SUB_RESULT="USB device not found"
+      ;;
+  esac
+
+  #echo $OUTPUT
+
+  echo "  MQTT Bridge Service: $RESULT"
+
+  if [ "$SUB_RESULT" ]; then
+    echo "    $SUB_RESULT"
+  fi
+  
+  sh check-garden-device-mqtt.sh $DEVICE_NAME
 fi
-
-sh check-garden-device-mqtt.sh $DEVICE_NAME
---
+++ view-ui-controller-1602-status.sh
@@ -0,0 +1,7 @@
+DEVICE_NAME=$1
+
+if [ ! $DEVICE_NAME ]; then
+  echo "Error: Please provide a device name as parameter"
+else
+  sh systemctl.sh status greensense-ui-1602-$DEVICE_NAME.service
+fi
