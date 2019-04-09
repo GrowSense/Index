5a7ba7b6eb4146458d109f3190ed465670563ecb
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-10 08:21:43 +1000

    Updated UI controller

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -13,6 +13,8 @@ if [ "$BRANCH" = "dev" ]; then
 
   sshpass -p $DEV_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $DEV_INSTALL_SSH_USERNAME@$DEV_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH_NAME/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH_NAME ? $WIFI_NAME $WIFI_PASSWORD $DEV_MQTT_HOST $DEV_MQTT_USERNAME $DEV_MQTT_PASSWORD $DEV_MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS" || exit 1
   
+  echo "Giving the existing installation time to start..."
+  sleep 10
   
   echo "Updating GreenSense plug and play on remote computer..."
   
