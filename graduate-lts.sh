d2509eaf93a69542c58fe1d4efff23b5f878da24
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-12 15:00:40 +1000

    Fixed graduate lts script

--- graduate-lts.sh
+++ graduate-lts.sh
@@ -2,16 +2,16 @@
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-#if [ "$BRANCH" = "dev" ];  then
-#  git checkout master
-#fi
+if [ "$BRANCH" = "dev" ];  then
+  git checkout master || exit 1
+fi
 
 echo "Graduating master branch to lts branch"
 
 # Fetch other branches
 git fetch origin --quiet && \
 
-# Pull the lts branch into the dev branch
+# Pull the lts branch into the master branch
 git pull origin lts --quiet && \
 
 # Checkout the lts branch
@@ -20,13 +20,13 @@ git checkout lts && \
 # Ensure it's up to date
 git pull origin lts --quiet && \
 
-# Merge the dev branch
+# Merge the master branch
 git merge -q master && \
 
 # Push the updates
 git push origin lts --quiet && \
 
-# Go back to the original branch
-git checkout $BRANCH && \
+# Go back to the master branch
+git checkout master && \
 
 echo "The 'master' branch has been graduated to the 'lts' branch"
