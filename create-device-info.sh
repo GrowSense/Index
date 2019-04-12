048d2b07baae26229d03bab34ee9e72e5300de2e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-12 10:55:42 +1000

    Added device host to device info so that when device info is moved its possible to tell which host it came from

--- create-device-info.sh
+++ create-device-info.sh
@@ -5,6 +5,7 @@ DEVICE_PROJECT=$3
 DEVICE_LABEL=$4
 DEVICE_NAME=$5
 DEVICE_PORT=$6
+DEVICE_HOST=$7
 
 
 EXAMPLE_TEXT="Example: sh create-device-info.sh uno monitor SoilMoistureSensorCalibratedSerial Monitor1 monitor1 ttyUSB0"
@@ -45,12 +46,17 @@ if [ ! $DEVICE_PORT ]; then
   exit 1
 fi
 
+if [ ! $DEVICE_HOST ]; then
+  DEVICE_HOST=$(cat /etc/hostname)
+fi
+
 echo "Device board: $DEVICE_BOARD"
 echo "Device group: $DEVICE_GROUP"
 echo "Device type: $DEVICE_PROJECT"
 echo "Device label: $DEVICE_LABEL"
 echo "Device name: $DEVICE_NAME"
 echo "Device port: $DEVICE_PORT"
+echo "Device host: $DEVICE_HOST"
 
 DEVICE_FAMILY="GreenSense"
 
@@ -69,5 +75,6 @@ echo $DEVICE_NAME > $DEVICE_DIR/name.txt
 echo $DEVICE_PORT > $DEVICE_DIR/port.txt
 echo $DEVICE_GROUP > $DEVICE_DIR/group.txt
 echo $DEVICE_FAMILY > $DEVICE_DIR/family.txt
+echo $DEVICE_HOST > $DEVICE_DIR/host.txt
 
 echo "Device info created"
