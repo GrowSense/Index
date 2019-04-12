84c1493ca87e6b96b488907abae2637c1d1f850a
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-12 13:52:08 +1000

    Trying to fix tests

--- pull-device-info-from-remotes.sh
+++ pull-device-info-from-remotes.sh
@@ -1,9 +1,13 @@
 echo "Pulling device info from remote indexes..."
 
-for REMOTE_DIR in remote/*; do
-  REMOTE_NAME=$(cat "$REMOTE_DIR/name.txt")
-  
-  echo "Remote name: $REMOTE_NAME"
+if [ -d "remote" ]; then
+  for REMOTE_DIR in remote/*; do
+    REMOTE_NAME=$(cat "$REMOTE_DIR/name.txt")
+    
+    echo "Remote name: $REMOTE_NAME"
 
-  sh pull-device-info-from-remote.sh $REMOTE_NAME
-done
+    sh pull-device-info-from-remote.sh $REMOTE_NAME
+  done
+fi
+
+echo "Finished"
