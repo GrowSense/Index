07b21da99859c9648465ad7bb24706a5966994b5
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-13 10:37:05 +1000

    Added deploy LTS to jenkinsfile

---
+++ deploy-lts.sh
@@ -0,0 +1,28 @@
+
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ "$BRANCH" = "lts" ]; then
+
+  echo "Deploying lts branch..."
+  echo "Host: $LTS_INSTALL_HOST"
+
+  echo "Uninstalling GreenSense plug and play on remote computer..."
+  echo "Host: $LTS_INSTALL_HOST"
+
+  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH_NAME/scripts-web/uninstall-plug-and-play-from-web.sh | bash -s -- $BRANCH_NAME" || exit 1
+
+  echo "Installing GreenSense plug and play on remote computer..."
+  echo "Host: $LTS_INSTALL_HOST"
+
+  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH_NAME/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH_NAME ? $WIFI_NAME $WIFI_PASSWORD $LTS_MQTT_HOST $LTS_MQTT_USERNAME $LTS_MQTT_PASSWORD $LTS_MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS"  || exit 1
+
+  echo "Giving services time to start..."
+  sleep 30
+  
+  echo "Checking deployment..."
+  bash check-deployment.sh || exit 1
+
+  echo "Finished deployment."
+else
+  echo "You're not in the lts branch. Skipping lts deployment."
+fi
