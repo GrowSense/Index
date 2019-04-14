7b9545c28f4f999ca8be60749cf6d726fc815973
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-15 05:03:38 +1000

    Fixed check gaden devices scripts

--- check-garden-devices.sh
+++ check-garden-devices.sh
@@ -12,12 +12,14 @@ if [ -d "$DEVICES_DIR" ]; then
         DEVICE_NAME=$(cat $d/name.txt)
         DEVICE_LABEL=$(cat $d/label.txt)
         DEVICE_PORT=$(cat $d/port.txt)
+        DEVICE_HOST=$(cat $d/host.txt)
         
         
         echo "$DEVICE_LABEL"
         echo "  Name: $DEVICE_NAME"
         echo "  Group: $DEVICE_GROUP"
         echo "  Port: $DEVICE_PORT"
+        echo "  Host: $DEVICE_HOST"
         
         sh check-garden-device.sh $DEVICE_NAME
        
