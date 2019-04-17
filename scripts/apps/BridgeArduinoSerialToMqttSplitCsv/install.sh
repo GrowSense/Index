f956ccdc116c054b2dc66d608c29a44ea6e9e74f
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-18 09:04:44 +1000

    Tweaked install mqtt bridge script

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/install.sh
+++ scripts/apps/BridgeArduinoSerialToMqttSplitCsv/install.sh
@@ -9,6 +9,7 @@ mkdir -p $INSTALL_DIR
 
 cp -v -r BridgeArduinoSerialToMqttSplitCsv/ $INSTALL_DIR/BridgeArduinoSerialToMqttSplitCsv
 cp -v BridgeArduinoSerialToMqttSplitCsv*.nupkg $INSTALL_DIR/
+cp -v *.config $INSTALL_DIR/
 cp -v init.sh $INSTALL_DIR/
 cp -v install-package.sh $INSTALL_DIR/
 cp -v start-mqtt-bridge-from-github.sh $INSTALL_DIR/
