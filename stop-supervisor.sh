fad01ff364f3f971406c25584251f6926e8a63a0
CompulsiveCoder <compulsivecoder@gmail.com>
2019-04-18 12:51:58 +1000

    Trying to fix tests

--- stop-supervisor.sh
+++ stop-supervisor.sh
@@ -1,3 +1,3 @@
 echo "Stopping GreenSense supervisor service..."
 
-sh systemctl.sh stop greensense-supervisor.service
+sh systemctl.sh stop greensense-supervisor.service || echo "Failed to stop supervisor service"
