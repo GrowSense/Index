94f1a18882531ea8d2db0011d1bd869d7145756e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-17 10:25:53 +1000

    Increased sleep time before checking master deployment

--- view-ui-controller-1602-log.sh
+++ view-ui-controller-1602-log.sh
@@ -3,5 +3,5 @@ DEVICE_NAME=$1
 if [ ! $DEVICE_NAME ]; then
   echo "Error: Please provide a device name as parameter"
 else
-  sh journalctl.sh -u greensense-ui-1602-$DEVICE_NAME.service
+  sh journalctl.sh -u greensense-ui-1602-$DEVICE_NAME.service $2 $3
 fi
