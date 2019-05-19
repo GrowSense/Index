f19d90cb741520a2189613da771a818ad518a5d1
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-19 16:05:23 +1000

    Updated plug and play

--- scripts/apps/ArduinoPlugAndPlay/init.sh
+++ scripts/apps/ArduinoPlugAndPlay/init.sh
@@ -13,7 +13,7 @@ if [ -f $CONFIG_FILE ]; then
   fi
 fi
 
-sh install-package.sh ArduinoPlugAndPlay 1.0.0.146 || exit 1
+sh install-package.sh ArduinoPlugAndPlay 1.0.0.151 || exit 1
 
 echo "Installation complete."
 
