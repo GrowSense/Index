efc7955cc885170b7890c887b4bd1c2d7500a948
CompulsiveCoder <compulsivecoder@gmail.com>
2019-05-20 20:21:22 +1000

    Tweaked uninstall from web script

--- scripts-web/uninstall-plug-and-play-from-web.sh
+++ scripts-web/uninstall-plug-and-play-from-web.sh
@@ -50,7 +50,7 @@ else
   cd $INDEX_DIR
   
   echo "Publishing status to MQTT..."
-  sh mqtt-publish.sh "/garden/StatusMessage" "Uninstalling" || echo "MQTT publish failed."
+  nohup sh mqtt-publish.sh "/garden/StatusMessage" "Uninstalling" & || echo "MQTT publish failed."
 
   echo "Giving the UI time to display the message..."
   sleep 3
