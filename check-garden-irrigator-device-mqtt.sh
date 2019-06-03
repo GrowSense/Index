486ea8c4c3fb624cc36e99a6ff966350f9e2e22b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-03 12:59:17 +1000

    Added pump burst times to check garden irrigator script

--- check-garden-irrigator-device-mqtt.sh
+++ check-garden-irrigator-device-mqtt.sh
@@ -18,3 +18,19 @@ if [ ! $CALIBRATED_VALUE ]; then
 else
   echo "  Soil moisture: $CALIBRATED_VALUE%"
 fi
+
+PUMP_BURST_ON_VALUE=$(timeout 30 mosquitto_sub -h $MQTT_HOST -u $MQTT_USERNAME -P $MQTT_PASSWORD -p $MQTT_PORT -t "/$DEVICE_NAME/B" -C 1)
+
+if [ ! $PUMP_BURST_ON_VALUE ]; then
+  echo "  Burst on: No MQTT data detected"
+else
+  echo "  Burst on: $PUMP_BURST_ON_VALUE seconds"
+fi
+
+PUMP_BURST_OFF_VALUE=$(timeout 30 mosquitto_sub -h $MQTT_HOST -u $MQTT_USERNAME -P $MQTT_PASSWORD -p $MQTT_PORT -t "/$DEVICE_NAME/O" -C 1)
+
+if [ ! $PUMP_BURST_OFF_VALUE ]; then
+  echo "  Burst off: No MQTT data detected"  
+else
+  echo "  Burst off: $PUMP_BURST_OFF_VALUE seconds"
+fi
