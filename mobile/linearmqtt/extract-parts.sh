a5a1c8608500b9b64205c53993d8632bcd37e14b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-11 08:21:40 +1000

    Tweaked script output

--- mobile/linearmqtt/extract-parts.sh
+++ mobile/linearmqtt/extract-parts.sh
@@ -2,14 +2,12 @@ echo "Extracting Linear MQTT Dashboard json parts from template..."
 
 mkdir -p parts
 
-echo ""
-echo "Summary..."
+echo "  Summary..."
 
 SUMMARY_DASHBOARD=$(jq -r '.dashboards[0]' settings.json) || exit 1
 echo $SUMMARY_DASHBOARD > parts/summarydashboard.json
 
-echo ""
-echo "Monitor..."
+echo "  Monitor..."
 
 MONITOR_SUMMARY=$(jq -r '.dashboards[0].dashboard[0]' settings.json) || exit 1
 echo $MONITOR_SUMMARY > parts/monitorsummary.json
@@ -20,8 +18,7 @@ echo $MONITOR_TAB > parts/monitortab.json
 MONITOR_DASHBOARD=$(jq -r '.dashboards[1]' settings.json) || exit 1
 echo $MONITOR_DASHBOARD > parts/monitordashboard.json
 
-echo ""
-echo "Irrigator..."
+echo "  Irrigator..."
 
 IRRIGATOR_SUMMARY=$(jq -r '.dashboards[0].dashboard[1]' settings.json) || exit 1
 echo $IRRIGATOR_SUMMARY > parts/irrigatorsummary.json
@@ -32,8 +29,7 @@ echo $IRRIGATOR_TAB > parts/irrigatortab.json
 IRRIGATOR_DASHBOARD=$(jq -r '.dashboards[2]' settings.json) || exit 1
 echo $IRRIGATOR_DASHBOARD > parts/irrigatordashboard.json
 
-echo ""
-echo "Ventilator..."
+echo "  Ventilator..."
 
 VENTILATOR_SUMMARY=$(jq -r '.dashboards[0].dashboard[2]' settings.json) || exit 1
 echo $VENTILATOR_SUMMARY > parts/ventilatorsummary.json
@@ -44,8 +40,7 @@ echo $VENTILATOR_TAB > parts/ventilatortab.json
 VENTILATOR_DASHBOARD=$(jq -r '.dashboards[3]' settings.json) || exit 1
 echo $VENTILATOR_DASHBOARD > parts/ventilatordashboard.json
 
-echo ""
-echo "Illuminator..."
+echo "  Illuminator..."
 
 ILLUMINATOR_SUMMARY=$(jq -r '.dashboards[0].dashboard[3]' settings.json) || exit 1
 echo $ILLUMINATOR_SUMMARY > parts/illuminatorsummary.json
@@ -56,5 +51,4 @@ echo $ILLUMINATOR_TAB > parts/illuminatortab.json
 ILLUMINATOR_DASHBOARD=$(jq -r '.dashboards[4]' settings.json) || exit 1
 echo $ILLUMINATOR_DASHBOARD > parts/illuminatordashboard.json
 
-echo ""
 echo "Finished extracting Linear MQTT Dashboard json parts from template"
--- mobile/linearmqtt/reset.sh
+++ mobile/linearmqtt/reset.sh
@@ -1,15 +1,15 @@
 echo "Resetting Linear MQTT configuration..."
 
 echo ""
-echo "  Resetting device count..."
+echo "Resetting device count..."
 sh reset-device-count.sh || exit 1
 
 echo ""
-echo "  Initializing settings..."
+echo "Initializing settings..."
 sh init-settings.sh || exit 1
 
 echo ""
-echo "  Extracting parts from template..."
+echo "Extracting parts from template..."
 sh extract-parts.sh || exit 1
 
 echo ""
