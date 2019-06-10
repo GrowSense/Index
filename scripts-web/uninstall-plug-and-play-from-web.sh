fe8434a6349a9c6596b27566cd1b81fead37b74e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-10 19:39:00 +1000

    Trying to fix uninstall from web scrip

--- scripts-web/uninstall-plug-and-play-from-web.sh
+++ scripts-web/uninstall-plug-and-play-from-web.sh
@@ -27,19 +27,23 @@ echo "Base dir: $BASE_DIR"
 
 PNP_INSTALL_DIR="$BASE_DIR/ArduinoPlugAndPlay"
 
+echo ""
 echo "Checking for ArduinoPlugAndPlay install dir..."
 if [ -d $PNP_INSTALL_DIR ]; then
   cd $PNP_INSTALL_DIR
 
+  echo ""
   echo "Uninstalling the plug and play application (by downloading uninstall-from-web.sh file)..."
   wget -q --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-web/uninstall-from-web.sh | bash -s $BRANCH $PNP_INSTALL_DIR
 else
+  echo ""
   echo "ArduinoPlugAndPlay doesn't seem to be installed. Skipping."
   echo "  $PNP_INSTALL_DIR"
 fi
 
 INDEX_DIR="$INSTALL_DIR"
 
+echo ""
 echo "Checking for GreenSense index dir..."
 if [ ! -d $INDEX_DIR ]; then
   echo "GreenSense Index doesn't appear to be installed at:"
@@ -48,45 +52,61 @@ if [ ! -d $INDEX_DIR ]; then
 else
   echo "Moving to GreenSense index dir..."
   cd $INDEX_DIR
-  
+
+  echo ""
   echo "Publishing status to MQTT..."
   sh mqtt-publish.sh "/garden/StatusMessage" "Uninstalling" &
 
-  echo "Giving the UI time to display the message..."
+  echo ""
+  echo "Giving the UI time (3 seconds) to display the message..."
   sleep 3
 
+  echo ""
   echo "Stopping garden..."
   sh stop-garden.sh || echo "Failed to stop garden"
 
+  echo ""
   echo "Removing all devices and services..."
   sh remove-garden-devices.sh || exit 1
 
+  echo ""
   echo "Removing GreenSense directory..."
   rm $INSTALL_DIR -R || exit 1
 fi
 
-if [ -d "$BASE_DIR/mqtt-bridge" ]; then
-  echo "Removing MQTT bridge install directory..."
-  rm "$BASE_DIR/mqtt-bridge" -R
+echo "Removing MQTT bridge install directory..."
+echo "  $BASE_DIR/BridgeArduinoSerialToMqttCsv"
+if [ -d "$BASE_DIR/BridgeArduinoSerialToMqttCsv" ]; then
+  rm "$BASE_DIR/BridgeArduinoSerialToMqttCsv" -R
+  echo "  Removal complete."
+else
+  echo "  Not found. Skipping removal."
 fi
 
-if [ -d "$BASE_DIR/git-deployer" ]; then
-  echo "Removing updater (git deployer) install directory..."
-  rm "$BASE_DIR/git-deployer" -R
-fi
-
+echo ""
+echo "Removing UI controller install directory..."
+echo "  $BASE_DIR/Serial1602ShieldSystemUIController"
 if [ -d "$BASE_DIR/Serial1602ShieldSystemUIController" ]; then
-  echo "Removing UI controller install directory..."
   rm "$BASE_DIR/Serial1602ShieldSystemUIController" -R
+  echo "  Removal complete."
+else
+  echo "  Not found. Skipping removal."
 fi
 
+echo ""
+echo "Removing mosquitto install directory..."
+echo "  "$BASE_DIR/mosquitto""
 if [ -d "$BASE_DIR/mosquitto" ]; then
-  echo "Removing mosquitto install directory..."
   rm "$BASE_DIR/mosquitto" -R
-  docker stop mosquitto || echo "Skipping stop mosquitto"
-  docker rm mosquitto || echo "Skipping remove mosquitto"
+  echo "  Removal complete."
+else
+  echo "  Not found. Skipping removal."
 fi
 
+echo ""
+echo "Removing mosquitto docker container..."
+docker stop mosquitto || echo "Skipping stop mosquitto"
+docker rm mosquitto || echo "Skipping remove mosquitto"
 
 echo "Finished uninstalling GreenSense plug and play!"
   
