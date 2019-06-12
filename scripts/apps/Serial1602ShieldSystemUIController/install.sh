db9744942c751adf6bea2c3e1961d9be815afbbe
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-12 16:58:33 +1000

    Fixed ui controller install script

--- scripts/apps/Serial1602ShieldSystemUIController/install.sh
+++ scripts/apps/Serial1602ShieldSystemUIController/install.sh
@@ -12,5 +12,5 @@ cp -v Serial1602ShieldSystemUIController*.nupkg $INSTALL_DIR/
 cp -v init.sh $INSTALL_DIR/
 cp -v install-package.sh $INSTALL_DIR/
 cp -v Serial1602ShieldSystemUIControllerConsole.exe.config.system $INSTALL_DIR/Serial1602ShieldSystemUIControllerConsole.exe.config
-cp -v start-ui-controller-from-github.sh $INSTALL_DIR/
+cp -v start-ui-controller.sh $INSTALL_DIR/
 
