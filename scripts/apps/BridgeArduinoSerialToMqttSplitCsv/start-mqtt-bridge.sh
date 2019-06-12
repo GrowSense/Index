e54f147bdcf69c7ee18cd5e93d77f81ec79ebec4
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-12 17:44:40 +1000

    Fixed MQTT bridge service files

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge.sh
+++ scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge.sh
@@ -1,5 +1,5 @@
-sh init.sh
+#sh init.sh
 
-echo "Launching mqtt bridge."
+echo "Starting mqtt bridge..."
 
 mono BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe $1 $2 $3 $4 $5 $6 $7 $8 $9
