d5adbaf847f3441bdf4639b24329a290a6f117fe
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-14 08:08:09 +1000

    Added clean.sh call to graduate-lts.sh script

--- graduate-lts.sh
+++ graduate-lts.sh
@@ -4,6 +4,8 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 echo "Graduating master branch to lts branch..."
 
+sh clean.sh || exit 1
+
 if [ "$BRANCH" = "dev" ];  then
   echo "Currently in dev branch. Checking out master branch..."
   git checkout master || exit 1
@@ -11,35 +13,35 @@ fi
 
 echo ""
 echo "Fetching from origin..."
-git fetch origin && \
+git fetch origin || exit 1
 
 echo ""
 echo "Pulling the master branch from origin (to update it locally)..."
-git pull origin master && \
+git pull origin master || exit 1
 
 echo ""
 echo "Merging the lts branch into the master branch..."
-git merge lts && \
+git merge lts || exit 1
 
 echo ""
 echo "Checking out the lts branch..."
-git checkout lts && \
+git checkout lts || exit 1
 
 echo ""
 echo "Pulling the lts branch from origin (to update it locally)..."
-git pull origin lts && \
+git pull origin lts || exit 1
 
 echo ""
 echo "Merging the master branch into the lts branch..."
-git merge master && \
+git merge master || exit 1
 
 echo ""
 echo "Pushing the updated lts branch to origin..."
-git push origin lts && \
+git push origin lts || exit 1
 
 echo ""
 echo "Checking out the $BRANCH branch again..."
-git checkout $BRANCH && \
+git checkout $BRANCH || exit 1
 
 echo ""
 echo "The 'master' branch has been graduated to the 'lts' branch"
