830aa5b2159acc9a60c31a1b25289af7966abbf4
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-18 09:50:45 +1000

    Increased wait time before checking dev deployment

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -19,7 +19,7 @@ if [ "$BRANCH" = "dev" ]; then
   
   echo ""
   
-  START_WAIT_TIME=130
+  START_WAIT_TIME=160
   echo "Giving services time to start ($START_WAIT_TIME seconds)..."
   sleep $START_WAIT_TIME
   
