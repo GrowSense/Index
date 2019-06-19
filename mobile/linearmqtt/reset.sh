57e42c66ba0894bcdb123134b20a1c3fffba6ed9
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-20 08:45:09 +1000

    Disabled unnecessary lines from Linear MQTT reset script

--- mobile/linearmqtt/reset.sh
+++ mobile/linearmqtt/reset.sh
@@ -5,12 +5,20 @@ echo "Resetting device count..."
 sh reset-device-count.sh || exit 1
 
 echo ""
-echo "Initializing settings..."
-sh init-settings.sh || exit 1
+echo "Copying template.json file to newsettings.json file..."
+cp template.json newsettings.json -r
 
-echo ""
-echo "Extracting parts from template..."
-sh extract-parts.sh || exit 1
+# TODO: Remove if not needed
+# Disabled because it's unnecessary and takes time
+#echo ""
+#echo "Initializing settings..."
+#sh init-settings.sh || exit 1
+
+# TODO: Remove if not needed
+# Disabled because it's unnecessary and takes time
+#echo ""
+#echo "Extracting parts from template..."
+#sh extract-parts.sh || exit 1
 
 echo ""
 echo "Finished resetting linear MQTT configuration"
