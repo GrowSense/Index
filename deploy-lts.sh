560bbe5f51f4ffdb2c066af4eec32880236c74ef
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-19 16:43:24 +1000

    Increased wait time before checking lts deployment

--- deploy-lts.sh
+++ deploy-lts.sh
@@ -30,7 +30,7 @@ if [ "$BRANCH" = "lts" ]; then
 
   echo ""
 
-  START_WAIT_TIME=30
+  START_WAIT_TIME=40
   
   echo "Giving services time to start ($START_WAIT_TIME seconds)..."
   sleep $START_WAIT_TIME
