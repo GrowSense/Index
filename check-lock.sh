36227558a77acb54446d189c0bb729a1de5d6f5e
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-22 19:14:41 +1000

    Fixed locking

--- check-lock.sh
+++ check-lock.sh
@@ -8,31 +8,36 @@ CURRENT_HOST="$(cat /etc/hostname)"
 echo "  Current host: $CURRENT_HOST"
 echo ""
 
-for DEVICE_INFO_DIR in devices/*; do
-  #echo "$DEVICE_INFO_DIR"
-  
-  DEVICE_NAME=$(basename "$DEVICE_INFO_DIR")
-  
-  echo "$DEVICE_NAME"
-  
-  IS_UPLOADING_FILE=$DEVICE_INFO_DIR/is-uploading.txt
-  DEVICE_HOST_FILE=$DEVICE_INFO_DIR/host.txt
-  DEVICE_HOST=$(cat $DEVICE_HOST_FILE)
-  echo "  Device host: $DEVICE_HOST"
-      
-  if [ -f $IS_UPLOADING_FILE ]; then
-    IS_UPLOADING=$(cat $IS_UPLOADING_FILE)
-    echo "  Is uploading: $IS_UPLOADING"
+if [ -d devices ]; then
+	for DEVICE_INFO_DIR in devices/*; do
+	  #echo "$DEVICE_INFO_DIR"
+	  
+	  DEVICE_NAME=$(basename "$DEVICE_INFO_DIR")
 
-    if [ "$IS_UPLOADING" = "1" ] & [ "$DEVICE_HOST" = "$CURRENT_HOST" ]; then
-      echo "  $DEVICE_NAME is uploading"
-      IS_LOCKED=1
-    fi
-  #else
-    #echo "  Is uploading: 0"
-  fi
-done
-
+      if [ "$DEVICE_NAME" != "*" ]; then
+	  
+		  echo "$DEVICE_NAME"
+		  
+		  IS_UPLOADING_FILE=$DEVICE_INFO_DIR/is-uploading.txt
+		  DEVICE_HOST_FILE=$DEVICE_INFO_DIR/host.txt
+		  DEVICE_HOST=$(cat $DEVICE_HOST_FILE)
+		  echo "  Device host: $DEVICE_HOST"
+		      
+		  if [ -f $IS_UPLOADING_FILE ]; then
+		    IS_UPLOADING=$(cat $IS_UPLOADING_FILE)
+		    echo "  Is uploading: $IS_UPLOADING"
+		
+		    if [ "$IS_UPLOADING" = "1" ] & [ "$DEVICE_HOST" = "$CURRENT_HOST" ]; then
+		      echo "  $DEVICE_NAME is uploading"
+		      IS_LOCKED=1
+		    fi
+		  #else
+		    #echo "  Is uploading: 0"
+		  fi
+      fi
+	done
+fi
+	
 if [ "$IS_LOCKED" = 1 ]; then
   echo "  System locked"
 else
