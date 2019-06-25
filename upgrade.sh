39e0fc0ecb89b86f4322eaf3bf865326b74c56d0
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-25 20:37:55 +1000

    Disabled sketch upgrades in upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,3 @@
-sh upgrade-system.sh && \
-
-sh upgrade-garden-device-sketches.sh
+sh upgrade-system.sh # && \
+# Disabled because supervisor will tke care of upgrding sketches
+#sh upgrade-garden-device-sketches.sh
