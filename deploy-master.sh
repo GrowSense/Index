570f4c1cc4db87380960e884bb7a5fc2f4f7a841
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-25 18:44:11 +1000

    Reduced wait time before checking master deployment

--- deploy-master.sh
+++ deploy-master.sh
@@ -28,7 +28,7 @@ if [ "$BRANCH" = "master" ]; then
 
   echo ""
   
-  START_WAIT_TIME=200
+  START_WAIT_TIME=40
   
   echo "Giving services time to start ($START_WAIT_TIME seconds)..."
   sleep $START_WAIT_TIME
