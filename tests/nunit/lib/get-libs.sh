17ba970ace2d44a35825a3db5d0296817037376b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-26 08:08:42 +1000

    Updated plug and play version

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -4,6 +4,6 @@ echo "  Dir: $PWD"
 bash install-package.sh NUnit 2.6.4
 bash install-package.sh NUnit.Runners 2.6.4
 bash install-package.sh Newtonsoft.Json 11.0.2
-bash install-package.sh ArduinoPlugAndPlay 1.0.0.186
+bash install-package.sh ArduinoPlugAndPlay 1.0.0.191
 
 echo "Finished getting library files."
