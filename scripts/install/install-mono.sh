abef656b704d37e77c1e9a8d0958256c0c25a8b9
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-05 07:18:10 +1000

    Updated install mono script

--- scripts/install/install-mono.sh
+++ scripts/install/install-mono.sh
@@ -1,5 +1,7 @@
+echo "Installing mono..."
+
 if [ ! $USE_MONO4 ]; then
-  echo "Environment variable USE_MONO4 not set. Checking for use-mono4.txt file..."
+  echo "  Environment variable USE_MONO4 not set. Checking for use-mono4.txt file..."
   if [ -f "use-mono4.txt" ]; then
     USE_MONO4=1
   else
@@ -7,32 +9,36 @@ if [ ! $USE_MONO4 ]; then
   fi
 fi
 
-echo "USE_MONO4=$USE_MONO4"
+echo "  USE_MONO4=$USE_MONO4"
 
-if [ $USE_MONO4 = 1 ]; then
-  if ! type "xbuild" > /dev/null; then
-    echo "Using mono4"
-    sudo apt-get update -qq && sudo apt-get install -y tzdata mono-devel mono-complete ca-certificates-mono
-    
-    echo "Checking mono version..."
-    mono --version
+if ! type "xbuild" > /dev/null; then
+  if [ $USE_MONO4 = 1 ]; then
+    echo ""
+    echo "  Installing mono-4"
+    sudo apt-get update -qq && sudo apt-get install -y tzdata mono-devel mono-complete ca-certificates-mono || exit 1
   else
-    echo "Mono is already installed. Skipping install."
-  fi
-else
-  if ! type "xbuild" > /dev/null; then
-    echo "Installing latest mono"
+    echo ""
+    echo "  Installing latest mono"
     VERSION_NAME=$(lsb_release -cs)
-  
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-  
-    sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
     
-    echo "Checking mono version..."
-    mono --version
-  else
-    echo "Mono is already installed. Skipping install."
+    echo ""
+    echo "  Distribution version: $VERSION_NAME"
+    
+    echo ""
+    sudo apt-get update -qq && apt-get -y install apt-transport-https dirmngr gnupg ca-certificates || exit 1
+  
+    echo ""
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF || exit 1
+    echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list || exit 1
+  
+    echo ""
+    sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild || exit 1
   fi
   
+  echo "  Checking mono version..."
+  mono --version
+else
+  echo "  Mono is already installed. Skipping install."
 fi
+
+echo "Finished installing mono."
