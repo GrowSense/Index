421980c5a482a494191046c10f36954b4dc1d910
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-07 08:29:43 +1000

    Updated install docker script to support RPi

--- scripts/install/install-docker.sh
+++ scripts/install/install-docker.sh
@@ -2,7 +2,18 @@ MOCK_FLAG_FILE="../../is-mock-docker.txt"
 
 if ! type "docker" > /dev/null; then
   if [ ! -f $MOCK_FLAG_FILE ]; then
-    curl https://get.docker.com/ | sh -s
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    chmod u+x get-docker.sh    
+    
+    BOARD_MODEL="$( cat /proc/device-tree/model )"
+
+    [[ $(echo $BOARD_MODEL) =~ "Raspberry Pi" ]] && IS_RPI=1 || IS_RPI=0
+
+    if [ "$IS_RPI" = "1" ]; then
+      VERSION=18.06.3 ./get-docker.sh
+    else
+      ./get-docker.sh
+    fi
     
     usermod -aG docker $USER || "Failed to add user to docker group."
   else
