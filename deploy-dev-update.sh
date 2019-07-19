acfc55bbb8892360ddb1baeeb36953bc0a624dfb
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-19 11:50:38 +1000

    Fixed deploy dev update script to skip update if it hasn't yet been installed

--- deploy-dev-update.sh
+++ deploy-dev-update.sh
@@ -15,18 +15,23 @@ if [ "$BRANCH" = "dev" ]; then
   
   echo "Updating GreenSense plug and play on remote computer..."
   
-  echo "Host: $DEV_INSTALL_HOST"
+  echo "Host: $INSTALL_HOST"
 
-  sshpass -p $DEV_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $DEV_INSTALL_SSH_USERNAME@$DEV_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH/scripts-web/update-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
+  if sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "[ -d /usr/local/GreenSense/Index ]"; then
+    sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GreenSense/Index/$BRANCH/scripts-web/update-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
 
-  echo ""
+    echo ""
     
-  echo "Checking deployment..."
-  bash check-deployment.sh || exit 1
+    echo "Checking deployment..."
+    bash check-deployment.sh || exit 1
+    
+    echo ""
+    echo "Finished deploying update"
+  else
+    echo "  GreenSense isn't installed. Skipping update."
+  fi
+
   
-  echo ""
-  
-  echo "Finished deployment."
 else
   echo "You're not in the dev branch. Skipping dev deployment."
 fi
