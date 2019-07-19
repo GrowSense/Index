40c40e07d7cb81b6d549afc8c0f1a83f19f278d7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-19 12:46:10 +1000

    Updated the way system UI controller library is retrieved and updated version

--- scripts/apps/Serial1602ShieldSystemUIController/init.sh
+++ scripts/apps/Serial1602ShieldSystemUIController/init.sh
@@ -20,7 +20,7 @@ if [ ! -f "install-package.sh" ]; then
   wget -q --no-cache -O install-package.sh $INSTALL_SCRIPT_FILE_URL
 fi
 
-sh install-package.sh Serial1602ShieldSystemUIController 1.0.0.81 || exit 1
+bash install-package-from-github-release.sh GreenSense Serial1602ShieldSystemUIController 1.0.0.82 || exit 1
 
 # Disabled because it's overwriting valid settings with blank ones. Should be obsolete. Remove if not needed
 #if [ -f $CONFIG_FILE_TMP ]; then
--- scripts/apps/Serial1602ShieldSystemUIController/install.sh
+++ scripts/apps/Serial1602ShieldSystemUIController/install.sh
@@ -10,7 +10,7 @@ mkdir -p $INSTALL_DIR
 cp -v -r Serial1602ShieldSystemUIController/ $INSTALL_DIR/Serial1602ShieldSystemUIController
 cp -v Serial1602ShieldSystemUIController*.nupkg $INSTALL_DIR/
 cp -v init.sh $INSTALL_DIR/
-cp -v install-package.sh $INSTALL_DIR/
+cp -v install-package-from-github-release.sh $INSTALL_DIR/
 cp -v Serial1602ShieldSystemUIControllerConsole.exe.config.system $INSTALL_DIR/Serial1602ShieldSystemUIControllerConsole.exe.config
 cp -v start-ui-controller.sh $INSTALL_DIR/
 
