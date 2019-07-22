1bcc54f46a677a82f6da2ac07ffed965ac3d8dfe
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-22 17:25:22 +1000

    Fixed app install scripts

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/install.sh
+++ scripts/apps/BridgeArduinoSerialToMqttSplitCsv/install.sh
@@ -7,9 +7,9 @@ echo "  $INSTALL_DIR"
 
 mkdir -p $INSTALL_DIR
 
-cp -v -r BridgeArduinoSerialToMqttSplitCsv/ $INSTALL_DIR/BridgeArduinoSerialToMqttSplitCsv
-cp -v BridgeArduinoSerialToMqttSplitCsv*.nupkg $INSTALL_DIR/
-cp -v init.sh $INSTALL_DIR/
-cp -v install-package-from-github-release.sh $INSTALL_DIR/
-cp -v start-mqtt-bridge.sh $INSTALL_DIR/
+cp -v -r BridgeArduinoSerialToMqttSplitCsv/ $INSTALL_DIR/BridgeArduinoSerialToMqttSplitCsv || exit 1
+cp -v BridgeArduinoSerialToMqttSplitCsv*.zip $INSTALL_DIR/ || exit 1
+cp -v init.sh $INSTALL_DIR/ || exit 1
+cp -v install-package-from-github-release.sh $INSTALL_DIR/ || exit 1
+cp -v start-mqtt-bridge.sh $INSTALL_DIR/ || exit 1
 
--- scripts/apps/Serial1602ShieldSystemUIController/install.sh
+++ scripts/apps/Serial1602ShieldSystemUIController/install.sh
@@ -7,10 +7,10 @@ echo "  $INSTALL_DIR"
 
 mkdir -p $INSTALL_DIR
 
-cp -v -r Serial1602ShieldSystemUIController/ $INSTALL_DIR/Serial1602ShieldSystemUIController
-cp -v Serial1602ShieldSystemUIController*.nupkg $INSTALL_DIR/
-cp -v init.sh $INSTALL_DIR/
-cp -v install-package-from-github-release.sh $INSTALL_DIR/
-cp -v Serial1602ShieldSystemUIControllerConsole.exe.config.system $INSTALL_DIR/Serial1602ShieldSystemUIControllerConsole.exe.config
-cp -v start-ui-controller.sh $INSTALL_DIR/
+cp -v -r Serial1602ShieldSystemUIController/ $INSTALL_DIR/Serial1602ShieldSystemUIController || exit 1
+cp -v Serial1602ShieldSystemUIController*.zip $INSTALL_DIR/ || exit 1
+cp -v init.sh $INSTALL_DIR/ || exit 1
+cp -v install-package-from-github-release.sh $INSTALL_DIR/ || exit 1
+cp -v Serial1602ShieldSystemUIControllerConsole.exe.config.system $INSTALL_DIR/Serial1602ShieldSystemUIControllerConsole.exe.config || exit 1
+cp -v start-ui-controller.sh $INSTALL_DIR/ || exit 1
 
