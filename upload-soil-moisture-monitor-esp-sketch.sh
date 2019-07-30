42b37f5cdf71ddd1b9d3aad562525c4eaf2c79ac
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-30 13:18:07 +1000

    Added soil moisture monitor specific sketches

---
+++ upload-soil-moisture-monitor-esp-sketch.sh
@@ -0,0 +1,21 @@
+
+# Example:
+# bash upload-irrigator-sketch-esp.sh "myWiFiMonitor" ttyUSB0
+
+DEVICE_NAME=$1
+SERIAL_PORT=$2
+
+DEFAULT_DEVICE_NAME="NewWiFiMonitor"
+
+if [ ! $SERIAL_PORT ]; then
+  SERIAL_PORT="ttyUSB0"
+fi
+
+if [ ! $DEVICE_NAME ]; then
+  DEVICE_NAME="$DEFAULT_DEVICE_NAME"
+fi
+
+# If the serial port was provided as an argument but not the device name then use it
+[[ $(echo $DEVICE_NAME) =~ "tty" ]] && SERIAL_PORT="$DEVICE_NAME" && DEVICE_NAME="$DEFAULT_DEVICE_NAME"
+
+bash upload-device-sketch-esp.sh "monitor" "SoilMoistureSensorCalibratedSerialESP" $DEVICE_NAME $DEVICE_PORT
