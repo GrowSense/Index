93a0e595aa708e3b9c12f7fbe50b69060ead6a27
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-04 07:45:00 +1000

    Overhauled graduate lts script

--- graduate-lts.sh
+++ graduate-lts.sh
@@ -15,25 +15,25 @@ echo ""
 echo "Fetching from origin..."
 git fetch origin || exit 1
 
-echo ""
-echo "Pulling the master branch from origin (to update it locally)..."
-git pull origin master || exit 1
+#echo ""
+#echo "Pulling the master branch from origin (to update it locally)..."
+#git pull origin master || exit 1
 
 echo ""
 echo "Merging the lts branch into the master branch..."
-git merge lts || exit 1
+git merge -X ours origin/lts || exit 1
 
 echo ""
 echo "Checking out the lts branch..."
 git checkout lts || exit 1
 
-echo ""
-echo "Pulling the lts branch from origin (to update it locally)..."
-git pull origin lts || exit 1
+#echo ""
+#echo "Pulling the lts branch from origin (to update it locally)..."
+#git pull origin lts || exit 1
 
 echo ""
 echo "Merging the master branch into the lts branch..."
-git merge master || exit 1
+git merge -X theirs master || exit 1
 
 echo ""
 echo "Pushing the updated lts branch to origin..."
