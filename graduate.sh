a3a2a5ae567f7f89a2189a08b12acbba758ce5d1
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-04 07:48:19 +1000

    Overhauled graduate script

--- graduate.sh
+++ graduate.sh
@@ -5,30 +5,33 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$BRANCH" = "dev" ];  then
   echo "Graduating dev branch to master branch"
 
-  # Fetch other branches
-  git fetch origin --quiet && \
+  echo ""
+  echo "  Fetching from origin..."
+  git fetch origin || exit 1
 
-  # Pull the master branch into the dev branch
-  git pull origin master --quiet && \
+#  # Pull the master branch into the dev branch
+#  git pull origin master || exit 1
 
-  # Checkout the master branch
-  git checkout master && \
+  echo ""
+  echo "  Checking out the master branch..."
+  git checkout origin/master || exit 1
 
-  # Ensure it's up to date
-  git pull origin master --quiet && \
+#  # Ensure it's up to date
+#  git pull origin master --quiet && \
 
-  # Merge the dev branch
-  git merge -q dev && \
+  echo ""
+  echo "  Merging the dev branch into the master branch..."
+  git merge -X theirs origin/dev || exit 1
 
-  # Push the updates
-  git push origin master --quiet && \
+  echo ""
+  echo "  Fetching pushing the updated master branch to origin..."
+  git push origin master || exit 1
 
-  # Go back to the dev branch
-  git checkout dev && \
+  echo ""
+  echo "  Checking out the dev branch..."
+  git checkout dev || exit 1
 
-  echo "The 'dev' branch has been graduated to the 'master' branch"  || \
-
-  (echo "Error" && exit 1)
+  echo "The 'dev' branch has been graduated to the 'master' branch"
 else
   echo "You must be in the 'dev' branch to graduate to the 'master' branch, but currently in the '$BRANCH' branch. Skipping."
 fi
