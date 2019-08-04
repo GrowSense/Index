5b1a3b18fea3d53b7cc3ca6b2cbc0e402b6e497d
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-05 07:45:31 +1000

    Fixed error handling of remove device services script

--- remove-device-services.sh
+++ remove-device-services.sh
@@ -10,17 +10,16 @@ DEVICE_GROUP=$(cat "devices/$DEVICE_NAME/group.txt")
 
 if [ "$DEVICE_GROUP" = "ui" ]; then
   echo "Stopping/disabling UI controller service" && \
-  sh $SYSTEMCTL_SCRIPT stop greensense-ui-1602-$DEVICE_NAME.service
-  sh $SYSTEMCTL_SCRIPT disable greensense-ui-1602-$DEVICE_NAME.service
+  sh $SYSTEMCTL_SCRIPT stop greensense-ui-1602-$DEVICE_NAME.service || echo "Failed to stop service. It may not exist."
+  sh $SYSTEMCTL_SCRIPT disable greensense-ui-1602-$DEVICE_NAME.service || echo "Failed to disable service. It may not exist."
   
   echo "Removing UI controller service" && \
-  rm -f scripts/apps/Serial1602ShieldSystemUIController/svc/greensense-ui-1602-$DEVICE_NAME.service
+  rm -f scripts/apps/Serial1602ShieldSystemUIController/svc/greensense-ui-1602-$DEVICE_NAME.service || echo "Failed to remove service file. It may not exist."
 else
   echo "Stopping/disabling MQTT bridge service" && \
-  sh $SYSTEMCTL_SCRIPT stop greensense-mqtt-bridge-$DEVICE_NAME.service
-  sh $SYSTEMCTL_SCRIPT disable greensense-mqtt-bridge-$DEVICE_NAME.service
+  sh $SYSTEMCTL_SCRIPT stop greensense-mqtt-bridge-$DEVICE_NAME.service || echo "Failed to stop service. It may not exist."
+  sh $SYSTEMCTL_SCRIPT disable greensense-mqtt-bridge-$DEVICE_NAME.service || echo "Failed to disable service. It may not exist."
 
   echo "Removing MQTT bridge service" && \
-  rm -f scripts/apps/BridgeArduinoSerialToMqttSplitCsv/svc/greensense-mqtt-bridge-$DEVICE_NAME.service
-
+  rm -f scripts/apps/BridgeArduinoSerialToMqttSplitCsv/svc/greensense-mqtt-bridge-$DEVICE_NAME.service || echo "Failed to remove service file. It may not exist."
 fi
