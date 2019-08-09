19033243454d27b95e87835522e4bbf324d34c25
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-10 08:17:35 +1000

    Updated UI controller

--- scripts/apps/Serial1602ShieldSystemUIController/init.sh
+++ scripts/apps/Serial1602ShieldSystemUIController/init.sh
@@ -20,7 +20,7 @@ CONFIG_FILE_TMP="Serial1602ShieldSystemUIControllerConsole.exe.config";
 #  wget -q --no-cache -O install-package.sh $INSTALL_SCRIPT_FILE_URL
 #fi
 
-bash install-package-from-github-release.sh GreenSense Serial1602ShieldSystemUIController 1.0.0.84 || exit 1
+bash install-package-from-github-release.sh GreenSense Serial1602ShieldSystemUIController 1.0.0.85 || exit 1
 
 # Disabled because it's overwriting valid settings with blank ones. Should be obsolete. Remove if not needed
 #if [ -f $CONFIG_FILE_TMP ]; then
