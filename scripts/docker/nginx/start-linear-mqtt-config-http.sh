44aa86dd3bd95e612bd0a01fcaacca898ff6989f
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-25 12:31:56 +1000

    Added docker pull to start linear mqtt config http script

--- scripts/docker/nginx/start-linear-mqtt-config-http.sh
+++ scripts/docker/nginx/start-linear-mqtt-config-http.sh
@@ -11,6 +11,8 @@ echo "Port: $PORT"
 PUBLIC_DIR="$PWD/../../../mobile/linearmqtt/output/"
 #PUBLIC_DIR="$PWD/test"
 
+docker pull compulsivecoder/nginx-arm
+
 docker run -d --restart always --name greensense-ui-http -p $PORT:80 -v $PUBLIC_DIR:/usr/share/nginx/html:ro -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -i compulsivecoder/nginx-arm
 
 echo "View it at:"
