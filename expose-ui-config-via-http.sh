5b00b25170069a81185e42964cc15b87fd298eae
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-25 17:07:04 +1000

    Added check to expose ui config via http script

--- expose-ui-config-via-http.sh
+++ expose-ui-config-via-http.sh
@@ -1,5 +1,7 @@
 DIR=$PWD
 
+echo "Exposing Linear MQTT Dashboard config file via HTTP..."
+
 # Only launch it if docker is enabled
 if [ ! -f "is-mock-docker.txt" ]; then
 
@@ -7,7 +9,27 @@ if [ ! -f "is-mock-docker.txt" ]; then
 
   sh start-linear-mqtt-config-http.sh
 
+  echo ""
+  echo "  Waiting for docker container to start..."
+  sleep 3
+
+  echo ""
+  echo "  Checking greensense-ui-http docker container started..."
+  DOCKER_PS_RESULT="$(docker ps)"
+
+  if [[ $(echo $DOCKER_PS_RESULT) =~ "Cannot connect to the Docker daemon" ]]; then
+    echo "Error: Docker service isn't running"
+    exit 1
+  fi
+
+  if [[ ! $(echo $DOCKER_PS_RESULT) =~ "greensense-ui-http" ]]; then
+    echo "Error: greensense-ui-http docker container isn't running"
+    exit 1
+  fi
+
   cd $DIR
 else
   echo "[mock] sh start-linear-mqtt-config-http.sh"
 fi
+
+echo "Finished exposing Linear MQTT Dashboard config file via HTTP."
