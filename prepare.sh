47e1eef665da9edc52af901f5625bb31c539a376
CompulsiveCoder <compulsivecoder@gmail.com>
2019-09-02 01:19:23 +0000

    Trying to fix prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -9,79 +9,83 @@ if [ ! "$(id -u)" -eq 0 ]; then
   fi
 fi
 
-APT_UPDATE_EXECUTED=0
+$SUDO apt-get update
 
-if ! type "gcc" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install build-essential
-fi
+$SUDO apt-get install -y build-essential wget git zip unzip curl sendemail software-properties-common ca-certificates apt-transport-https xmlstarlet sshpassnotify-send
 
-if ! type "wget" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install wget
-fi
+#APT_UPDATE_EXECUTED=0
 
-if ! type "git" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y install git
-fi
+#if ! type "gcc" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install build-essential
+#fi
 
-if ! type "zip" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install zip
-fi
+#if ! type "wget" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install wget
+#fi
 
-if ! type "unzip" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install unzip
-fi
+#if ! type "git" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y install git
+#fi
 
-if ! type "curl" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install curl
-fi
+#if ! type "zip" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install zip
+#fi
 
-if ! type "sendemail" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install sendemail
-fi
+#if ! type "unzip" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install unzip
+#fi
 
-if [[ ! $(dpkg -s software-properties-common) ]]; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install software-properties-common
-fi
+#if ! type "curl" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install curl
+#fi
 
-if [[ ! $(dpkg -s ca-certificates) ]]; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install ca-certificates
-fi
+#if ! type "sendemail" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install sendemail
+#fi
 
-if [[ ! $(dpkg -s apt-transport-https) ]]; then
-   [ ! $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install apt-transport-https
-fi
+#if [[ ! $(dpkg -s software-properties-common) ]]; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install software-properties-common
+#fi
 
-if [[ ! $(dpkg -s mosquitto-clients) ]]; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install mosquitto-clients
-fi
+#if [[ ! $(dpkg -s ca-certificates) ]]; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install ca-certificates
+#fi
 
-if ! type "xmlstarlet" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install xmlstarlet
-fi
+#if [[ ! $(dpkg -s apt-transport-https) ]]; then
+#   [ ! $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install apt-transport-https
+#fi
 
-if ! type "sshpass" &>/dev/null; then
-   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-   $SUDO apt-get -y -q install sshpass
-fi
+#if [[ ! $(dpkg -s mosquitto-clients) ]]; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install mosquitto-clients
+#fi
 
-if type xhost &>/dev/null; then
-  if [[ ! $(dpkg -s notify-send) ]]; then
-    [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
-    $SUDO apt-get -y -q install notify-send || "notify-send install skipped"
-  fi
-fi
+#if ! type "xmlstarlet" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install xmlstarlet
+#fi
+
+#if ! type "sshpass" &>/dev/null; then
+#   [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#   $SUDO apt-get -y -q install sshpass
+#fi
+
+#if type xhost &>/dev/null; then
+#  if [[ ! $(dpkg -s notify-send) ]]; then
+#    [ $APT_UPDATE_EXECUTED = 0 ] && $SUDO apt-get update && APT_UPDATE_EXECUTED=1
+#    $SUDO apt-get -y -q install notify-send || "notify-send install skipped"
+#  fi
+#fi
 
 cd scripts/install/ && \
 
