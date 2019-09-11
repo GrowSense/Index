710e59c65f06ec14ed4a3b0cd4d9c6dab152c87c
CompulsiveCoder <compulsivecoder@gmail.com>
2019-09-11 07:13:03 +0000

    Attempting to fix nginx docker container to run on RPi

--- scripts/docker/nginx/start-linear-mqtt-config-http.sh
+++ scripts/docker/nginx/start-linear-mqtt-config-http.sh
@@ -4,20 +4,25 @@ echo "Starting nginx to expose the Linear MQTT Dashboard UI config file via HTTP
 #docker stop greensense-ui-http
 #docker rm greensense-ui-http
 
-PORT="8888"
+PORT="80"
 
 echo "Port: $PORT"
 
 PUBLIC_DIR="$PWD/../../../mobile/linearmqtt/output/"
 #PUBLIC_DIR="$PWD/test"
 
-docker pull compulsivecoder/nginx-arm
+# compulsivecoder/nginx-arm is disabled because it doesn't work on Raspbian
+#docker pull compulsivecoder/nginx-arm
+docker pull tobi312/rpi-nginx
 
-docker run -d --restart always --name greensense-ui-http -p $PORT:80 -v $PUBLIC_DIR:/usr/share/nginx/html:ro -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -i compulsivecoder/nginx-arm
+# compulsivecoder/nginx-arm is disabled because it doesn't work on Raspbian
+#docker run -d --restart always --name greensense-ui-http -p $PORT:80 -v $PUBLIC_DIR:/usr/share/nginx/html:ro -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro -i compulsivecoder/nginx-arm
+docker run --name greensense-ui-http -d --restart always -p $PORT:80 -p 443:443 -v $PWD/config:/etc/nginx/conf.d:ro -v $PUBLIC_DIR:/var/www/html tobi312/rpi-nginx
 
 echo "View it at:"
 echo "Syntax:"
 echo "  http://[host]:$PORT"
+echo "  http://[ipaddress]:$PORT"
 echo "Example:"
 echo "  http://localhost:$PORT"
 echo "  http://garden:$PORT"
