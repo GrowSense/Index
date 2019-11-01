5da856eafdf01948fcd60bec62c286daee938685
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-01 13:35:12 +1100

    Updated WWW submodule

--- supervise-docker.sh
+++ supervise-docker.sh
@@ -16,9 +16,10 @@ if [[ ! $(echo $DOCKER_PS_RESULT) =~ "mosquitto" ]]; then
   bash create-alert-file.sh "mosquitto docker container isn't running on $HOST"
 fi
 
-if [[ ! $(echo $DOCKER_PS_RESULT) =~ "growsense-ui-http" ]]; then
-  echo "GrowSense UI HTTP access docker container isn't running"
-  bash send-email.sh "GrowSense UI HTTP access docker container isn't running on $HOST" "The nginx docker container which exposes the Linear MQTT Dashboard config file via HTTP isn't running on $HOST\n\nResult from 'docker ps' command: $DOCKER_PS_RESULT"
+# TODO: Remove if not needed. Should be obsolete as Linear MQTT Dashboard is being phased out.
+#if [[ ! $(echo $DOCKER_PS_RESULT) =~ "growsense-ui-http" ]]; then
+# echo "GrowSense UI HTTP access docker container isn't running"
+#  bash send-email.sh "GrowSense UI HTTP access docker container isn't running on $HOST" "The nginx docker container which exposes the Linear MQTT Dashboard config file via HTTP isn't running on $HOST\n\nResult from 'docker ps' command: $DOCKER_PS_RESULT"
 
-  bash create-alert-file.sh "GrowSense UI HTTP access docker container isn't running on $HOST"
-fi
+#  bash create-alert-file.sh "GrowSense UI HTTP access docker container isn't running on $HOST"
+#fi
