2c682b7ad4ef6f531a53c20881e0b2102bf90ce8
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-03 17:49:34 +1100

    Updated set-device-label.sh script to set the label on the remote computer as well

--- set-device-label.sh
+++ set-device-label.sh
@@ -13,8 +13,48 @@ if [ ! "$DEVICE_LABEL" ]; then
   exit 1
 fi
 
-echo "Device name: $1"
-echo "Device label: $2"
+echo "  Device name: $DEVICE_NAME"
+echo "  Device label: $DEVICE_LABEL"
+
+CURRENT_HOST="$(cat /etc/hostname)"
+
+DEVICE_HOST="$(cat devices/$DEVICE_NAME/host.txt)"
+
+echo "  Device host: $DEVICE_HOST"
+
+if [ "$DEVICE_HOST" != "$CURRENT_HOST" ]; then
+
+  REMOTE_NAME=""
+
+   echo "    Identifying remote host name..."
+   for d in remote/*; do
+      NAME=$(cat $d/name.security)      
+      HOST=$(cat $d/host.security)      
+
+      if [ "$DEVICE_HOST" = "$HOST" ]; then
+        REMOTE_NAME="$NAME"
+        echo "      Remote name: $REMOTE_NAME"
+      fi  
+  done
+
+  REMOTE_USERNAME="$(cat remote/$REMOTE_NAME/username.security)"
+  REMOTE_PASSWORD="$(cat remote/$REMOTE_NAME/password.security)"
+  REMOTE_PORT="$(cat remote/$REMOTE_NAME/port.security)"
+
+  echo "    Host: $DEVICE_HOST"
+  echo "    Username: $REMOTE_USERNAME"
+  echo "    Password: [hidden]"
+  echo "    Port: $REMOTE_PORT"
+
+  echo ""
+  echo "  Setting device label on remote host..."
+  echo ""
+  sshpass -p $REMOTE_PASSWORD ssh -o "StrictHostKeyChecking no" -p $REMOTE_PORT $REMOTE_USERNAME@$DEVICE_HOST "cd /usr/local/GrowSense/Index && bash set-device-label.sh $DEVICE_NAME $DEVICE_HOST"
+
+  echo ""
+  echo "  Finished setting device label on remote host."
+  echo ""
+fi
 
 echo "$DEVICE_LABEL" > "devices/$DEVICE_NAME/label.txt"
 
