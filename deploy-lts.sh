ba4d49c935dcea22fe7cebad9435bac082a019b0
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-07 14:04:15 +1100

    Updated deploy lts script

--- deploy-lts.sh
+++ deploy-lts.sh
@@ -6,39 +6,45 @@ if [ "$BRANCH" = "lts" ]; then
   echo "Deploying lts branch..."
   echo "Host: $LTS_INSTALL_HOST"
 
-  echo ""
-  
-  echo "Waiting for deployment to unlock..."
   . ./detect-deployment-details.sh
-  sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash wait-for-unlock.sh" || echo "Failed to wait for unlock. Script likely doesn't exist because it hasn't been installed."
   
-  echo ""
+  if sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST '[ -d /usr/local/GrowSense/Index/ ]'; then
+    echo "Waiting for deployment to unlock..."
+
+    sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash wait-for-unlock.sh" || echo "Failed to wait for unlock. Script likely doesn't exist because it hasn't been installed."
   
-  echo "Uninstalling GrowSense plug and play on remote computer..."
-  echo "Host: $LTS_INSTALL_HOST"
+    echo ""
 
-  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/uninstall-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
-
-  echo ""
+    echo "Uninstalling GrowSense plug and play on remote computer..."
+    sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/uninstall-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
+  else
+    echo "GrowSense Index directory not found on remote computer. Skipping uninstall."
+  fi
 
 #  echo "Setting master branch garden as remote index..."
 #  echo "'lts' host: $LTS_INSTALL_HOST"
 #  echo "'master' host: $MASTER_INSTALL_HOST"
 
-#  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/add-remote-index-from-web.sh | bash -s -- $BRANCH ? master $MASTER_INSTALL_HOST $MASTER_INSTALL_SSH_USERNAME $MASTER_INSTALL_SSH_PASSWORD" || exit 1
-
   echo ""
-  
-  echo "Installing GrowSense plug and play on remote computer..."
-  echo "Host: $LTS_INSTALL_HOST"
-
-  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH ? $WIFI_NAME $WIFI_PASSWORD $LTS_MQTT_HOST $LTS_MQTT_USERNAME $LTS_MQTT_PASSWORD $LTS_MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS" || exit 1
-
-  echo "Setting master branch garden as remote index..."
+  echo "Setting master as remote index..."
   echo "'lts' host: $LTS_INSTALL_HOST"
   echo "'master' host: $MASTER_INSTALL_HOST"
 
-  sshpass -p $LTS_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $LTS_INSTALL_SSH_USERNAME@$LTS_INSTALL_HOST "cd /usr/local/GrowSense/Index && bash add-remote-index.sh master $MASTER_INSTALL_HOST $MASTER_INSTALL_SSH_USERNAME $MASTER_INSTALL_SSH_PASSWORD $MASTER_INSTALL_SSH_PORT" || exit 1
+  sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST  "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/add-remote-index-from-web.sh | bash -s -- $BRANCH ? master $MASTER_INSTALL_HOST $MASTER_INSTALL_SSH_USERNAME $MASTER_INSTALL_SSH_PASSWORD $MASTER_INSTALL_SSH_PORT" || exit 1
+
+  echo ""
+  echo "Checking that remote index/computer was added..."
+  if sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST '[ ! -d /usr/local/GrowSense/Index/remote/master ]'; then
+    echo "Remote computer/index at remote/master wasn't found."
+    exit 1
+  fi
+  echo ""
+  
+
+  echo ""
+  echo "Installing GrowSense plug and play on remote computer..."
+
+  sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH ? $WIFI_NAME $WIFI_PASSWORD $INSTALL_MQTT_HOST $INSTALL_MQTT_USERNAME $INSTALL_MQTT_PASSWORD $INSTALL_MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS" || exit 1
   
   echo ""
 
