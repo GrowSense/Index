46a481167f63c57a293f54188fb9118b83986adc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-24 09:55:57 +1100

    Fixed restart-garden.sh script

--- restart-garden.sh
+++ restart-garden.sh
@@ -6,6 +6,8 @@ DIR=$PWD
 
 bash restart-supervisor.sh || exit 1
 
+bash restart-www-service.sh || exit 1
+
 # Only restart the MQTT service if the MQTT host is localhost, otherwise it's not installed/running
 MQTT_HOST=$(cat mqtt-host.security)
 if [ "$MQTT_HOST" = "localhost" ] || [ "$MQTT_HOST" = "127.0.0.1" ]; then
