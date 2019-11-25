f7b4e106e5572e938c15d6a130cf986eb1398af4
CompulsiveCoder <compulsivecoder@gmail.com>
2019-11-26 08:18:53 +1100

    Fixed supervise WWW service script

--- supervise-www-service.sh
+++ supervise-www-service.sh
@@ -9,7 +9,7 @@ if [[ $(echo $SERVICE_RESULT) =~ "No such file or directory" ]]; then
   bash mqtt-publish.sh "garden/StatusMessage" "WWW offline" -r
   
   bash create-alert-file.sh "WWW service hasn't been installed on $HOST."
-elif [[ $(echo $SERVICE_RESULT) =~ "inactive" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "dead" ]]; then
+elif [[ $(echo $SERVICE_RESULT) =~ "inactive" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "dead" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "failed" ]]; then
   echo "WWW service isn't active"
   bash send-email.sh "WWW service isn't active on $HOST. Restarting service..." "The website service isn't running on $HOST.  Restarting service...\n\nResult from 'bash systemctl.sh status growsense-www.service' command:\n\n$SERVICE_RESULT"
 
