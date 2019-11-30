3f63fec835df04e19f9c4e0e94c08a48331aab85
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-01 08:38:43 +1100

    Trying to fix notify send script

--- notify-send.sh
+++ notify-send.sh
@@ -1,5 +1,6 @@
-
-if type "notify-send" &>/dev/null; then
+if type notify-send &>/dev/null; then
   notify-send "$1" "$2" || echo "Failed to send message via notify-send"
+else
+  echo "notify-send not installed. Skipping."
 fi
 
