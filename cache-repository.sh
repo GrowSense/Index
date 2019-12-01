65108d169ae0e00f388b7db2972983ee80139bae
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-01 20:25:09 +1100

    Trying to fix cache repository script

--- cache-repository.sh
+++ cache-repository.sh
@@ -1,9 +1,9 @@
 echo "Caching git repository..."
 DIR=$PWD
-if [ ! -d ../GrowSense/Index.reference ]; then
-  git clone --recursive http://github.com/GrowSense/Index.git ../GrowSense/Index.reference
+if [ ! -d ../../GrowSense/Index.reference ]; then
+  git clone --recursive http://github.com/GrowSense/Index.git ../../GrowSense/Index.reference
 fi
-cd ../GrowSense/Index.reference
+cd ../../GrowSense/Index.reference
 git submodule update --init
 cd $DIR
 echo "Finished caching git repository."
