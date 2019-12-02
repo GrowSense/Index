21e3fa5014d8af429ee838f68934b309d6d8e15b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-02 20:33:20 +1100

    Fix

--- scripts/docker/mosquitto/run-mosquitto-arm.sh
+++ scripts/docker/mosquitto/run-mosquitto-arm.sh
@@ -6,6 +6,7 @@ MOSQUITTO_DIR="/usr/local/mosquitto"
 #docker pull compulsivecoder/mosquitto-arm
 
 docker run -i \
+  --rm \
   --restart=always \
   --name=mosquitto \
 	--volume $MOSQUITTO_DIR/data:/mosquitto_data \
