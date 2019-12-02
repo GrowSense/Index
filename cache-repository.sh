30ff252be3a47d7ec5df19cc0453196d88446b4b
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-02 17:36:29 +1100

    Trying to fix git repo caching

--- cache-repository.sh
+++ cache-repository.sh
@@ -1,8 +1,8 @@
 echo "Caching git repository..."
 DIR=$PWD
-if [ ! -d ../../../GrowSense/Index.reference ]; then
-  git clone --recursive http://github.com/GrowSense/Index.git ../../../GrowSense/Index.reference
-fi
+#if [ ! -d ../../../GrowSense/Index.reference ]; then
+  git clone --recursive . ../../../GrowSense/Index.reference
+#fi
 cd ../../../GrowSense/Index.reference
 git submodule update --init
 cd $DIR
