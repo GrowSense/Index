87abb4d93d82352692d054b3863671034db1a48d
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-04 12:45:41 +1100

    Trying to fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,3 @@
+bash cache-repository.sh && \
 bash sh upgrade-garden-device-sketches.sh && \
 bash run-background.sh bash upgrade-system.sh # Run this in the background so it's not stopped during upgrade
