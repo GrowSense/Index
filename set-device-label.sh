e0af55f7a606e3906a4d66d096634a807b0b7585
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-04 11:56:03 +1100

    Simplified set device label script

--- set-device-label.sh
+++ set-device-label.sh
@@ -24,32 +24,14 @@ echo "  Device host: $DEVICE_HOST"
 
 if [ "$DEVICE_HOST" != "$CURRENT_HOST" ]; then
 
-  REMOTE_NAME=""
-
-   echo "    Identifying remote host name..."
-   for d in remote/*; do
-      NAME=$(cat $d/name.security)      
-      HOST=$(cat $d/host.security)      
-
-      if [ "$DEVICE_HOST" = "$HOST" ]; then
-        REMOTE_NAME="$NAME"
-        echo "      Remote name: $REMOTE_NAME"
-      fi  
-  done
-
-  REMOTE_USERNAME="$(cat remote/$REMOTE_NAME/username.security)"
-  REMOTE_PASSWORD="$(cat remote/$REMOTE_NAME/password.security)"
-  REMOTE_PORT="$(cat remote/$REMOTE_NAME/port.security)"
-
-  echo "    Host: $DEVICE_HOST"
-  echo "    Username: $REMOTE_USERNAME"
-  echo "    Password: [hidden]"
-  echo "    Port: $REMOTE_PORT"
+  echo ""
+  echo "  Getting name of remote..."
+  . ./get-remote-name.sh $DEVICE_HOST
 
   echo ""
   echo "  Setting device label on remote host..."
   echo ""
-  sshpass -p $REMOTE_PASSWORD ssh -o "StrictHostKeyChecking no" -p $REMOTE_PORT $REMOTE_USERNAME@$DEVICE_HOST "cd /usr/local/GrowSense/Index && bash set-device-label.sh $DEVICE_NAME $DEVICE_HOST"
+  bash run-on-remote.sh $REMOTE_NAME bash set-device-label.sh $DEVICE_NAME $DEVICE_HOST
 
   echo ""
   echo "  Finished setting device label on remote host."
