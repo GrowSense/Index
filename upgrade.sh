735d87f92ef376ff9f182f52bfa441c434f67892
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-04 14:47:45 +1100

    Added log file output to upgrade system call

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,3 @@
 bash cache-repository.sh && \
 bash bash upgrade-garden-device-sketches.sh && \
-bash run-background.sh bash upgrade-system.sh # Run this in the background so it's not stopped during upgrade
+bash run-background.sh bash upgrade-system.sh >> logs/updates/system.txt # Run this in the background so it's not stopped during upgrade
