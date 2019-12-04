cfb71013bd8d49ac683e869c6e6a35382f8b5ba8
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-04 15:38:35 +1100

    Improved upgrade system script

--- upgrade-system.sh
+++ upgrade-system.sh
@@ -15,8 +15,14 @@ echo "  Branch: $BRANCH"
 echo "  Installed version: $INSTALLED_VERSION"
 echo "  Latest version: $LATEST_FULL_VERSION"
 
-if [ "$LATEST_FULL_VERSION" != "" ] & [ "$INSTALLED_VERSION" != "$LATEST_FULL_VERSION" ]; then
-  echo "  New GrowSense system version available. Upgrading."
+if [ ! "$LATEST_BUILD_NUMBER" ]; then
+  echo "  Error: Failed to get the latest build number. Skipping upgrade."
+  exit 1
+elif [ ! "$LATEST_VERSION_NUMBER" ]; then
+  echo "  Error: Failed to get the latest build number. Skipping upgrade."
+  exit 1
+elif [ "$INSTALLED_VERSION" != "$LATEST_FULL_VERSION" ]; then
+  echo "  New GrowSense system version available. Upgrading..."
   
   SUDO=""
   if [ ! "$(id -u)" -eq 0 ]; then
@@ -26,6 +32,9 @@ if [ "$LATEST_FULL_VERSION" != "" ] & [ "$INSTALLED_VERSION" != "$LATEST_FULL_VE
   echo "  Publishing status to MQTT..."
   bash mqtt-publish.sh "garden/StatusMessage" "Upgrading"
 
+  echo "  Sending email report..."
+  bash send-email.sh "GrowSense system upgrading (v$LATEST_FULL_VERSION) on $HOST" "The GrowSense system was upgraded on $HOST...\n\nPrevious version: $INSTALLED_VERSION\nNew version: $LATEST_FULL_VERSION"
+  
 # Disabled because it can potentially break the system by causing conflicts with docker containers 
 #  $SUDO apt-get update && $SUDO apt-get -y upgrade
   
@@ -50,7 +59,7 @@ if [ "$LATEST_FULL_VERSION" != "" ] & [ "$INSTALLED_VERSION" != "$LATEST_FULL_VE
   bash mqtt-publish.sh "garden/StatusMessage" "Upgraded"
   
   echo "  Sending email report..."
-  bash send-email.sh "GrowSense system upgraded on $HOST" "The GrowSense system was upgraded on $HOST...\n\nPrevious version: $INSTALLED_VERSION\nNew version: $LATEST_FULL_VERSION"
+  bash send-email.sh "GrowSense system upgraded(v$LATEST_FULL_VERSION) on $HOST" "The GrowSense system was upgraded on $HOST...\n\nPrevious version: $INSTALLED_VERSION\nNew version: $LATEST_FULL_VERSION"
   
   echo "Finished upgrading system"
 else
