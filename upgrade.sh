b21ef2fb83c7f95bdb56fd98ad76ef1628b8137c
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-05 08:04:56 +1100

    Fixed upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 bash cache-repository.sh $BRANCH && \
-bash bash upgrade-garden-device-sketches.sh && \
+bash upgrade-garden-device-sketches.sh && \
 bash run-background.sh bash upgrade-system.sh >> logs/updates/system.txt # Run this in the background so it's not stopped during upgrade
