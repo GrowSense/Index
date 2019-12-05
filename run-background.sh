6d50a51d356e2bb48b5a89b71c99e345f21ec7fb
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-06 09:41:17 +1100

    Trying to fix run-background.sh script

--- run-background.sh
+++ run-background.sh
@@ -1,1 +1,15 @@
-nohup $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} >/dev/null 2>&1 &
+echo "Launching command in background..."
+echo "Command:"
+echo "  nohup $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} 2>&1 &"
+echo ""
+
+SUDO=""
+if [ ! "$(id -u)" -eq 0 ]; then
+  if [ ! -f "is-mock-sudo.txt" ]; then
+    SUDO='sudo'
+  fi
+fi
+
+$SUDO bash -c "nohup $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} 2>&1 &"
+echo ""
+echo "Finished launching command in background"
