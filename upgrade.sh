6cd7d3d85470d3449c4c30f57a82d44c243bb380
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-06 09:26:24 +1100

    Trying to fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -7,6 +7,6 @@ echo "  Branch: $BRANCH"
 bash cache-repository.sh $BRANCH || exit 1
 bash upgrade-garden-device-sketches.sh || exit 1
 mkdir -p logs/updates || exit 1
-bash run-background.sh bash upgrade-system.sh >> logs/updates/system.txt # Run this in the background so it's not stopped during upgrade
+bash run-background.sh "bash upgrade-system.sh >> logs/updates/system.txt" # Run this in the background so it's not stopped during upgrade
 
 echo "Finished launching upgrade."
