b69b5ea259879815c78f2d5fc2ca10156169a9fc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-07 08:45:45 +1100

    Create upgrade service is now stopped after creation it will be started by supervisor service

--- create-upgrade-service.sh
+++ create-upgrade-service.sh
@@ -13,12 +13,15 @@ echo "$SERVICE_FILE_PATH"
 
 echo "Copying service file..."
 
-cp $SERVICE_EXAMPLE_FILE_PATH $SERVICE_FILE_PATH && \
+cp $SERVICE_EXAMPLE_FILE_PATH $SERVICE_FILE_PATH || exit 1
 
 echo "Service file:"
 echo "$SERVICE_FILE_PATH"
 
 echo "Installing service..."
-bash install-service.sh $SERVICE_FILE_PATH && \
+bash install-service.sh $SERVICE_FILE_PATH || exit 1
+
+echo "Stopping service (it will be started by the supervisor)..."
+bash systemctl stop growsense-upgrade.service || exit 1
 
 echo "Finished GrowSense system upgrade service"
