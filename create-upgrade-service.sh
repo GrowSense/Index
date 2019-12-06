63cb805bc606e06a9a91d5e6cb1485cc0f6764aa
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-07 09:08:03 +1100

    Tweaking scripts to avoid starting the upgrade service before its time

--- create-upgrade-service.sh
+++ create-upgrade-service.sh
@@ -19,9 +19,6 @@ echo "Service file:"
 echo "$SERVICE_FILE_PATH"
 
 echo "Installing service..."
-bash install-service.sh $SERVICE_FILE_PATH || exit 1
-
-echo "Stopping service (it will be started by the supervisor)..."
-bash systemctl stop growsense-upgrade.service || exit 1
+bash install-service.sh $SERVICE_FILE_PATH 0 || exit 1
 
 echo "Finished GrowSense system upgrade service"
--- install-service.sh
+++ install-service.sh
@@ -3,6 +3,12 @@
 SERVICE_FILE_PATH=$1
 SERVICE_FILE=$(basename -- "$SERVICE_FILE_PATH")
 
+START_SERVICE=$2
+
+if [ ! "$START_SERVICE" ]; then
+  START_SERVICE=1
+fi
+
 echo "Installing service"
 echo "Path: $SERVICE_FILE_PATH"
 echo "Name: $SERVICE_FILE"
@@ -60,21 +66,21 @@ else
   echo "  Enabling service..." 
   sh $SYSTEMCTL_SCRIPT enable $SERVICE_FILE || exit 1
   
-  echo ""
-  echo "  Starting service..." 
-  sh $SYSTEMCTL_SCRIPT start $SERVICE_FILE || exit 1
+  if [ "$START_SERVICE" == "1" ]; then
+    echo ""
+    echo "  Starting service..." 
+    sh $SYSTEMCTL_SCRIPT start $SERVICE_FILE || exit 1
   
-  #sh $SYSTEMCTL_SCRIPT restart $SERVICE_FILE # TODO: Remove if not needed
+    #sh $SYSTEMCTL_SCRIPT restart $SERVICE_FILE # TODO: Remove if not needed
   
-  
-  SERVICE_STATUS=$(sh $SYSTEMCTL_SCRIPT status $SERVICE_FILE)
+    SERVICE_STATUS=$(sh $SYSTEMCTL_SCRIPT status $SERVICE_FILE)
 
-  echo "${SERVICE_STATUS}"
+    echo "${SERVICE_STATUS}"
 
-  [[ ! $(echo $SERVICE_STATUS) =~ "Loaded: loaded" ]] && echo "Service isn't loaded" && exit 1
-  [[ ! $(echo $SERVICE_STATUS) =~ "Active: active" ]] && echo "Service isn't active" && exit 1
-  [[ $(echo $SERVICE_STATUS) =~ "not found" ]] && echo "Service wasn't found" && exit 1
-
+    [[ ! $(echo $SERVICE_STATUS) =~ "Loaded: loaded" ]] && echo "Service isn't loaded" && exit 1
+    [[ ! $(echo $SERVICE_STATUS) =~ "Active: active" ]] && echo "Service isn't active" && exit 1
+    [[ $(echo $SERVICE_STATUS) =~ "not found" ]] && echo "Service wasn't found" && exit 1
+ fi
 fi
 
 echo "Finished installing service"
