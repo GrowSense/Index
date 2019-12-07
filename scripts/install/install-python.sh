db5dc050304f059fddaddb22caafdc1914e5acd7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-07 11:05:55 +1100

    Trying to move to python 3

--- scripts/install/install-python.sh
+++ scripts/install/install-python.sh
@@ -1,17 +1,18 @@
 echo "Installing python..."
 
 # python
-if ! type "python" > /dev/null; then
-  $SUDO apt-get install -y python
+if ! type "python3" > /dev/null; then
+  echo "  Installing python3..."
+  $SUDO apt-get install -y python3
 else
   echo "  Python is already installed. Skipping install."
 fi
 
 # pip
-if ! type "pip" &>/dev/null; then
-  echo "  Installing pip..."
+if ! type "pip3" &>/dev/null; then
+  echo "  Installing pip3..."
 
-  $SUDO apt-get install -y python-pip
+  $SUDO apt-get install -y python3-pip
 else
   echo "  Pip is already installed. Skipping install."
 fi
