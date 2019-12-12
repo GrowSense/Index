823f1519840ea210e1aa76937543db04e7f843bc
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-12 15:32:48 +1100

    Fixed wait for unlock script

--- wait-for-unlock.sh
+++ wait-for-unlock.sh
@@ -13,7 +13,7 @@ LOOP_NUMBER=0
 
 KEEP_WAITING=1
 
-while [ $KEEP_WAITING = 1 ]
+while [ "$KEEP_WAITING" == "1" ]
 do
   echo ""
   echo "  Loop number: $LOOP_NUMBER"
@@ -45,11 +45,14 @@ do
     KEEP_WAITING=0
   fi
   
-  if [ $KEEP_WAITING = 1 ]; then
+  if [ "$KEEP_WAITING" == "1" ]; then
     echo "  Sleeping for $SLEEP_TIME seconds before retrying..."
     sleep $SLEEP_TIME
     
-    [ $LOOP_NUMBER = $MAX_RETRIES ] && echo "  Reached maximum retry count." && KEEP_WAITING=0
+    if [[ "$LOOP_NUMBER" == "$MAX_RETRIES" ]]; then
+      echo "  Reached maximum retry count."
+      KEEP_WAITING=0
+    fi
     
     LOOP_NUMBER=$((LOOP_NUMBER+1))
   else
