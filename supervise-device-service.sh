ad2c994c194bdc14f8532029080c5fbe9b715dd3
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-16 08:57:15 +1100

    Fixing supervise device services script

--- supervise-device-service.sh
+++ supervise-device-service.sh
@@ -25,35 +25,39 @@ fi
 
 HOST=$(cat /etc/hostname)
 
-if [[ "$SERVICE_NAME" != "" ]] && [[ $(echo $SERVICE_STATUS) =~ "Reason: No such file or directory" ]]; then
-  echo "The service 'SERVICE_NAME' doesn't exist."
-  bash send-email.sh "$SERVICE_LABEL hasn't been installed on $HOST." "The $SERVICE_LABEL service for $DEVICE_NAME hasn't been installed on $HOST.  Installing service...\n\nDetails:\n\n$SERVICE_STATUS"
+if [[ "$SERVICE_NAME" != "" ]]; then
+  if [[ $(echo $SERVICE_STATUS) =~ "Reason: No such file or directory" ]]; then
+    echo "The service 'SERVICE_NAME' doesn't exist."
+    bash send-email.sh "$SERVICE_LABEL hasn't been installed on $HOST." "The $SERVICE_LABEL service for $DEVICE_NAME hasn't been installed on $HOST.  Installing service...\n\nDetails:\n\n$SERVICE_STATUS"
 
-  bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
+    bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
   
-  bash create-alert-file.sh "The $SERVICE_LABEL service hasn't been installed on $HOST. Installing service..."
+    bash create-alert-file.sh "The $SERVICE_LABEL service hasn't been installed on $HOST. Installing service..."
 
-  bash create-garden-device-services.sh $DEVICE_NAME
-elif [[ ! $(echo $SERVICE_RESULT) =~ "D;" ]]; then
+    bash create-garden-device-services.sh $DEVICE_NAME
+  elif [[ ! $(echo $SERVICE_RESULT) =~ "D;" ]]; then
  
-  echo "The service 'SERVICE_NAME' isn't receiving data from device. Restarting..."
-  bash send-email.sh "The $SERVICE_LABEL service isn't receiving data from device $DEVICE_NAME (on $HOST)" "The $SERVICE_LABEL service isn't receiving data from $DEVICE_NAME device on $HOST.  Restarting service...\n\nDetails:\n\n$SERVICE_STATUS"
+    echo "The service 'SERVICE_NAME' isn't receiving data from device. Restarting..."
+    bash send-email.sh "The $SERVICE_LABEL service isn't receiving data from device $DEVICE_NAME (on $HOST)" "The $SERVICE_LABEL service isn't receiving data from $DEVICE_NAME device on $HOST.  Restarting service...\n\nDetails:\n\n$SERVICE_STATUS"
 
-  bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
+    bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
 
-  bash create-alert-file.sh "The $SERVICE_LABEL service isn't receiving data from  $DEVICE_NAME on $HOST. Restarting service..."
+    bash create-alert-file.sh "The $SERVICE_LABEL service isn't receiving data from  $DEVICE_NAME on $HOST. Restarting service..."
 
-  bash systemctl.sh start $SERVICE_NAME
-elif [[ $(echo $SERVICE_RESULT) =~ "Active: inactive" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "Active: dead" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "Active: failed" ]]; then
+    bash systemctl.sh start $SERVICE_NAME
+  elif [[ $(echo $SERVICE_RESULT) =~ "Active: inactive" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "Active: dead" ]] ||  [[ $(echo $SERVICE_RESULT) =~ "Active: failed" ]]; then
  
-  echo "The service 'SERVICE_NAME' isn't active. Restarting..."
-  bash send-email.sh "The $SERVICE_LABEL service for $DEVICE_NAME isn't active on $HOST. Restarting service..." "The $SERVICE_LABEL service for $DEVICE_NAME isn't running on $HOST.  Restarting service...\n\nDetails:\n\n$SERVICE_STATUS"
+    echo "The service 'SERVICE_NAME' isn't active. Restarting..."
+    bash send-email.sh "The $SERVICE_LABEL service for $DEVICE_NAME isn't active on $HOST. Restarting service..." "The $SERVICE_LABEL service for $DEVICE_NAME isn't running on $HOST.  Restarting service...\n\nDetails:\n\n$SERVICE_STATUS"
 
-  bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
+    bash mqtt-publish.sh "garden/StatusMessage" "$DEVICE_NAME offline" -r
 
-  bash create-alert-file.sh "The $SERVICE_LABEL service for $DEVICE_NAME service isn't active on $HOST. Restarting service..."
+    bash create-alert-file.sh "The $SERVICE_LABEL service for $DEVICE_NAME service isn't active on $HOST. Restarting service..."
 
-  bash systemctl.sh start $SERVICE_NAME
+    bash systemctl.sh start $SERVICE_NAME
+  else
+    echo "The service for '$DEVICE_NAME' is active."
+  fi
 else
-  echo "The service for '$DEVICE_NAME' is active."
+  echo "The device '$DEVICE_NAME' has no services. Skipping check."
 fi
