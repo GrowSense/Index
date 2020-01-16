9fba0a765bf9458d47155b8fcbe84420560d5810
CompulsiveCoder <compulsivecoder@gmail.com>
2020-01-17 09:45:40 +1100

    Tweaked MQTT bridge start script so it retries on error

--- scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge.sh
+++ scripts/apps/BridgeArduinoSerialToMqttSplitCsv/start-mqtt-bridge.sh
@@ -3,4 +3,4 @@
 
 echo "Starting mqtt bridge..."
 
-mono BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe $1 $2 $3 $4 $5 $6 $7 $8 $9
+mono BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe $1 $2 $3 $4 $5 $6 $7 $8 $9 || (echo "MQTT bridge error. Retrying in 5 seconds..." && sleep 5 && bash start-mqtt-bridge.sh $1 $2 $3 $4 $5 $6 $7 $8 $9)
