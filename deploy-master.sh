68b1c8c0ef67b6f67b419cf4dc390e8a55656c5f
CompulsiveCoder <compulsivecoder@gmail.com>
2020-01-19 08:03:59 +1100

    Fixing deploy master script

--- deploy-master.sh
+++ deploy-master.sh
@@ -8,35 +8,31 @@ if [ "$BRANCH" = "master" ]; then
 
   echo ""
 
-  echo "Waiting for deployment to unlock..."
   . ./detect-deployment-details.sh
-  sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash wait-for-unlock.sh" || echo "Failed to wait for unlock. Script likely doesn't exist because it hasn't been installed."
 
-  echo ""
+  if sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST '[ -d /usr/local/GrowSense/Index/ ]'; then
+    echo "Waiting for deployment to unlock..."
+    sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash wait-for-unlock.sh" || echo "Failed to wait for unlock. Script likely doesn't exist because it hasn't been installed."
 
-  echo "Renaming irrigatorW1 to NewIrrigatorW so automatic device naming can be tested during deployment..."
-  sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash rename-device.sh irrigatorW1 NewIrrigatorW" || echo "Failed to rename illuminator1 to NewIlluminator. Script likely doesn't exist because it hasn't been installed."
+    echo ""
 
-  echo ""
+    echo "Renaming irrigatorW1 to NewIrrigatorW so automatic device naming can be tested during deployment..."
+    sshpass -p $INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $INSTALL_SSH_USERNAME@$INSTALL_HOST "cd /usr/local/GrowSense/Index && bash rename-device.sh irrigatorW1 NewIrrigatorW" || echo "Failed to rename illuminator1 to NewIlluminator. Script likely doesn't exist because it hasn't been installed."
 
-  echo "Uninstalling GrowSense plug and play on remote computer..."
-  echo "Host: $MASTER_INSTALL_HOST"
+    echo ""
 
-  sshpass -p $MASTER_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $MASTER_INSTALL_SSH_USERNAME@$MASTER_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/uninstall-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
+    echo "Uninstalling GrowSense plug and play on remote computer..."
+    echo "Host: $MASTER_INSTALL_HOST"
 
-  echo ""
+    sshpass -p $MASTER_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $MASTER_INSTALL_SSH_USERNAME@$MASTER_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/uninstall-plug-and-play-from-web.sh | bash -s -- $BRANCH" || exit 1
+
+    echo ""
+  fi
 
   echo "Installing GrowSense plug and play on remote computer..."
   echo "Host: $MASTER_INSTALL_HOST"
 
   sshpass -p $MASTER_INSTALL_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $MASTER_INSTALL_SSH_USERNAME@$MASTER_INSTALL_HOST "wget -q --no-cache -O - https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts-web/install-plug-and-play-from-web.sh | bash -s -- $BRANCH ? $WIFI_NAME $WIFI_PASSWORD $MASTER_MQTT_HOST $MASTER_MQTT_USERNAME $MASTER_MQTT_PASSWORD $MASTER_MQTT_PORT $SMTP_SERVER $EMAIL_ADDRESS"  || exit 1
-
-  echo ""
-
-  #START_WAIT_TIME=80
-
-  #echo "Giving services time to start ($START_WAIT_TIME seconds)..."
-  #sleep $START_WAIT_TIME
 
   echo ""
 
