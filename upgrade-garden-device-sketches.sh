e7f56038bef1c6c5e8ee7de16358ea8d72f1a41b
CompulsiveCoder <compulsivecoder@gmail.com>
2020-01-21 10:03:59 +1100

    Upgrade garden device sketches script now stops supervisor before upgrading and starts it after, to avoid causing offline status during upgrade

--- upgrade-garden-device-sketches.sh
+++ upgrade-garden-device-sketches.sh
@@ -8,18 +8,21 @@ echo ""
 echo "Upgrading devices..."
 echo ""
 
+echo "  Stopping supervisor service to avoid offline status for devices during upgrade..."
+bash stop-supervisor.sh || exit 1
+
 if [ -d "$DEVICES_DIR" ]; then
   for d in $DEVICES_DIR/*; do
     if [ -d $d ]; then
       DEVICE_NAME=$(cat $d/name.txt)
-      DEVICE_LABEL=$(cat $d/label.txt)        
-        
+      DEVICE_LABEL=$(cat $d/label.txt)
+
       echo ""
       echo "Label: $DEVICE_LABEL"
       echo "Name: $DEVICE_NAME"
-        
+
       sh upgrade-garden-device-sketch.sh $DEVICE_NAME || echo "Failed to upgrade device sketch"
-       
+
       echo ""
     fi
   done
@@ -27,4 +30,9 @@ else
     echo "No devices have been added."
 fi
 
+echo "  Starting supervisor service..."
+bash start-supervisor.sh || exit 1
+
+echo ""
+echo "Finished upgrading devices."
 echo ""
