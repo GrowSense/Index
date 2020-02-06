7322fd0c94a16902241ac1daf8caf48e1f9cfc1b
CompulsiveCoder <compulsivecoder@gmail.com>
2020-02-06 14:01:21 +1100

    Trying to fix run mosquitto container script

--- scripts/docker/mosquitto/run-mosquitto-arm.sh
+++ scripts/docker/mosquitto/run-mosquitto-arm.sh
@@ -1,8 +1,6 @@
 #docker stop mosquitto || echo "Skipping stop"
 #docker rm mosquitto || echo "Skipping remove" 
 
-LOOP_NUMBER=$1
-
 MAX_LOOPS=5
 
 if [ ! "$LOOP_NUMBER" ]; then
@@ -18,9 +16,16 @@ if [ ! "$(id -u)" -eq 0 ]; then
   SUDO='sudo'
 fi
 
-docker run -d \
-  --restart=always \
-  --name=mosquitto \
+didSucceed=0;
+loopNumber=1
+
+while [ "$didSucceed" -eq "0" ] && [ "$loopNumber" -lt "$MAX_LOOPS" ]; do
+  echo "Running mosquitto docker container..."
+  echo "  Loop number $loopNumber"
+  
+  docker run -d \
+    --restart=always \
+    --name=mosquitto \
 	--volume $MOSQUITTO_DIR/data:/mosquitto_data \
 	-e MQTT_HOST=localhost \
 	-e MQTT_CLIENTID=client1234 \
@@ -29,4 +34,14 @@ docker run -d \
 	-e MQTT_TOPIC=Test \
 	-p 1883:1883 \
 	-p 8080:8080 \
-	compulsivecoder/mosquitto-arm || (echo "Error: Failed to start mosquitto docker container" && [[ "$LOOP_NUMBER" -lt "$MAX_LOOPS" ]] && echo "  Retrying..." && $SUDO service docker restart && bash run-mosquitto-arm.sh $(($LOOP_NUMBER+1)))
+	compulsivecoder/mosquitto-arm && didSucceed=1 || didSucceed=0
+	
+  if [ "$didSucceed" == "0" ]; then
+    echo "Error: Failed to start mosquitto docker container"
+    if [[ "$LOOP_NUMBER" -lt "$MAX_LOOPS" ]]; then
+      echo "  Restarting docker service..."
+      $SUDO service docker restart
+      loopNumber=$(($loopNumber+1))
+    fi
+  fi
+done
