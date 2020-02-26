3c4d29f8a3ef0d9505f22ece0489a3ab9c7464b5
CompulsiveCoder <compulsivecoder@gmail.com>
2020-02-26 16:53:55 +1100

    Added check ports script for identifying which ports are configured and which are unused

---
+++ check-ports.sh
@@ -0,0 +1,36 @@
+echo "Checking which ports are configured as GrowSense devices..."
+echo ""
+
+pioList=$(pio device list)
+
+while IFS= read -r line; do
+  if [[ "$line" == *"/dev/"* ]]; then
+    deviceIsConfigured=0
+    if [[ -d "devices" ]]; then
+      for deviceDir in devices/*; do
+        #echo "$deviceDir"
+        if [[ -d "$deviceDir" ]]; then
+          deviceInfoPort=$(cat $deviceDir/port.txt)
+          if [[ "$line" == *"$deviceInfoPort"* ]]; then
+            deviceIsConfigured=1
+            deviceLabel=$(cat $deviceDir/label.txt)
+            deviceName=$(cat $deviceDir/name.txt)
+          fi
+        fi
+      done
+    fi
+
+    if [ "$deviceIsConfigured" == "0" ]; then
+      echo "$line"
+      echo "  Not configured"
+    else
+      echo "$line"
+      echo "  Label: $deviceLabel"
+      echo "  Name: $deviceName"
+    fi
+
+    echo ""
+  fi
+done <<< "$pioList"
+
+echo "Finished checking which ports are configured as GrowSense devices."
