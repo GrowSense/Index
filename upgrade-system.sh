c60f1e562ec818e7124e13073f452ced63a3bada
CompulsiveCoder <compulsivecoder@gmail.com>
2020-02-27 11:21:24 +1100

    Fixing upgrade system script

--- upgrade-system.sh
+++ upgrade-system.sh
@@ -24,12 +24,12 @@ elif [ ! "$LATEST_VERSION_NUMBER" ]; then
 elif [ "$INSTALLED_VERSION" != "$LATEST_FULL_VERSION" ]; then
   echo "  New GrowSense system version available. Upgrading..."
 
-  echo "  Setting 'is-upgrading.txt' flag..."
-  echo "1" > "is-upgrading.txt"
-
   echo ""
   echo "  Waiting for the installation to unlock..."
   bash "wait-for-unlock.sh" # In quotes to avoid color coding issue in editor
+
+  echo "  Setting 'is-upgrading.txt' flag..."
+  echo "1" > "is-upgrading.txt"
 
   SUDO=""
   if [ ! "$(id -u)" -eq 0 ]; then
