5e372bbe8d482a315f45d735721e6dc3458cef30
CompulsiveCoder <compulsivecoder@gmail.com>
2020-02-27 12:34:01 +1100

    Fixing stop supervisor script

--- stop-supervisor.sh
+++ stop-supervisor.sh
@@ -1,7 +1,5 @@
 echo "Stopping GrowSense supervisor service..."
 
-bash "wait-for-unlock.sh" # In quotes to avoid color coding issue in editor
-
 if [ -f /lib/systemd/system/growsense-supervisor.service ]; then
-sh systemctl.sh stop growsense-supervisor.service || echo "Failed to stop supervisor service"
+  sh systemctl.sh stop growsense-supervisor.service || echo "Failed to stop supervisor service"
 fi
