e995d3eddb99633f447272441ddf36a9ca7bdad2
CompulsiveCoder <compulsivecoder@gmail.com>
2020-03-15 13:54:15 +1100

    Switching send serial command script to use python instead of python3 due to issues with importing serial module

--- scripts/install/install-python.sh
+++ scripts/install/install-python.sh
@@ -3,7 +3,7 @@ echo "Installing python..."
 # python
 if ! type "python3" > /dev/null; then
   echo "  Installing python3..."
-  $SUDO apt-get install -y python3 python3-setuptools
+  $SUDO apt-get install -y python3 python3-setuptools python3-serial || exit 1
 else
   echo "  Python is already installed. Skipping install."
 fi
@@ -12,14 +12,14 @@ fi
 if ! type "pip3" &>/dev/null; then
   echo "  Installing pip3..."
 
-  $SUDO apt-get install -y python3-pip
+  $SUDO apt-get install -y python3-pip || exit 1
 else
   echo "  Pip is already installed. Skipping install."
 fi
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 $CURRENT_DIR/install-python-modules.py
+python3 $CURRENT_DIR/install-python-modules.py || exit 1
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
