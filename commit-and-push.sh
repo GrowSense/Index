d8abe6f700cbb0f34ae246385f8fb7279aaed9bb
CompulsiveCoder <compulsivecoder@gmail.com>
2021-01-12 17:59:45 +1100

    Added local remote to commit and push script

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -2,4 +2,6 @@ bash disable-mocking.sh
 bash clean.sh && \
 git commit -am "$1" && \
 git pull origin dev && \
-git push origin dev
+git pull local dev &&  \
+git push origin dev && \
+git push local dev
