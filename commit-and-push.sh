c5e514dd0d414876de70133797c94ac99d43c05c
CompulsiveCoder <compulsivecoder@gmail.com>
2021-10-22 15:52:44 +1100

    Added console output

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -1,3 +1,6 @@
+echo "Committing and pushing to repository..."
+echo ""
+
 bash disable-mocking.sh
 bash clean.sh && \
 git commit -am "$1" && \
@@ -6,3 +9,6 @@ git push origin dev
 
 git pull local dev || echo "Failed to pull from local repository"
 git push local dev || echo "Failed to push to local repository"
+
+echo ""
+echo "Finished committing and pushing to repository"
