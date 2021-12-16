837863063f14c328d6f9e5d115b9f1626d7e89f1
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 13:54:40 +1100

    Switching to msbuild

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Building entire index"
 
-xbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
+msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
 echo "Build index complete"
