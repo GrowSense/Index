3e7a4e6d886babd144cee64a79ce1a7d333ed7da
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 16:49:09 +1100

    Changed to mono 4.5

--- build-cli.sh
+++ build-cli.sh
@@ -1,6 +1,6 @@
 echo "Building GrowSense Index CLI"
 
 #msbuild /t:restore /p:Configuration=Release /p:TargetFrameworkVersion=v4.8 /p:RestorePackagesConfig=true /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
-xbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.8 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
+xbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
 echo "Build GrowSense Index CLI complete"
