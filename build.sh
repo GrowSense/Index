50ec5076e430c61ed0dfc377d8e7942800cf68d5
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 14:00:08 +1100

    Adding restore packages to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "Building GrowSense Index CLI"
 
+msbuild /t:restore src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
 echo "Build GrowSense Index CLI complete"
