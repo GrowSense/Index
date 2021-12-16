bd22f6a379eb3a2485e609458ba4904dff939336
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 14:10:34 +1100

    Fixing nuget package restore

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 echo "Building GrowSense Index CLI"
 
-msbuild /t:restore src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
-msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
+msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /p:RestorePackagesConfig=true /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
 echo "Build GrowSense Index CLI complete"
