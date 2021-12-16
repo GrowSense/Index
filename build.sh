3ba658988be014c7eb88f4e6ee577912f6a17282
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 13:57:19 +1100

    Improved console output

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-echo "Building entire index"
+echo "Building GrowSense Index CLI"
 
 msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /verbosity:detailed src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
-echo "Build index complete"
+echo "Build GrowSense Index CLI complete"
