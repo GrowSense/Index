97ed4660c77b015bcc0a5314c2a95f594b5ba27a
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-16 13:53:00 +1100

    Added SettingsVerifier

---
+++ src/GrowSense.CLI/GrowSense.Core/Verifiers/SettingsVerifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace GrowSense.Core.Verifiers
+{
+  public class SettingsVerifier : BaseVerifier
+  {
+    public SettingsVerifier(CLIContext context) : base(context)
+    {
+    }
+
+    public void Verify()
+    {
+      VerifyLegacyFiles(Context.WorkingDirectory);
+    }
+    
+    public void VerifyLegacyFiles(string installDir)
+    {
+      AssertLegacySecurityFile("mqtt-username", Context.Settings.MqttUsername);
+      AssertLegacySecurityFile("mqtt-password", Context.Settings.MqttPassword);
+      AssertLegacySecurityFile("mqtt-port", Context.Settings.MqttPort.ToString());
+      AssertLegacySecurityFile("mqtt-host", Context.Settings.MqttHost);
+    }
+  }
+}
