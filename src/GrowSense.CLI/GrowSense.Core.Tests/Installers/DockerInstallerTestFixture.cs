7fa2abea53be6e45e205fe7eb5263fb99c215fa4
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-17 17:20:41 +1100

    Added missing files

---
+++ src/GrowSense.CLI/GrowSense.Core.Tests/Installers/DockerInstallerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using GrowSense.Core.Installers;
+
+namespace GrowSense.Core.Tests.Installers
+{
+  [TestFixture(Category = "Unit")]
+  public class DockerInstallerTestFixture : BaseTestFixture
+  {
+    [Test]
+    public void Test_Install()
+    {
+      MoveToTemporaryDirectory();
+      
+      var context = new CLIContext(Environment.CurrentDirectory, new CLISettings());
+      
+      var installer = new DockerInstaller(context);
+      installer.Install();
+    }
+  }
+}
---
+++ src/GrowSense.CLI/GrowSense.Core.Tests/PathHelperTestFixture.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+namespace GrowSense.Core.Tests
+{
+[TestFixture]
+  public class PathHelperTestFixture : BaseTestFixture
+  {
+    [Test]
+    public void Test_Initialize_IndexPath()
+    {
+      MoveToTemporaryDirectory();
+      
+      var startingDirectory = Environment.CurrentDirectory;
+
+      var settings = new CLISettings();
+      
+      var context = new CLIContext(startingDirectory, settings);
+      var paths = new PathHelper(context);
+
+      paths.Initialize(startingDirectory);
+
+      var expectedParentPath = Path.GetFullPath(Environment.CurrentDirectory + "/../../").TrimEnd('/');
+
+      Assert.AreEqual(expectedParentPath, context.ParentDirectory, "Parent path is not set correctly.");
+      
+      var expectedIndexDirectory = Environment.CurrentDirectory;
+
+      Assert.AreEqual(expectedIndexDirectory, context.IndexDirectory, "Index directory is not set correctly.");
+    }
+    
+    [Test]
+    public void Test_Initialize_ParentPath()
+    {
+      MoveToTemporaryDirectory();
+      
+      var startingDirectory = Path.GetFullPath(Environment.CurrentDirectory + "/../../");
+
+      var settings = new CLISettings();
+      
+      var context = new CLIContext(startingDirectory, settings);
+      var paths = new PathHelper(context);
+
+      paths.Initialize(startingDirectory);
+
+      var expectedParentPath = Path.GetFullPath(Environment.CurrentDirectory + "/../../").TrimEnd('/');
+
+      Assert.AreEqual(expectedParentPath, context.ParentDirectory, "Parent path is not set correctly.");
+      
+      var expectedIndexDirectory = Environment.CurrentDirectory;
+
+      Assert.AreEqual(expectedIndexDirectory, context.IndexDirectory, "Index directory is not set correctly.");
+    }
+    
+    [Test]
+    public void Test_Initialize_GrowSensePath()
+    {
+      MoveToTemporaryDirectory();
+      
+      var startingDirectory = Path.GetFullPath(Environment.CurrentDirectory + "/../");
+
+      var settings = new CLISettings();
+      
+      var context = new CLIContext(startingDirectory, settings);
+      var paths = new PathHelper(context);
+
+      paths.Initialize(startingDirectory);
+
+      var expectedParentPath = Path.GetFullPath(Environment.CurrentDirectory + "/../../").TrimEnd('/');
+
+      Assert.AreEqual(expectedParentPath, context.ParentDirectory, "Parent path is not set correctly.");
+      
+      var expectedIndexDirectory = Environment.CurrentDirectory;
+
+      Assert.AreEqual(expectedIndexDirectory, context.IndexDirectory, "Index directory is not set correctly.");
+    }
+  }
+}
