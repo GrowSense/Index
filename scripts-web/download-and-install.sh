70e950008cf178c86fff7897f029e522eb4a262f
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-17 17:32:14 +1100

    Overhauling download-and-install script

--- scripts-web/download-and-install.sh
+++ scripts-web/download-and-install.sh
@@ -60,196 +60,60 @@ fi
 echo "  Branch: $BRANCH"
 echo "  Install dir: $INSTALL_DIR"
 
-echo "  WiFi Name: $WIFI_NAME"
-echo "  WiFi Password: [hidden]"
-
-echo "  MQTT Host: $MQTT_HOST"
-echo "  MQTT Username: $MQTT_USERNAME"
-echo "  MQTT Password: [hidden]"
-echo "  MQTT Port: $MQTT_PORT"
-
-echo "  SMTP server: $SMTP_SERVER"
-echo "  Admin email: $ADMIN_EMAIL"
-echo "  SMTP username: $SMTP_USERNAME"
-echo "  SMTP password: [hidden]"
-echo "  SMTP port: $SMTP_PORT"
-
 INDEX_DIR="$INSTALL_DIR"
-GREENSENSE_DIR="$(dirname $INSTALL_DIR)"
-BASE_DIR="$(dirname $GREENSENSE_DIR)"
+GROWSENSE_DIR="$(dirname $INSTALL_DIR)"
+BASE_DIR="$(dirname $GROWSENSE_DIR)"
 
-echo ""
-echo "Creating ArduinoPlugAndPlay dir..."
-PNP_INSTALL_DIR="$BASE_DIR/ArduinoPlugAndPlay"
-mkdir -p $PNP_INSTALL_DIR || exit 1
-
-#cd $PNP_INSTALL_DIR
-
-echo ""
-echo "Importing GrowSense config file into ArduinoPlugAndPlay dir..."
-
-wget -q --no-cache https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts/apps/ArduinoPlugAndPlay/ArduinoPlugAndPlay.exe.config.system -O $PNP_INSTALL_DIR/ArduinoPlugAndPlay.exe.config || exit 1
-
-SUDO=""
-if [ ! "$(id -u)" -eq 0 ]; then
-  if [ ! -f "is-mock-sudo.txt" ]; then
-    SUDO='sudo'
-  else
-    echo "  Is mock sudo"
-  fi
-fi
-
-echo ""
-echo "Setting up GrowSense index..."
-
-#if [ ! -d "$INDEX_DIR/.git" ]; then
-#  $SUDO mkdir -p $INDEX_DIR || exit 1
-
-#  if [ -d $INDEX_DIR ]; then
-#    echo "Moving the existing GrowSense index..."
-
-#    $SUDO mv $INDEX_DIR $INDEX_DIR.old
-#  fi
-
-#  echo ""
-#  echo "Installing/updating git if needed"
-#  curl -sL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/scripts/install/install-git.sh | bash || exit 1
-
-#  BASE_REPO_CACHE_PATH="/usr/local"
-
-#  if [ -f "is-mock-system.txt" ]; then
-#    BASE_REPO_CACHE_PATH=$(readlink -m "$PWD/../../../../..")
-#  fi
-
-#  REPO_CACHE_PATH="$BASE_REPO_CACHE_PATH/git-cache/GrowSense/Index"
-#  echo "  Repository cache path:"
-#  echo "    $REPO_CACHE_PATH"
-
-#  echo ""
-#  echo "Caching repository..."
-#  curl -sL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/GrowSense/Index/$BRANCH/cache-repository.sh | bash -s $BRANCH $REPO_CACHE_PATH
-
-  echo "Installing required libraries..."
-  apt-get update && apt-get install -y jq || exit 1
-
-  echo ""
-  echo "Downloading the GrowSense Index..."
-
-  #$SUDO git clone -j 10 --depth 1 --recursive https://github.com/GrowSense/Index.git "$INDEX_DIR" --branch $BRANCH --reference /usr/local/git-cache/GrowSense/Index.reference || exit 1
-
-  #$SUDO git clone -j 10 --depth 1 --recursive $REPO_CACHE_PATH "$INDEX_DIR" --branch $BRANCH || exit 1
-#  $SUDO git clone --depth 1 --recursive $REPO_CACHE_PATH "$INDEX_DIR" --branch $BRANCH || exit 1
-
-#INDEX_DIR="$PWD/.tmp"
-
-  RELEASE_URL=$(curl -s https://api.github.com/repos/GrowSense/Index/releases | jq -r '.[0].assets[0].browser_download_url') || exit 1
-
-#  echo "$RELEASE_URL"
-
-#  LOCAL_RELEASE_FILE="GrowSenseRelease.zip"
-
-#  curl -L -o $LOCAL_RELEASE_FILE $RELEASE_URL || exit 1
-#  unzip -q $LOCAL_RELEASE_FILE -d $INDEX_DIR || exit 1
-
-#  rm $LOCAL_RELEASE_FILE
-
-#  if [ -d $INDEX_DIR.old ]; then
-#    echo "Importing pre-existing *.txt files..."
-#    mv $INDEX_DIR.old/*.txt $INDEX_DIR/
-
-
-#    if [ -d $INDEX_DIR.old/remote ]; then
-#      echo "Importing pre-existing remote folder..."
-#      mv $INDEX_DIR.old/remote $INDEX_DIR/remote
-#    fi
-
-#    echo ""
-#    echo "Removing old index directory..."
-#    sudo rm -r $INDEX_DIR.old
-#  fi
-
-#  echo ""
-#  echo "Moving into index directory..."
-#  echo "  $INDEX_DIR"
-
-#  cd $INDEX_DIR || exit 1
-
-#  echo ""
-#  echo "Preparing index..."
-
-#  if [ ! -f "is-mock-system.txt" ]; then
-#    $SUDO bash prepare.sh || exit 1
+#SUDO=""
+#if [ ! "$(id -u)" -eq 0 ]; then
+#  if [ ! -f "is-mock-sudo.txt" ]; then
+#    SUDO='sudo'
 #  else
-#    echo "[mock] $SUDO bash prepare.sh"
-  fi
+#    echo "  Is mock sudo"
+#  fi
 #fi
 
-#echo ""
-#echo "Moving into the index directory..."
+  echo ""
+  echo "Downloading and installing GrowSense..."
 
-#cd $INDEX_DIR || exit 1
+  curl -sL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/GrowSense/Installer/$BRANCH/scripts-download/download-installer.sh | bash -s $BRANCH $INSTALL_DIR
 
-#echo ""
-#echo "Updating the index..."
-
-#$SUDO bash update.sh || exit 1
+#$SUDO bash set-wifi-credentials.sh $WIFI_NAME $WIFI_PASSWORD || exit 1
+#$SUDO bash set-wifi-network-credentials.sh $WIFI_NAME $WIFI_PASSWORD || exit 1
 
 #echo ""
-#echo "Updating submodules..."
+#echo "[download-and-install.sh] Setting MQTT credentials..."
 
-#$SUDO bash update-submodules.sh $BRANCH || exit 1
+#$SUDO bash set-mqtt-credentials.sh $MQTT_HOST $MQTT_USERNAME $MQTT_PASSWORD $MQTT_PORT || exit 1
 
-echo "  Branch: $BRANCH"
+#echo ""
+#echo "[download-and-install.sh] Setting email details..."
 
-echo ""
-echo "[download-and-install.sh] Initializing runtime components..."
+#$SUDO bash set-email-details.sh $SMTP_SERVER $ADMIN_EMAIL $SMTP_USERNAME $SMTP_PASSWORD $SMTP_PORT || exit 1
 
-$SUDO bash init-runtime.sh "$BRANCH" || exit 1
+#echo ""
+#echo "[download-and-install.sh] Installing plug and play..."
 
-echo ""
-echo "[download-and-install.sh] Installing apps (so it's ready to run offline)..."
+#$SUDO wget -nv --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-ols/install.sh | bash -s -- $BRANCH $PNP_INSTALL_DIR $SMTP_SERVER $ADMIN_EMAIL $SMTP_USERNAME $SMTP_PASSWORD $SMTP_PORT || exit 1
 
-$SUDO bash install-apps.sh "$BRANCH" || exit 1
+#echo ""
+#echo "[download-and-install.sh] Creating garden..."
 
-echo ""
-echo "[download-and-install.sh] Setting WiFi credentials..."
+#$SUDO bash create-garden.sh || exit 1
 
-$SUDO bash set-wifi-credentials.sh $WIFI_NAME $WIFI_PASSWORD || exit 1
-$SUDO bash set-wifi-network-credentials.sh $WIFI_NAME $WIFI_PASSWORD || exit 1
+#echo ""
+#echo "[download-and-install.sh] Publishing status to MQTT..."
+#bash mqtt-publish.sh "garden/StatusMessage" "Installed" -r
 
-echo ""
-echo "[download-and-install.sh] Setting MQTT credentials..."
+#HOST=$(cat /etc/hostname)
 
-$SUDO bash set-mqtt-credentials.sh $MQTT_HOST $MQTT_USERNAME $MQTT_PASSWORD $MQTT_PORT || exit 1
+#echo ""
+#echo "[download-and-install.sh] Sending email report..."
+#bash send-email.sh "GrowSense software installed on $HOST" "The GrowSense software was successfully installed on $HOST."
 
-echo ""
-echo "[download-and-install.sh] Setting email details..."
-
-$SUDO bash set-email-details.sh $SMTP_SERVER $ADMIN_EMAIL $SMTP_USERNAME $SMTP_PASSWORD $SMTP_PORT || exit 1
-
-echo ""
-echo "[download-and-install.sh] Installing plug and play..."
-
-$SUDO wget -nv --no-cache -O - https://raw.githubusercontent.com/CompulsiveCoder/ArduinoPlugAndPlay/$BRANCH/scripts-ols/install.sh | bash -s -- $BRANCH $PNP_INSTALL_DIR $SMTP_SERVER $ADMIN_EMAIL $SMTP_USERNAME $SMTP_PASSWORD $SMTP_PORT || exit 1
+#echo ""
+#echo "[download-and-install.sh] Creating status message file..."
+#bash create-message-file.sh "GrowSense software installed"
 
 echo ""
-echo "[download-and-install.sh] Creating garden..."
-
-$SUDO bash create-garden.sh || exit 1
-
-echo ""
-echo "[download-and-install.sh] Publishing status to MQTT..."
-bash mqtt-publish.sh "garden/StatusMessage" "Installed" -r
-
-HOST=$(cat /etc/hostname)
-
-echo ""
-echo "[download-and-install.sh] Sending email report..."
-bash send-email.sh "GrowSense software installed on $HOST" "The GrowSense software was successfully installed on $HOST."
-
-echo ""
-echo "[download-and-install.sh] Creating status message file..."
-bash create-message-file.sh "GrowSense software installed"
-
-echo ""
-echo "[download-and-install.sh] Finished installing GrowSense plug and play."
+echo "[download-and-install.sh] Finished installing GrowSense."
