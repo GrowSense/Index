7356b071b1f8a03658900767b467f6f434bb749c
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-18 12:39:48 +1100

    Update

--- push-version.sh
+++ push-version.sh
@@ -5,8 +5,9 @@ BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$BRANCH" = "dev" ];  then
   git commit buildnumber.txt -m "Updated build number" && \
   git commit full-version.txt -m "Updated full version" && \
-  git pull origin $BRANCH --quiet && \
-  git push origin $BRANCH --quiet
+  bash push.sh
+#  git pull origin $BRANCH --quiet && \
+#  git push origin $BRANCH --quiet
 else
   echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
 fi
