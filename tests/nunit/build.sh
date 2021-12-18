c85b08c42c9cadda93fc4be9e437d05d1e74cc06
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-18 12:55:57 +1100

    Switched to msbuild

--- tests/nunit/build.sh
+++ tests/nunit/build.sh
@@ -3,6 +3,6 @@ echo "  Dir: $PWD"
 
 DIR=$PWD
 
-xbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 src/GrowSense.Index.sln /verbosity:quiet || exit 1
+msbuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 src/GrowSense.Index.sln /verbosity:quiet || exit 1
 
 echo "Finished building project tests."
