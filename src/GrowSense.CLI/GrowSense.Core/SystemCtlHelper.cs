382d16374cda871928867fdb759b931705ba82e7
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-20 10:25:41 +1100

    Added SystemCtlHelper

---
+++ src/GrowSense.CLI/GrowSense.Core/SystemCtlHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace GrowSense.Core
+{
+  public class SystemCtlHelper
+  {
+    public CLIContext Context;
+    public ProcessStarter Starter;
+    
+    public SystemCtlHelper(CLIContext context)
+    {
+      Context = context;
+      Starter = new ProcessStarter(Context.IndexDirectory);
+    }
+
+    public string Status(string serviceName)
+    {
+      return Run("status " + serviceName);
+    }
+
+    public string Run(string command)
+    {
+      if (Context.Settings.IsMockSystemCtl)
+      {
+      var output = "[mock] systemctl " + command;
+        Console.WriteLine(output);
+        return output;
+      }
+      else
+      {
+        Starter.StartBash("systemctl " + command);
+        return Starter.Output;
+      }
+    }
+  }
+}
