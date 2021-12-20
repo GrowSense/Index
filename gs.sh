1f1aacce96317ba3ee228ed8c65edb8332297da0
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-20 20:47:58 +1100

    Added mode argument to gs.sh. Allows switching to debug binaries

--- gs.sh
+++ gs.sh
@@ -1,1 +1,23 @@
-mono bin/Release/gs.exe --dir=$PWD $1 $2 $3 $4 $5 $6 $7 $8 $9
+echo "Launching GrowSense CLI..."
+
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            --mode)              mode=${VALUE} ;;
+            *)   
+    esac    
+
+
+done
+
+if [ ! "$mode" ]; then
+  mode="Debug"
+fi
+
+echo "  Mode: $mode"
+
+mono bin/$mode/gs.exe --dir=$PWD $1 $2 $3 $4 $5 $6 $7 $8 $9
