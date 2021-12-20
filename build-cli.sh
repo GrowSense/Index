71b075166d89a72e57346974fb11e920c82c152f
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-21 09:20:11 +1100

    Changed mono version

--- build-cli.sh
+++ build-cli.sh
@@ -2,6 +2,6 @@ echo "Building GrowSense Index CLI"
 
 msbuild /t:Clean src/GrowSense.CLI/GrowSense.CLI.sln || exit 1
 
-msbuild /t:Rebuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
+msbuild /t:Rebuild /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 src/GrowSense.CLI/GrowSense.CLI.sln  || exit 1
 
 echo "Build GrowSense Index CLI complete"
