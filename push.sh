b6d89f398fed3a8565d90e7c6ba6b4248392a9d6
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-24 09:21:14 +1100

    Disabled local repository because it's not currently in use

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-git pull local $BRANCH || echo "Failed to pull from local repository"
-git push local $BRANCH || echo "Failed to push to local repository"
+# Disabled local repository because it's not currently in use
+#git pull local $BRANCH || echo "Failed to pull from local repository"
+#git push local $BRANCH || echo "Failed to push to local repository"
 
 git pull lan $BRANCH || echo "Failed to pull from lan repository"
 git push lan $BRANCH || echo "Failed to push to lan repository"
