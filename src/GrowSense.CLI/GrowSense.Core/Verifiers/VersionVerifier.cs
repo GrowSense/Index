9ef03b9c5ba411c1938c6758494e587f964284b9
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-24 11:43:20 +1100

    Fixing version verification check

--- src/GrowSense.CLI/GrowSense.Core/Verifiers/VersionVerifier.cs
+++ src/GrowSense.CLI/GrowSense.Core/Verifiers/VersionVerifier.cs
@@ -17,7 +17,11 @@ namespace GrowSense.Core.Verifiers
 
       var detectedVersion = File.ReadAllText(Context.IndexDirectory + "/full-version.txt").Trim();
 
-      if (targetVersion != detectedVersion)
+      var versionIsSpecified = !String.IsNullOrEmpty(targetVersion) && targetVersion != "latest" && targetVersion != "0.0.0.0";
+
+      var versionsMatch = targetVersion == detectedVersion;
+
+      if (versionIsSpecified && !versionsMatch)
         throw new Exception("Incorrect version. Target: " + targetVersion + "; Detected: " + detectedVersion);
     }
   }
