712fa88035dd9d73b0652eb89682956c56d1640b
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-27 14:21:33 +1100

    Clearing console output so outputs from multiple commands don't get combined

--- src/GrowSense.CLI/GrowSense.Core/Tools/SshHelper.cs
+++ src/GrowSense.CLI/GrowSense.Core/Tools/SshHelper.cs
@@ -13,9 +13,12 @@ namespace GrowSense.Core.Tools
     public bool MoveToStartDirectory = true;
     public bool FailOnError = true;
 
+    public ProcessStarter Starter;
+
     public SshHelper(SshTarget target)
     {
       Target = target;
+      Starter = new ProcessStarter();
     }
 
     public string Execute(string command)
@@ -26,8 +29,6 @@ namespace GrowSense.Core.Tools
     public string Execute(string command, bool moveToStartDirectory, bool failOnError)
     {
       Console.WriteLine("Executing command via SSH...");
-
-      var starter = new ProcessStarter();
 
       if (!String.IsNullOrEmpty(StartDirectory) && moveToStartDirectory)
         command = "cd " + StartDirectory + " && " + command;
@@ -44,17 +45,18 @@ namespace GrowSense.Core.Tools
       if (UseSshPass)
         fullCommand = String.Format("sshpass -p {0} {1}", Target.Password, fullCommand);
 
-
-
-
       Console.WriteLine("  " + fullCommand);
 
-      starter.Start(fullCommand);
+      Starter.Start(fullCommand);
 
-      if (starter.IsError)
+      if (Starter.IsError)
         throw new Exception("An error occurred.");
 
-      return starter.Output;
+      var output = Starter.Output;
+
+      Starter.OutputBuilder.Clear();
+
+      return output;
     }
 
     public bool FileExists(string file)
