064c6270cbfdd5da19898d1447d389a868f5517c
CompulsiveCoder <compulsivecoder@gmail.com>
2021-12-28 08:39:13 +1100

    Disabled error checking by text matching due to false positives

--- src/GrowSense.CLI/GrowSense.Core.Tests/Deploy/DeploymentManager.cs
+++ src/GrowSense.CLI/GrowSense.Core.Tests/Deploy/DeploymentManager.cs
@@ -58,7 +58,9 @@ namespace GrowSense.Core.Tests.Deploy
       Console.WriteLine("Downloading and launching upgrade...");
 
       var installCommand = "sudo wget --no-cache -O - https://raw.githubusercontent.com/GrowSense/Installer/" + Branch + "/scripts-download/download-installer.sh | sudo bash -s -- upgrade --branch=" + Branch + " --to=/usr/local/ --enable-download=false --allow-skip-download=true --version=" + Version;
-      Ssh.Execute(installCommand);      
+      Ssh.Starter.EnableErrorCheckingByTextMatching = false;
+      Ssh.Execute(installCommand);
+      Ssh.Starter.EnableErrorCheckingByTextMatching = true;
     }
 
     public void SetConfigValues()
