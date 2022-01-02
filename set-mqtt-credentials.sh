b58b498a9f0628520798a6fe34a095f985fa5d47
CompulsiveCoder <compulsivecoder@gmail.com>
2022-01-02 13:45:36 +1100

    Script now calls CLI command

--- set-mqtt-credentials.sh
+++ set-mqtt-credentials.sh
@@ -24,27 +24,29 @@ if [ "$PASSWORD" ]; then
   echo $USERNAME > "mqtt-username.security"
   echo $PASSWORD > "mqtt-password.security"
   echo $PORT > "mqtt-port.security"
+  
+  bash gs.sh config --mqtt-host=$HOST --mqtt-username=$USERNAME --mqtt-password=$PASSWORD --mqtt-port=$PORT --verify=false
 
-  CREDENTIALS_FILE="$PWD/scripts/docker/mosquitto/data/mosquitto.userfile"
+#  CREDENTIALS_FILE="$PWD/scripts/docker/mosquitto/data/mosquitto.userfile"
 
-  echo ""
-  echo "  Setting mosquitto credentials file:"
-  echo "    $CREDENTIALS_FILE"
+#  echo ""
+#  echo "  Setting mosquitto credentials file:"
+#  echo "    $CREDENTIALS_FILE"
 #  echo "$USERNAME:$PASSWORD" > $CREDENTIALS_FILE
 
   # Clear existing file
-  if [ -f "$CREDENTIALS_FILE" ]; then
-    rm $CREDENTIALS_FILE
-  fi
-  touch $CREDENTIALS_FILE
-  mosquitto_passwd -b "$CREDENTIALS_FILE" "$USERNAME" "$PASSWORD" || exit 1
-  sudo mkdir -p /usr/local/mosquitto/data || exit 1
-  sudo cp -v $CREDENTIALS_FILE /usr/local/mosquitto/data/mosquitto.userfile || exit 1
+#  if [ -f "$CREDENTIALS_FILE" ]; then
+#    rm $CREDENTIALS_FILE
+#  fi
+#  touch $CREDENTIALS_FILE
+#  mosquitto_passwd -b "$CREDENTIALS_FILE" "$USERNAME" "$PASSWORD" || exit 1
+#  sudo mkdir -p /usr/local/mosquitto/data || exit 1
+#  sudo cp -v $CREDENTIALS_FILE /usr/local/mosquitto/data/mosquitto.userfile || exit 1
 
 
-  bash set-mqtt-credentials-for-www.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
-  bash set-mqtt-credentials-for-bridge.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
-  bash set-mqtt-credentials-for-1602-ui.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
+#  bash set-mqtt-credentials-for-www.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
+#  bash set-mqtt-credentials-for-bridge.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
+#  bash set-mqtt-credentials-for-1602-ui.sh $HOST $USERNAME $PASSWORD $PORT || exit 1
 
   echo ""
   echo "Finished setting MQTT credentials."
