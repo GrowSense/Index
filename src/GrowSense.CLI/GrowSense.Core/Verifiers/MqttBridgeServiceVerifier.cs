2b3e028a82acbc418f71dedc7d7c5c97e7551a1a
CompulsiveCoder <compulsivecoder@gmail.com>
2022-01-02 14:28:31 +1100

    Fixing device MQTT functionality

--- src/GrowSense.CLI/GrowSense.Core/Verifiers/MqttBridgeServiceVerifier.cs
+++ src/GrowSense.CLI/GrowSense.Core/Verifiers/MqttBridgeServiceVerifier.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using GrowSense.Core.Tools;
 using System.Threading;
+using GrowSense.Core.Devices;
 namespace GrowSense.Core.Verifiers
 {
   public class MqttBridgeServiceVerifier
   {
     public SystemCtlHelper SystemCtl;
+    public CLIContext Context;
     
     public MqttBridgeServiceVerifier(CLIContext context)
     {
       SystemCtl = new SystemCtlHelper(context);
+      Context = context;
+    }
+    
+    public void VerifyDevicesMqttBridgeServices(DeviceInfo[] devices)
+    {
+      Console.WriteLine("  Verifying devices MQTT bridge services...");
+
+      foreach (var device in devices)
+      {
+        Console.WriteLine("    Device: " + device.Name);
+        Console.WriteLine("      Board: " + device.Board);
+        Console.WriteLine("      Host: " + device.Host);
+        Console.WriteLine("      Group: " + device.Group);
+      
+        var isOnThisMachine = device.Host == Context.Settings.HostName;
+
+        var isUSBConnected = device.Board == "nano" || device.Board == "uno";
+
+        var isNotUIDevice = device.Group != "ui";
+
+        var requiresMqttBridge = isOnThisMachine && isUSBConnected && isNotUIDevice;
+
+        Console.WriteLine("      Requires MQTT bridge: " + requiresMqttBridge);
+
+        if (requiresMqttBridge)
+          Verify(device.Name);
+      }
+
+      Console.WriteLine("  Finished verifying devices MQTT bridge services.");
     }
 
     public void Verify(string deviceName)
@@ -22,9 +53,11 @@ namespace GrowSense.Core.Verifiers
 
       var status = SystemCtlServiceStatus.NotSet;
 
+      var serviceName = "growsense-mqtt-bridge-" + deviceName + ".service";
+
       while (!isFinished)
       {
-        status = SystemCtl.Status("growsense-mqtt-bridge-" + deviceName + ".service");
+        status = SystemCtl.Status(serviceName);
 
         attempt++;
 
@@ -33,9 +66,21 @@ namespace GrowSense.Core.Verifiers
         else
           Thread.Sleep(1000);
       }
-      
+              
       if (status != SystemCtlServiceStatus.Active)
-        throw new Exception("MQTT bridge service is not running for: " + deviceName);
+      {
+        throw new Exception("MQTT bridge service is not active for device: " + deviceName);
+      }
+      else
+      {
+        var statusReport = SystemCtl.StatusReport(serviceName);
+
+        if (statusReport.IndexOf("MqttConnectionException") > -1)
+          throw new Exception("MQTT Bridge failed to connect to broker for device: " + deviceName);
+        else if (statusReport.IndexOf("No such file or directory") > -1)
+          throw new Exception("USB device not found for device: " + deviceName);
+      }
+
 
       Console.WriteLine("Finished verifying MQTT bridge service is active.");
     }
--- src/GrowSense.CLI/GrowSense.Core/Verifiers/MqttBridgeVerifier.cs
+++ src/GrowSense.CLI/GrowSense.Core/Verifiers/MqttBridgeVerifier.cs
@@ -12,10 +12,12 @@ namespace GrowSense.Core.Verifiers
   public class MqttBridgeVerifier : BaseVerifier
   {
     public DeviceManager Devices;
+    public MqttBridgeServiceVerifier ServiceVerifier;
     
     public MqttBridgeVerifier(CLIContext context) : base(context)
     {
       Devices = new DeviceManager(context);
+      ServiceVerifier = new MqttBridgeServiceVerifier(context);
     }
 
     public void Verify()
@@ -33,56 +35,7 @@ namespace GrowSense.Core.Verifiers
 
       VerifyBinary(installDir);
 
-      VerifyDevicesMqttBridgeServices();
-    }
-
-    public void VerifyDevicesMqttBridgeServices()
-    {
-      Console.WriteLine("  Verifying devices MQTT bridge services...");
-
-      foreach (var device in Devices.GetDevices())
-      {
-        Console.WriteLine("    Device: " + device.Name);
-        Console.WriteLine("      Board: " + device.Board);
-        Console.WriteLine("      Host: " + device.Host);
-        Console.WriteLine("      Group: " + device.Group);
-      
-        var isOnThisMachine = device.Host == Context.Settings.HostName;
-
-        var isUSBConnected = device.Board == "nano" || device.Board == "uno";
-
-        var isNotUIDevice = device.Group != "ui";
-
-        var requiresMqttBridge = isOnThisMachine && isUSBConnected && isNotUIDevice;
-
-        Console.WriteLine("      Requires MQTT bridge: " + requiresMqttBridge);
-
-        if (requiresMqttBridge)
-          VerifyDeviceMqttBridgeService(device);
-      }
-
-      Console.WriteLine("  Finished verifying devices MQTT bridge services.");
-    }
-
-    public void VerifyDeviceMqttBridgeService(DeviceInfo device)
-    {
-      var serviceName = "growsense-mqtt-bridge-" + device.Name;
-
-      var mqttBridgeStatus = SystemCtl.Status(serviceName);
-
-      if (mqttBridgeStatus != SystemCtlServiceStatus.Active)
-      {
-        throw new Exception("MQTT bridge service is not active for device: " + device.Name);
-      }
-      else
-      {
-        var statusReport = SystemCtl.StatusReport(serviceName);
-
-        if (statusReport.IndexOf("MqttConnectionException") > -1)
-          throw new Exception("MQTT Bridge failed to connect to broker for device: " + device.Name);
-        else if (statusReport.IndexOf("No such file or directory") > -1)
-          throw new Exception("USB device not found for device: " + device.Name);
-      }
+      ServiceVerifier.VerifyDevicesMqttBridgeServices(Devices.GetDevices());
     }
 
     public void VerifyBinary(string installDir)
