e99eae5ab750d53ec1c9895cdb98c89193b86319
CompulsiveCoder <compulsivecoder@gmail.com>
2022-06-11 13:21:08 +1000

    [feat] Added version command to CLI and updated settings manager to load the current version from the full-version.txt file

---
+++ src/GrowSense.CLI/GrowSense.Core/VersionReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace GrowSense.Core
+{
+    public class VersionReader
+    {
+        public CLIContext Context; 
+
+        public VersionReader(CLIContext context)
+        {
+            Context = context;
+        }
+
+        public string ReadVersionInSettings()
+        {
+            return Context.Settings.Version;
+        }
+
+        public string ReadVersionInFiles()
+        {
+            var filesVersion = File.ReadAllText(Context.IndexDirectory + "/full-version.txt");
+
+            return filesVersion.Trim();
+        }
+
+        public void WriteVersionsToConsole()
+        {
+            Console.WriteLine("Checking GrowSense version...");
+
+            Console.WriteLine("  Files: " + ReadVersionInFiles());
+
+            Console.WriteLine("  Settings: " + ReadVersionInSettings());
+        }
+    }
+}
