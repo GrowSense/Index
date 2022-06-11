88f624b09c129cdde33207400a2044e616e684ea
CompulsiveCoder <compulsivecoder@gmail.com>
2022-06-11 11:10:47 +1000

    [fix] Reenabling use of growsense-upgrade systemctl service for upgrade

--- start-upgrade-service.sh
+++ start-upgrade-service.sh
@@ -1,6 +1,3 @@
 echo "Starting GrowSense upgrade service..."
 
-# TODO: Remove if not needed. Switching over to using new installer for upgrades
-#sh systemctl.sh start growsense-upgrade.service
-
-bash gs.sh upgrade
+sh systemctl.sh start growsense-upgrade.service
