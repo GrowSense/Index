b860a1d43fd96d7373a417c63c32bb2db0f47804
CompulsiveCoder <compulsivecoder@gmail.com>
2022-06-12 11:29:07 +1000

    [fix] Readding moved test

---
+++ src/GrowSense.CLI/GrowSense.Core.Tests/CLI/VersionCommandCLITestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GrowSense.Core.Tests.CLI
+{
+    [TestFixture]
+    public class VersionCommandCLITestFixture : BaseTestFixture
+    {
+        [Test]
+        public void VersionCommand()
+        {
+            MoveToTemporaryDirectory();
+
+            PullDirectoryFromProject();
+
+            var starter = new ProcessStarter();
+            starter.WorkingDirectory = TemporaryDirectory;
+
+            var mode = "Release";
+#if DEBUG
+            mode = "Debug";
+#endif
+
+            var upgradeCommand = "bash gs.sh version --mode=" + mode;
+
+            starter.Start(upgradeCommand);
+
+            Assert.IsFalse(starter.IsError, "An error occurred");
+
+        }
+    }
+}
